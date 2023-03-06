efce6822bec575d248ee94f7e512f0bcadbde0ae
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-06 15:17:13 +0900

    feat: 사람 및 최대 사다리 높이 예외 기능
    * 사용자가 사람 또는 최대 사다리 높이를 잘못 입력하는 경우 발생되는 예외

---
+++ src/main/java/kr/codesquad/InvalidMaximumLadderHeight.java
@@ -0,0 +1,9 @@
+package kr.codesquad;
+
+public class InvalidMaximumLadderHeight extends RuntimeException{
+    private static final String message = "적절하지 않은 입력입니다. 1 이상의 사다리 높이를 입력해주세요.";
+
+    public InvalidMaximumLadderHeight(){
+        super(message);
+    }
+}
