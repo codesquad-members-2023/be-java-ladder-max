eb72c34b2d4a0f9c8cb598f3a1c24df18dffba63
Jeongyong Park <1239824@naver.com>
2023-03-06 15:22:26 +0900

    feat : 사다리의 모습을 출력하기 위해 사용자로부터 사람 이름들과 사다리 높이를 입력받기
    * (사람이름, 사다리 높이)라는 입력을 한 번에 관리하기 위해 `Input` 클래스 생성
    * 이에 따른 로직 수정

--- src/main/java/kr/codesquad/view/OutputView.java
+++ src/main/java/kr/codesquad/view/OutputView.java
@@ -4,8 +4,8 @@ import kr.codesquad.domain.Ladder;
 
 public class OutputView {
 
-	public void printGetCountOfPersonMsg() {
-		System.out.println("참여할 사람은 몇 명인가요?");
+	public void printGetNamesOfPersonMsg() {
+		System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
 	}
 
 	public void printGetHeightOfLadderMsg() {
