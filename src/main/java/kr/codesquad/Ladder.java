de80e7cb8410532c36c3be7c0fab5a3e6edbfa5d
jwp345 <jwp135@naver.com>
2023-03-06 16:16:21 +0900

    else문 제거 + depth가 2인 메소드 분리 + rows와 cols는 ladders배열이 변경될 경우 개방폐쇄 원칙에 위반된다고 생각되어 리팩토링

--- src/main/java/kr/codesquad/Ladder.java
+++ src/main/java/kr/codesquad/Ladder.java
@@ -6,26 +6,26 @@ import java.util.Random;
 public class Ladder {
     private final String[][] ladders;
 
-    private final int rows, cols;
-
     Ladder(int people, int length) {
         this.ladders = new String[length][people - 1]; // 출력할 때 | 추가할거임
-        this.rows = length;
-        this.cols = people - 1;
         makeLadder();
     }
 
     void makeLadder() {
+        for(String[] ladder : ladders) {
+            makeLine(ladder);
+        }
+    }
+
+    private void makeLine(String[] ladder) {
         Random random = new Random();
-        for(int idx1 = 0; idx1 < rows; idx1++) {
-            for(int idx2 = 0; idx2 < cols; idx2++) {
-                int curr = random.nextInt(2);
-                if(curr == 0) {
-                    ladders[idx1][idx2] = "-";
-                } else {
-                    ladders[idx1][idx2] = " ";
-                }
+        for(int idx = 0; idx < ladder.length; idx++) {
+            int curr = random.nextInt(2);
+            if(curr == 0) {
+                ladder[idx] = "-";
+                continue;
             }
+            ladder[idx] = " ";
         }
     }
 
