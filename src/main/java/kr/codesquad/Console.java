deb3130b8a637fba229554e25e3478642884239a
Sully <2dcoder@naver.com>
2023-03-06 11:30:06 +0900

    feat(Console): 기본 생성자 추가하고, int형 변환
    정수를 입력받는데 String으로 리턴해버렸네..

--- src/main/java/kr/codesquad/Console.java
+++ src/main/java/kr/codesquad/Console.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Console {
+    public Console() {
+    }
+
     public void askParticipant() {
         System.out.println("참여할 사람은 몇 명인가요?");
     }
@@ -13,8 +16,8 @@ public class Console {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
     }
 
-    public String returnInput() throws IOException {
+    public int returnInput() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        return br.readLine();
+        return Integer.parseInt(br.readLine());
     }
 }
--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Console console = new Console();
         console.askParticipant();
-        String participant = console.returnInput();
+        int participantNumber = console.returnInput();
         console.askLadderHeight();
-        String ladderHeight = console.returnInput();
+        int ladderHeight = console.returnInput();
     }
 }
