54afa6e114560fad87daa0796214c6a7793f612f
Jeongyong Park <1239824@naver.com>
2023-03-06 12:21:10 +0900

    feat : 사다리를 생성하기 위해 사용자로부터 사람 수, 높이를 입력받기

---
+++ src/main/java/kr/codesquad/view/OutputView.java
@@ -0,0 +1,12 @@
+package kr.codesquad.view;
+
+public class OutputView {
+
+	public void printGetCountOfPersonMsg() {
+		System.out.println("참여할 사람은 몇 명인가요?");
+	}
+
+	public void printGetHeightOfLadderMsg() {
+		System.out.println("최대 사다리 높이는 몇 개인가요?");
+	}
+}
