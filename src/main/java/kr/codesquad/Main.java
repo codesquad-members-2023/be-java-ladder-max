61a67ba0cd17b9bb918c512f3ef06a41f01477d2
crtEvent <crtevn@gmail.com>
2023-03-06 23:45:38 +0900

    refactor: Main.main() depth 개선
    - 반복 실행하는 while문은 main()에 두고
    - game 실행과 예외처리 부분은 runApplication()에 따로 분리
      - depth는 개선되었으나, 여전히 runApplication()은 프로그램 실행과 예외처리를 담당한다

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -8,17 +8,23 @@ import kr.codesquad.view.InputView;
 import java.io.IOException;
 
 public class Main {
+
+    private static GameController controller = new GameController();
+
+    private static void runApplication() {
+        try {
+            controller.runLadderGame();
+        } catch (NumberFormatException e) {
+            System.out.println("잘못된 값을 입력하였습니다. 다시 입력해 주세요.");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) {
-        GameController controller = new GameController();
 
         while (true) {
-            try {
-                controller.runApplication();
-            } catch (NumberFormatException e) {
-                System.out.println("잘못된 값을 입력하였습니다. 다시 입력해 주세요.");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+            runApplication();
         }
 
     }
