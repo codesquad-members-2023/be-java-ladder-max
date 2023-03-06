54444920371ba1c885e3a62fa5c92b407215a894
pinetree <eodnjs568@gmail.com>
2023-03-06 17:14:40 +0900

    Ladder 클래스 내 메서드 생성
    생성자에 매개변수(input)을 통해 2차원 배열 생성
    makeBasicLadder() 메서드를 통해 사다리라인이 없는 기본적인 사다리 생성
    showFInalLadder() 메서드를 통해 사다리를 출력하는 메서드 생성

--- src/main/java/kr/codesquad/Application.java
+++ src/main/java/kr/codesquad/Application.java
@@ -3,11 +3,11 @@ package kr.codesquad;
 public class Application {
     public static void main(String[] args) {
         int peopleNum = Integer.parseInt(InputView.inputPeople());
-        int ladderCnt = Integer.parseInt(InputView.inputPeople());
+        int ladderCnt = Integer.parseInt(InputView.inputLadderHeight());
 
         Ladder ladder = new Ladder(peopleNum, ladderCnt);
-        ladder.makeLadderMap();
-        ladder.showTotalLadder();
+        ladder.makeBasicLadder();
+        ladder.showFinalLadder();
 
     }
 }
--- src/main/java/kr/codesquad/Ladder.java
+++ src/main/java/kr/codesquad/Ladder.java
@@ -3,43 +3,59 @@ package kr.codesquad;
 import java.util.Random;
 
 public class Ladder {
-    private String [][] laddder;
+    private String[][] laddder;
 
-    Ladder(int n, int m){
-        laddder = new String[2*m-1][n];
+    Ladder(int n, int m) {
+        this.laddder = new String[m][2 * n - 1];
 
     }
-    void makeLadderMap(){
 
-        for(int i=0; i<laddder.length; i++){
-            for(int j=0; j<laddder[i].length; j++){
-                if(j%2==1){
-                    laddder[i][j]= "|";
-                }
-                    laddder[i][j] =" ";
+    void makeBasicLadder() {
+        for (int i = 0; i < laddder.length; i++) {
+            for (int j = 0; j < laddder[i].length; j++) {
+                if (j % 2 == 0) {
+                    this.laddder[i][j] = "|";
+                } else this.laddder[i][j] = " ";
             }
         }
     }
 
-    void showTotalLadder(){
+    void showFinalLadder() {
 
-        for(int i=0 ; i<laddder.length; i++){
-            for(int j= 0; j<laddder[i].length; j++){
+        for (int i = 0; i < laddder.length; i++) {
+            for (int j = 0; j < laddder[i].length; j++) {
                 System.out.print(laddder[i][j]);
             }
             System.out.println();
         }
     }
 
-    void makeRandomLadderLine(){
+    void makeRandomLadderLine() {
+        /*
+         아무 위치에 사다리를 생성하는 것이 아니라,
+         2차원 배열에서 열의 인덱스가 홀수 일때
+         임의로 사다리를 그릴 수 있다.
+
+         사다리가 최대 들어갈 수 있는 회수는
+         m * (n-1) 이다.
+         */
         Random random = new Random();
         boolean randomBoolean = random.nextBoolean();
+
         // if -> True라면
-        if(randomBoolean){
+
+
+        for (int i = 0; i < laddder.length * laddder[i].length; i++) {
+
+        }
+        if (randomBoolean) {
+            String ladder = "-";
+
 
         }
     }
-    void printLadder(){
+
+    void printLadder() {
         System.out.println(laddder);
     }
 
