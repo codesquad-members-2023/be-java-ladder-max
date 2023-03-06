348451122239314e4f98a5b9888996395a3a4a8c
Sully <2dcoder@naver.com>
2023-03-06 11:23:58 +0900

    feat: 입력받는 콘솔 기능 추가
    Main에서 그냥 한번에 입력받고, 출력하려고 했지만
    Console이라는 클래스를 만들어서 최대한 메서드 분리할 예정

---
+++ src/main/java/kr/codesquad/Console.java
@@ -0,0 +1,20 @@
+package kr.codesquad;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Console {
+    public void askParticipant() {
+        System.out.println("참여할 사람은 몇 명인가요?");
+    }
+
+    public void askLadderHeight() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+    }
+
+    public String returnInput() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        return br.readLine();
+    }
+}
--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,13 @@
 package kr.codesquad;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Console console = new Console();
+        console.askParticipant();
+        String participant = console.returnInput();
+        console.askLadderHeight();
+        String ladderHeight = console.returnInput();
     }
 }
