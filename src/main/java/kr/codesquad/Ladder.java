2730f18014829590639165a82b71d586c7b0648c
CDchan <kdjin1215@Naver.com>
2023-03-06 17:16:24 +0900

    Ladder클래스에 사다리를 만들수있는 makeLadder메서드 구현
    makeLadder메서드 구현 및 Random한 사다리를 위해 Random하게 bool타입을
    return하는 random 메서드 추가 구현

--- src/main/java/kr/codesquad/Ladder.java
+++ src/main/java/kr/codesquad/Ladder.java
@@ -1,8 +1,11 @@
 package kr.codesquad;
 
+import java.util.Random;
+
 public class Ladder {
-    int peopleNum;
-    int ladderHeight;
+    private int peopleNum;
+    private int ladderHeight;
+    char [][] ladder;
 
     public void setPeopleNum(int peopleNum) {
         this.peopleNum = peopleNum;
@@ -10,5 +13,23 @@ public class Ladder {
 
     public void setLadderHeight(int ladderHeight) {
         this.ladderHeight = ladderHeight;
+    }
+
+    void makeLadder() {
+        ladder = new char[ladderHeight][peopleNum * 2 - 1];
+        for (int i = 0; i < ladderHeight; i++) {
+            for (int j = 0; j < peopleNum * 2 - 1; j++) {
+                if (j % 2 != 0) {
+                    ladder[i][j] = random() ? '-' : ' ';
+                } else {
+                    ladder[i][j] = '|';
+                }
+            }
+        }
+    }
+
+    boolean random(){
+        Random rd = new Random();
+        return rd.nextBoolean();
     }
 }
