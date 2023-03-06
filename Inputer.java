1aaefc2a5a6d665418bdc0034e367fe743ef6f52
javavoja <javavoja@protonmail.com>
2023-03-06 17:57:06 +0900

    2차 커밋
    2단계 구현(리팩토링 맛보기)

--- Inputer.java
+++ Inputer.java
@@ -2,13 +2,11 @@ import java.util.Scanner;
 
 public class Inputer {
     Scanner sc = new Scanner(System.in);
-
     public void setInput() {
         private int manNumFake = sc.nextInt();
         private int ladderNum = sc.nextInt();
     }
     private int manNum = manNumFake * (manNumFake - 1);
-
     public int getManNum(){
         int temp = manNum; return temp;
     }
