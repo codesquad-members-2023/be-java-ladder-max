a68275ebd4d8ad929e2035f032ecab353a534489
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-03-06 14:16:15 +0900

    리드미에 학습 계획 추가

---
+++ Ladder/Input.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Input {
+
+    public static void takeInput() {
+        printPeople();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int pplnumber = Integer.parseInt()
+    }
+}
---
+++ Ladder/RunLadder.java
@@ -0,0 +1,5 @@
+public class RunLadder {
+    public static void main(String[] args){
+        Input.takeInput();
+    }
+}
