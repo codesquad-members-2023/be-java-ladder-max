f25fce664ca978fd132f8da3182f17c4aa4a5063
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-06 15:16:38 +0900

    feat: 콘솔을 이용한 메시지 출력
    * 사용자에게 콘솔을 이용하여 입력 안내 메시지를 전달
    * 사용자가 적절하지 않은 입력시 경고 메시지 전달
    * 여러행으로 구성된 사다리(ladder) 문자열을 출력

---
+++ src/main/java/kr/codesquad/ConsoleOutput.java
@@ -0,0 +1,19 @@
+package kr.codesquad;
+
+public class ConsoleOutput {
+    public static void printInputPersonIntro(){
+        System.out.println("참여할 사람은 몇 명인가요?");
+    }
+
+    public static void printInputMaximumLadderHeightIntro(){
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+    }
+
+    public static void printInvalidInputNumber(String message){
+        System.out.println(message);
+    }
+
+    public static void printLadderBoard(String ladder){
+        System.out.println(ladder);
+    }
+}
