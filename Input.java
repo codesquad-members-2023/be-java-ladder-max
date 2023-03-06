04c7988566fbe2c4dd5f029b9ebdb33e06b3cc60
JIHYE <jdh324@naver.com>
2023-03-06 16:50:59 +0900

    입력 클래스 구현

---
+++ Input.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Input {
+    public static int inputPerson(BufferedReader br) throws IOException {
+        System.out.print("참여할 사람은 몇 명인가요?");
+        try {
+            return Integer.parseInt(br.readLine());
+        }catch (NumberFormatException e) {
+            System.out.println("정수가 아닙니다.");
+            return inputPerson(br);
+        }
+    }
+
+    public static int inputLine(BufferedReader br) throws IOException {
+        System.out.print("최대 사다리 높이는 몇 개인가요?");
+        try {
+            return Integer.parseInt(br.readLine());
+        }catch (NumberFormatException e) {
+            System.out.println("정수가 아닙니다.");
+            return inputLine(br);
+        }
+    }
+}
