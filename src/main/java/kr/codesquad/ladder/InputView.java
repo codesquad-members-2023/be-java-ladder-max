de141d4122b2f003e4cf0776ac0933e77cb8237e
jun <alap_u@naver.com>
2023-03-06 22:40:20 +0900

    플레이어 이름을 입력받는다.
    플레이어 이름을 입력받고 ","를 기준으로 구분한다.

---
+++ src/main/java/kr/codesquad/ladder/InputView.java
@@ -0,0 +1,15 @@
+package kr.codesquad.ladder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputView {
+
+    private final String NAME_SEPARATOR = ",";
+    private final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public String[] inputNames() throws IOException {
+        return bufferedReader.readLine().split(NAME_SEPARATOR);
+    }
+}
