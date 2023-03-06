13ca29d31924c6eb494a62838cab6d089b558404
Sully <2dcoder@naver.com>
2023-03-06 17:11:08 +0900

    test: LadderShapeTest 테스트 코드 생성
    테스트 메서드 틀까진 일단 만들어 뒀는데
    이제부터 어떡할까

---
+++ src/test/java/kr/codesquad/ladder/domain/LadderShapeTest.java
@@ -0,0 +1,25 @@
+package kr.codesquad.ladder.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LadderShapeTest {
+    @Test
+    public void minus_sign_가능_여부_체크() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    public void 최대_5글자_출력_여부_체크() {
+        // given
+
+        // when
+
+        // then
+    }
+}
