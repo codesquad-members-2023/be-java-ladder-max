bf158d08681f05093353984cc484c9601dddd186
jun <alap_u@naver.com>
2023-03-06 14:37:10 +0900

    사다리의 높이를 입력 받음
    BufferedReader를 이용해 높이를 입력받음 예외를 전파하지 않고 처리함.

--- src/main/java/kr/codesquad/ladder/InputView.java
+++ src/main/java/kr/codesquad/ladder/InputView.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 
 public class InputView {
 
+    private final String REQUEST_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private final String LADDER_HEIGHT_EXCEPTION_MESSAGE = "사다리의 높이는 숫자만 입력이 가능합니다.";
     private final String PARTICIPANT_COUNT_EXCEPTION_MESSAGE = "인원은 숫자만 입력이 가능합니다.";
     private final String REQUEST_PARTICIPANT_COUNT_MESSAGE = "참여할 사람은 몇 명인가요?";
 
@@ -18,6 +20,15 @@ public class InputView {
             return Integer.parseInt(bufferedReader.readLine());
         } catch (IOException e) {
             throw new RuntimeException(PARTICIPANT_COUNT_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public int inputLadderHeight() {
+        System.out.println(REQUEST_LADDER_HEIGHT_MESSAGE);
+        try {
+            return Integer.parseInt(bufferedReader.readLine());
+        } catch (IOException e) {
+            throw new RuntimeException(LADDER_HEIGHT_EXCEPTION_MESSAGE);
         }
     }
 
