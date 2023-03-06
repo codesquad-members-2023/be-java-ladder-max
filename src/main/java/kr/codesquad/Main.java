df7938118afe3fa70537a4564e1c62a6e1011871
crtEvent <crtevn@gmail.com>
2023-03-06 16:46:15 +0900

    add: 프로그램을 실행시키는 Main class 추가
     Main class에서 프로그램을 실행 시킨다

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,29 @@
 package kr.codesquad;
 
+import kr.codesquad.domain.Ladder;
+import kr.codesquad.view.OutputView;
+import kr.codesquad.view.InputView;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ladder ladder = new Ladder();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        while (true) {
+            try {
+                int playerCount = inputView.inputPlayerCount();
+                int ladderHeight = inputView.inputLadderHeight();
+                String[][] newLadder = ladder.makeLadder(playerCount, ladderHeight);
+                outputView.printLadder(newLadder);
+            } catch (NumberFormatException e) {
+                System.out.println("잘못된 값을 입력하였습니다. 다시 입력해 주세요.");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 }
