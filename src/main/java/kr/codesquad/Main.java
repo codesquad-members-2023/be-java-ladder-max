d721b039b4bc99424af7485e669fd2bfcdc2f244
jun <alap_u@naver.com>
2023-03-07 00:39:54 +0900

    사다리를 출력할때 이름도 함께 출력한다.

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,11 @@
 package kr.codesquad;
 
+import java.io.IOException;
+import kr.codesquad.ladder.LadderGame;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        LadderGame ladderGame = new LadderGame();
+        ladderGame.run();
     }
 }
--- src/main/java/kr/codesquad/ladder/LadderGame.java
+++ src/main/java/kr/codesquad/ladder/LadderGame.java
@@ -1,6 +1,20 @@
 package kr.codesquad.ladder;
 
+import static kr.codesquad.ladder.OutputView.printString;
+
+import java.io.IOException;
+
 public class LadderGame {
 
+    private final InputView inputView = new InputView();
+    private Ladder ladder;
 
+    public void run() throws IOException {
+        String[] names = inputView.inputNames();
+        int height = inputView.inputHeight();
+
+        ladder = Ladder.createLadder(height, names.length - 1);
+        printString(Names.createNames(names).toString());
+        printString(ladder.toString());
+    }
 }
