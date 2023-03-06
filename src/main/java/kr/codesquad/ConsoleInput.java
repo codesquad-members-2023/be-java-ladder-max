28be78d481598ff95d63383ed68dcd9acd8da18c
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-06 15:15:15 +0900

    feat: 콘솔을 이용한 사용자 입력 기능
    * ConsoleInput 객체 생성시 사람과 사다리 높이를 검증하는 validator 객체를 주입받습니다.
    * 잘못된 입력을 하게 되면 경모 메시지를 출력받고 다시 입력하게 됩니다.

---
+++ src/main/java/kr/codesquad/ConsoleInput.java
@@ -0,0 +1,51 @@
+package kr.codesquad;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleInput {
+    private final BufferedReader br;
+    private final LadderValidator validator;
+
+    public ConsoleInput(LadderValidator validator) {
+        this.br = new BufferedReader(new InputStreamReader(System.in));
+        this.validator = validator;
+    }
+
+    public int inputPerson(){
+        String text;
+        while(true){
+            try {
+                text = br.readLine();
+                if(!validator.validatePerson(text)){
+                    throw new InvalidPersonNumber();
+                }
+                break;
+            } catch (InvalidPersonNumber e) {
+                ConsoleOutput.printInvalidInputNumber(e.getMessage());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return Integer.parseInt(text);
+    }
+
+    public int inputMaximumLadderHeight(){
+        String text;
+        while(true){
+            try {
+                text = br.readLine();
+                if(!validator.validateLadderHeight(text)){
+                    throw new InvalidPersonNumber();
+                }
+                break;
+            } catch (InvalidMaximumLadderHeight e) {
+                ConsoleOutput.printInvalidInputNumber(e.getMessage());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return Integer.parseInt(text);
+    }
+}
