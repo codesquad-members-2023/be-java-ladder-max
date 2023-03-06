81350ab25453dbd3dd9d8d2bb4574c4d1454618f
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-03-06 15:43:27 +0900

    미션 1 구현 완료 했으나, 기존 src 폴더에서 작업 시 클래스를 찾을 수 없는 문제가 발생함. 해결 방법을 아직 찾지 못해서 일단 ladder 폴더를 만들어 작업

---
+++ ladder/src/Application.java
@@ -0,0 +1,9 @@
+public class Application {
+    public static void main(String[] args){
+        ladderController ladderController = new ladderController();
+        ladderController.getInput();
+        ladderController.createLadder();
+        ladderController.testPrintLadder();
+
+    }
+}
