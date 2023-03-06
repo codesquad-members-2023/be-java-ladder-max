7cdbb829ac00e79cd0a89f1fcf1cc0064aae6414
he2joo <hottestej6@gamil.com>
2023-03-06 19:00:09 +0900

    step2 구현 : 이중 for문 제거
    스트림을 사용해 이중for문을 제거했다. 또 메서드를 더 작은 단위로 나눴다.

--- src/main/java/kr/codesquad/Ladder.java
+++ src/main/java/kr/codesquad/Ladder.java
@@ -1,41 +1,44 @@
 package kr.codesquad;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Ladder {
-    public static void main(String[] args) {
 
-        int[] num = input();
-        char[][] chArr = makeLadder(num[1],num[0]);
-        printLadder(chArr);
-    }
-    static int[] input(){
-        Scanner scanner = new Scanner(System.in);
-        int[] num = new int[2];
-        System.out.println("참여할 사람은 몇 명인가요?");
-        num[0] = (scanner.nextInt()*2)-1;
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        num[1] = scanner.nextInt();
-        return num;
+    private int row;
+    private int column;
+    private Character[][] ladder;
+
+    Ladder(){}
+    Ladder(int row, int column){
+        this.row = row;
+        this.column = column;
+        ladder = new Character[row][column];
     }
 
-    static char[][] makeLadder(int row, int column){
-        char[][] ladder = new char[row][column];
-
+    private void makeLadder(){ // 랜덤으로 사다리 내부 만들기
+        makeBasic();
+        Random random = new Random();
         for (int i=0;i<row;i++){
-            Arrays.fill(ladder[i],'|');
-            for (int j=1;j<column;j+=2){
-                Random random = new Random();
-                ladder[i][j] = random.nextInt(2) == 0 ? '-' : ' ';
-            }
+            ladder[i] = Arrays.stream(ladder[i]).map((c -> c == '-' ? (random.nextInt(2) == 0 ? ' ' : '-') : c)).toArray(Character[]::new);
         }
-        return  ladder;
     }
-    static void printLadder(char[][] ladder){
-        for (char[] ch : ladder){
-            System.out.println(ch);
+    private void makeBasic(){ // 사다리 기본 틀 만들기
+        for (int i=0;i<row;i++){
+            IntStream intStream = IntStream.range(0,column);
+            Random random = new Random();
+            ladder[i] = intStream.mapToObj(k -> k % 2 == 0 ? '|' : '-').toArray(Character[]::new);
+        }
+    }
+    public void printLadder(){ // 사다리 출력하기
+        makeLadder();
+        for (Character[] ch : ladder){
+            String str = Arrays.toString(ch).replaceAll("\\[|\\]|,", "");
+            System.out.println(str);
         }
     }
 }
--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,17 @@
 package kr.codesquad;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("참여할 사람은 몇 명인가요?");
+        int c = (scanner.nextInt()*2)-1;
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        int r = scanner.nextInt();
+
+        Ladder la = new Ladder(r,c);
+        la.printLadder();
     }
 }
