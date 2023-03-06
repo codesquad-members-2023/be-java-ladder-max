8f18ccb0de793915963a94ee971597d0e301baf0
jwp345 <jwp135@naver.com>
2023-03-06 15:44:56 +0900

    1단계 구현

--- src/main/java/kr/codesquad/Ladder.java
+++ src/main/java/kr/codesquad/Ladder.java
@@ -1,17 +1,40 @@
 package kr.codesquad;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Ladder {
-    private final char[][] ladder;
-    private final Random random;
+    private final String[][] ladders;
+
+    private final int rows, cols;
 
     Ladder(int people, int length) {
-        this.ladder = new char[length][people - 1]; // 출력할 때 | 추가할거임
-        this.random = new Random();
+        this.ladders = new String[length][people - 1]; // 출력할 때 | 추가할거임
+        this.rows = length;
+        this.cols = people - 1;
+        makeLadder();
     }
 
     void makeLadder() {
+        Random random = new Random();
+        for(int idx1 = 0; idx1 < rows; idx1++) {
+            for(int idx2 = 0; idx2 < cols; idx2++) {
+                int curr = random.nextInt(2);
+                if(curr == 0) {
+                    ladders[idx1][idx2] = "-";
+                } else {
+                    ladders[idx1][idx2] = " ";
+                }
+            }
+        }
+    }
 
+    void printLadder() {
+        for(String[] ladder : ladders) {
+            System.out.print("|");
+            System.out.print(Arrays.stream(ladder)
+                    .reduce((o1, o2) -> o1 + "|" + o2).orElse(""));
+            System.out.println("|");
+        }
     }
 }
