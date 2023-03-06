a8bfc39e7a7ee7513cd33a68f82bfa6cb293199f
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-03-06 14:25:29 +0900

    메인 객체 +  입력 객체 생성 및 takeInput 메소드 구현

--- Ladder/Input.java
+++ Ladder/Input.java
@@ -1,11 +1,20 @@
+package Ladder;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Input {
 
-    public static void takeInput() {
-        printPeople();
+    public static void takeInput() throws IOException {
+        System.out.println("참여할 사람은 몇 명인가요?");
+        int pplNumber = takeInt();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        int ladderHeight = takeInt();
+    }
+
+    private static int takeInt() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int pplnumber = Integer.parseInt()
+        return Integer.parseInt(br.readLine());
     }
 }
