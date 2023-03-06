723839284382eddf54acdcb3d58d25b0408cfa4e
DOEKYONG <apochmn@naver.com>
2023-03-06 16:40:58 +0900

    step1구현 완료

---
+++ src/main/java/kr/codesquad/Input.java
@@ -0,0 +1,17 @@
+package kr.codesquad;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Input {
+
+    public int value() throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        return  Integer.parseInt(bufferedReader.readLine());
+    }
+
+}
--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,29 @@
 package kr.codesquad;
 
+import java.io.IOException;
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        Input input = new Input();
+        System.out.println("참여할 사람은 몇 명인가요?");
+        int width = input.value();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        int length = input.value();
+        Ladder ladder = new Ladder(width,length);
+        String[][] arr = ladder.createBasicArr();;
+        ladder.printGame(arr);
+
+//        for (int i = 0; i < arr.length; i++) {
+//            String[] inArr = arr[i];
+//            for (int j = 0; j < inArr.length; j++) {
+//                System.out.print(inArr[j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+
     }
 }
