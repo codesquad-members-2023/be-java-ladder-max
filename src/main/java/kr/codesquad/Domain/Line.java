e3479eacc8eef886ac576b29788dc4ac40855357
JJONSOO <pjs970616@naver.com>
2023-03-06 17:18:43 +0900

    feat : 라인이 겹치치 않도록 로직 추가 및 출력 형태 변환

--- src/main/java/kr/codesquad/Domain/Line.java
+++ src/main/java/kr/codesquad/Domain/Line.java
@@ -1,22 +1,21 @@
 package kr.codesquad.Domain;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Line {
     private List<Boolean> points;
     public Line(Integer width){
-        this.points = Stream.generate(() -> new Random().nextBoolean())
-                .limit(width)
-                .collect(Collectors.toList());
+        this.points = Stream.concat(Stream.generate(() -> new Random().nextBoolean())
+                .limit(width-1), Stream.of(false)).collect(Collectors.toList());
+        Collections.shuffle(points);
     }
 
     @Override
     public String toString() {
         String lineString = points.stream()
-                .map(i -> i ? "-|" : " |")
+                .map(i -> i ? "----|" : "    |")
                 .collect(Collectors.joining(""));
         return "|" + lineString;
     }
