3f1a184bf44039906a0231b6f06f44ab4cedd4ee
jun <alap_u@naver.com>
2023-03-06 22:51:47 +0900

    플레이어의 이름은 5글자 이하로 제한한다.

---
+++ src/main/java/kr/codesquad/ladder/Name.java
@@ -0,0 +1,19 @@
+package kr.codesquad.ladder;
+
+public class Name {
+
+    private final String NAME_LENGTH_EXCEPTION_MESSAGE = "이름은 5글자를 넘을 수 없습니다.";
+    private final String name;
+
+    public Name(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    public void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(NAME_LENGTH_EXCEPTION_MESSAGE);
+        }
+    }
+
+}
