89f72bca202e0b9ff2676ce794200640d4fec676
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-06 16:47:24 +0900

    Feat($Ladder.java): Ladder 클래스 생성
    생성자에서 Players와 height를 입력받아 생성합니다.
    생성자에 내장되어 있는 buildRungs는 무작위 숫자를 생성하고 그 숫자에
    따라서 rungs를 생성합니다.

---
+++ src/main/java/kr/codesquad/Ladder.java
@@ -0,0 +1,29 @@
+package kr.codesquad;
+
+public class Ladder {
+    private final int players;
+    private final int height;
+    private boolean[][] rungs;
+
+    Ladder(int players, int height) {
+        this.players = players;
+        this.height = height;
+        this.rungs = new boolean[height][players - 1];
+        buildRungs();
+    }
+
+    private void buildRungs() {
+        for(int row = 0; row < this.rungs.length; row++) {
+            for(int column = 0; column < this.rungs[row].length; column++) {
+                double random = Math.random();
+
+                if (random >= 0.5) {
+                    this.rungs[row][column] = true;
+                } else {
+                    this.rungs[row][column] = false;
+                }
+
+            }
+        }
+    }
+}
