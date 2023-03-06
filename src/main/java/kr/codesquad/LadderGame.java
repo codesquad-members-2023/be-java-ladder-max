519d407b3b9d811d8e1340be637a6ec6d7731df7
code-tree <dltjdqls1200@naver.com>
2023-03-06 15:16:04 +0900

    [feat]: 사다리 높이 입력받는 기능 추가

--- src/main/java/kr/codesquad/LadderGame.java
+++ src/main/java/kr/codesquad/LadderGame.java
@@ -1,14 +1,18 @@
 package kr.codesquad;
 
+import java.util.Scanner;
+
 public class LadderGame {
     private final int playerNumber;
+    private final int ladderHeight;
 
-    private LadderGame(int playerNumber) {
+    private LadderGame(int playerNumber, int ladderHeight) {
         this.playerNumber = playerNumber;
+        this.ladderHeight = ladderHeight;
     }
 
     public LadderGame() {
-        this(Screen.inputPlayerNumber());
+        this(Screen.inputPlayerNumber(), Screen.inputLadderHeight());
     }
 
 }
--- src/main/java/kr/codesquad/Screen.java
+++ src/main/java/kr/codesquad/Screen.java
@@ -3,10 +3,16 @@ package kr.codesquad;
 import java.util.Scanner;
 
 public class Screen {
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputPlayerNumber() {
         System.out.println("참여할 사람은 몇 명인가요?");
+
+        return scanner.nextInt();
+    }
+
+    public static int inputLadderHeight() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
 
         return scanner.nextInt();
     }
