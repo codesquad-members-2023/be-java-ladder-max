a3c699d9eb81e0fabf92faee17d0c7258c411673
crtEvent <crtevn@gmail.com>
2023-03-06 23:52:22 +0900

    refactor: OutputView for문을 개선된 for문으로 변경
    가독성 향상을 위함

--- src/main/java/kr/codesquad/view/OutputView.java
+++ src/main/java/kr/codesquad/view/OutputView.java
@@ -3,14 +3,14 @@ package kr.codesquad.view;
 public class OutputView {
 
     public void printLadder(String[][] ladder) {
-        for(int row = 0; row < ladder.length; row++) {
-            printLadderRow(ladder[row]);
+        for(String[] ladderRow : ladder) {
+            printLadderRow(ladderRow);
         }
     }
 
     private void printLadderRow(String[] ladderRow) {
-        for (int col = 0; col < ladderRow.length; col++) {
-            System.out.print(ladderRow[col] == null? " " : ladderRow[col]);
+        for(String cell : ladderRow) {
+            System.out.print(cell == null? " " : cell);
         }
         System.out.println();
     }
