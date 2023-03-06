2d92538fe011b8f7b9f6fb0f56705bba1b30c845
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-06 15:40:09 +0900

    feat: 문자열 2차원 배열을 문자열로 변환하는 기능
    * 이차원 배열에 저장되어 있는 사다리 문자들을 하나의 문자열로 변환하는 기능입니다.

---
+++ src/main/java/kr/codesquad/LadderConverter.java
@@ -0,0 +1,17 @@
+package kr.codesquad;
+
+public class LadderConverter {
+    private final String[][] board;
+
+    public LadderConverter(String[][] board) {
+        this.board = board;
+    }
+
+    public String convertToString(){
+        StringBuilder result = new StringBuilder();
+        for(String[] row : board){
+            result.append(String.join("", row)).append("\n");
+        }
+        return result.toString();
+    }
+}
