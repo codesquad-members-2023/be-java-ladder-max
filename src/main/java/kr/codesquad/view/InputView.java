8ff2f57856410e7f1fa7c14df9a654520aa8bc38
crtEvent <crtevn@gmail.com>
2023-03-06 16:45:30 +0900

    feat: 사용자로 부터 플레이서 수, 사다리 높이를 입력받는 기능 추가
    InputView.inputPlayerCount()에서 플레이서 수 입력 받기
    InputView.inputLadderHeight()에서 사다리 높이 입력 받기

--- src/main/java/kr/codesquad/view/InputView.java
+++ src/main/java/kr/codesquad/view/InputView.java
@@ -1,6 +1,18 @@
-package main.java.kr.codesquad.view;
+package kr.codesquad.view;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class InputView {
 
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    public int inputPlayerCount() throws IOException, NumberFormatException {
+        return Integer.parseInt(br.readLine());
+    }
+
+    public int inputLadderHeight() throws IOException, NumberFormatException {
+        return Integer.parseInt(br.readLine());
+    }
 }
