5dc14c6326a4ac36f50c683c448a11b334fecf0c
code-tree <dltjdqls1200@naver.com>
2023-03-06 15:39:21 +0900

    [feat]: 사다리 문자를 나타내는 LadderPart Enum 클래스 생성

---
+++ src/main/java/kr/codesquad/LadderPart.java
@@ -0,0 +1,26 @@
+package kr.codesquad;
+
+public enum LadderPart {
+    POLES('|'),
+    CROSSBAR('-'),
+    NONE(' ');
+
+    private final char shape;
+
+    LadderPart(char shape) {
+        this.shape = shape;
+    }
+
+    public static LadderPart create(int width) {
+        if (width % 2 == 0) {
+            return POLES;
+        }
+
+        
+        if (Math.random() > 0.6) {
+            return CROSSBAR;
+        }
+
+        return NONE;
+    }
+}
