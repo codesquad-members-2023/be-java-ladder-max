b5982abb830199b1014a86650b3541cfc96e560d
chung <chunghye1998>
2023-03-06 16:04:46 +0900

    feat: Input 클래스 생성 - inputN(), inputM() 구현

--- src/main/java/kr/codesquad/Application.java
+++ src/main/java/kr/codesquad/Application.java
@@ -1,7 +1,9 @@
 package kr.codesquad;
 
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Controller controller = new Controller();
         controller.run();
     }
--- src/main/java/kr/codesquad/Controller.java
+++ src/main/java/kr/codesquad/Controller.java
@@ -1,11 +1,16 @@
 package kr.codesquad;
 
+import java.io.IOException;
+
 public class Controller {
 
     private Output output;
+    private Input input;
 
-    public void run() {
+    public void run() throws IOException {
         output.printMessageN();
+        String n = input.inputN();
         output.printMessageM();
+        String m = input.inputM();
     }
 }
