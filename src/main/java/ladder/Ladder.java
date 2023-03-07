c1ce766a0843bc2387db15a59fc68bef2dbc5eb0
yeon <cerealnum@gmail.com>
2023-03-07 17:52:49 +0900

    feat : 2차원 배열 ladders를 List로 변경
    기존의 boolean[][] ladders를 List<List<Boolean>> ladders로 변경합니다.

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -1,27 +1,32 @@
 package ladder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ladder {
     private final int space;
     private final int height;
-    private final boolean[][] ladders;
+    private final List<List<Boolean>> ladders;
 
     public Ladder(int space, int height) {
         this.space = space;
         this.height = height;
-        this.ladders = new boolean[this.height][this.space];
+        this.ladders = new ArrayList<>();
         this.makeRandomLadder();
     }
 
     private void makeRandomLadder() {
         for (int i = 0; i < height; i++) {
-            makeRandomRung(i);
+            ladders.add(makeRandomRung());
         }
     }
 
-    private void makeRandomRung(int height) {
+    private List<Boolean> makeRandomRung() {
+        List<Boolean> hasRungs = new ArrayList<>();
         for (int i = 0; i < space; i++) {
-            ladders[height][i] = getRandomBoolean();
+            hasRungs.add(getRandomBoolean());
         }
+        return hasRungs;
     }
 
     private boolean getRandomBoolean() {
@@ -33,16 +38,16 @@ public class Ladder {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < height; i++) {
             builder.append(" ".repeat(2));
-            drawingSameHeight(builder, i);
+            drawingSameHeight(builder, ladders.get(i));
             builder.append(System.lineSeparator());
         }
         System.out.println(builder);
     }
 
-    private void drawingSameHeight(StringBuilder builder, int height) {
+    private void drawingSameHeight(StringBuilder builder, List<Boolean> spaces) {
         for (int j = 0; j < space; j++) {
             drawLeftLine(builder, j);
-            builder.append(ladders[height][j] ? "-----" : "     ");
+            builder.append(spaces.get(j) ? "-----" : "     ");
             drawRightLine(builder);
         }
     }
