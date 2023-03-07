af4edb8aa2de3754b8a93b8cfb273682c8367f5d
pinetree <eodnjs568@gmail.com>
2023-03-07 17:15:01 +0900

    Feat "Application 내 Inputview.close() 메서드 추가"
    InputView 내 추가된 close() 메서드를 Application 내 실행

--- src/main/java/kr/codesquad/Application.java
+++ src/main/java/kr/codesquad/Application.java
@@ -4,6 +4,7 @@ public class Application {
     public static void main(String[] args) {
         Ladder ladder = new Ladder(InputView.inputPeopleNumber(), InputView.inputLadderHeight());
         Outputview.printLadder(ladder.makeLadder());
+        InputView.close();
     }
 }
 
