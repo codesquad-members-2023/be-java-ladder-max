ca394676e517ef9639a62b858f41c8248a947361
pinetree <eodnjs568@gmail.com>
2023-03-07 17:06:41 +0900

    fix "Application(main 메서드가 있는 클래스) 수정"
    Ladder, Inputview, OutputView를 통해 실행만 하는 클래스로 변경

--- src/main/java/kr/codesquad/Application.java
+++ src/main/java/kr/codesquad/Application.java
@@ -2,14 +2,8 @@ package kr.codesquad;
 
 public class Application {
     public static void main(String[] args) {
-        int peopleNum = Integer.parseInt(Outputview.printPeopleFormat());
-        int ladderCnt = Integer.parseInt(Outputview.printLadderHeightFormat());
-
-        Ladder ladder = new Ladder(peopleNum, ladderCnt);
-        ladder.makeBasicLadder();
-        ladder.makeRandomLadderLine();
-        ladder.showFinalLadder();
-
+        Ladder ladder = new Ladder(InputView.inputPeopleNumber(), InputView.inputLadderHeight());
+        Outputview.printLadder(ladder.makeLadder());
     }
 }
 
