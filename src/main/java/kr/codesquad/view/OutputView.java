9392b75c5f37877a1b3130d467915e2bfe6cfab5
jinny-l <jinny.l.dev@gmail.com>
2023-03-07 19:15:20 +0900

    :recycle:refactor: 매겨변수 타입 변경
    * 이유:
    List<Player>를 Players 클래스로 래핍하여 플레이어 이름을 String 타입으로 반환하도록 변경

--- src/main/java/kr/codesquad/view/OutputView.java
+++ src/main/java/kr/codesquad/view/OutputView.java
@@ -6,7 +6,7 @@ import kr.codesquad.domain.Player;
 
 public class OutputView {
 
-    public static void printLadder(List<Player> players, List<List<String>> ladder) {
+    public static void printLadder(List<String> players, List<List<String>> ladder) {
         System.out.println("\n사다리 결과: \n");
         String result = ladder.stream()
                 .map(oneLine -> String.join("", oneLine))
@@ -15,7 +15,7 @@ public class OutputView {
         System.out.println(result);
     }
 
-    private static void printPlayers(List<Player> players) {
+    private static void printPlayers(List<String> players) {
         String oneLine = players.stream()
                 .map(player -> String.format("%-6s", player))
                 .collect(Collectors.joining());
