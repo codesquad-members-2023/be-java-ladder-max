fec63a698611bf735a9407139487aeb48e435f9e
sudago <sudago22@gmail.com>
2023-03-07 20:51:56 +0900

    상수 생성
    사람 수와 사다리 높이를 묻는 질문을 상수처리 했다.
    상수의 이름을 어떻게 명사로 지을지 고민이었다.

--- src/mission/view/InputView.java
+++ src/mission/view/InputView.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class InputView {
+    public static final String ASKING_NUMBER_OF_PEOPLE  = "참여할 사람은 몇 명인가요?";
+    public static final String ASKING_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
     BufferedReader br;
 
     public InputView(){
@@ -14,12 +16,12 @@ public class InputView {
     }
 
     public int getPeopleNumber() throws IOException {
-        System.out.println("참여할 사람은 몇 명인가요?");
+        System.out.println(ASKING_NUMBER_OF_PEOPLE);
         return Integer.parseInt(br.readLine());
     }
 
     public int getLadderHeight() throws IOException{
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        System.out.println(ASKING_LADDER_HEIGHT);
         return Integer.parseInt(br.readLine());
     }
 }
