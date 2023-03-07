a706c6ee69f7133aa7966557f8b7381b5587b3f8
pinetree <eodnjs568@gmail.com>
2023-03-07 16:09:54 +0900

    fix "OutputView 클래스 역할에 맞게 수정"
    Outputview 클래스 이름에 걸맞는메서드 작성

--- src/main/java/kr/codesquad/Outputview.java
+++ src/main/java/kr/codesquad/Outputview.java
@@ -1,13 +1,14 @@
 package kr.codesquad;
 
 public class Outputview {
-    public static String printPeopleFormat() {
-        System.out.println("참여할 사람은 몇 명인가요?");
-        return InputView.inputNumber();
+    public static void printPeopleFormat(String[][] ladder) {
+        for (int row = 0; row < ladder.length; row++) {
+            for (int column = 0; column < ladder[row].length; column++) {
+                System.out.print(ladder[row][column]);
+            }
+            System.out.println();
+        }
     }
 
-    public static String printLadderHeightFormat() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        return InputView.inputNumber();
-    }
+
 }
