c13b68951d1b7b565c26a7f215d6910506498d95
road-jin <rjswmtk@gmail.com>
2023-03-07 12:20:56 +0900

    refactor: 사다리 생성시 선언하고 초기화작업 과정을 하나로 합치고 파일에 EOL 추가
    Ladder.generateLadder 메서드
    1. 메서드  이름에 맞게 Boolean 2차원 배열을 리턴하도록 변경하고, for문을 stream 변경
    Ladder.ramdomBooleans 메서드
    1. 메서드 이름을 동사로 변경 -> getRandomBooleans
    2. 이 메서드가 호출시 Random 클래스를 계속 생성하여 낭비라고 생각하여 매개변수에서 받을 수 있게 변경
    Main, Ladder 클래스
    1. 파일에 EOL 추가

--- src/main/java/kr/codesquad/domain/Ladder.java
+++ src/main/java/kr/codesquad/domain/Ladder.java
@@ -8,18 +8,17 @@ public class Ladder {
     private final Boolean[][] ladder;
 
     public Ladder(int numberOfUsers, int ladderHeight) {
-        this.ladder = new Boolean[numberOfUsers][ladderHeight];
-        generateLadder();
+        this.ladder = generateLadder(numberOfUsers, ladderHeight);
     }
 
-    private void generateLadder() {
-        for (int i = 0; i < this.ladder.length - 1; i++) {
-            this.ladder[i] = randomBooleans(this.ladder[i].length);
-        }
-    }
-
-    private Boolean[] randomBooleans(int length) {
+    private Boolean[][] generateLadder(int numberOfUsers, int ladderHeight) {
         Random random = new Random();
+        return IntStream.range(0, ladderHeight)
+            .mapToObj(n -> getRandomBooleans(random, numberOfUsers - 1))
+            .toArray(Boolean[][]::new);
+    }
+
+    private Boolean[] getRandomBooleans(Random random, int length) {
         return IntStream.range(0, length)
             .mapToObj(n -> random.nextBoolean())
             .toArray(Boolean[]::new);
