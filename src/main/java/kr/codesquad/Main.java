47cbccb56bbfdd2a4756cf5ea9ffa84333a7ef4a
JIHYE <jdh324@naver.com>
2023-03-07 14:50:53 +0900

    메인 클래스 구현

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,15 @@
 package kr.codesquad;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("참여할 사람은 몇 명인가요?");
+        int numPerson = Input.input();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        int numLine = Input.input();
+
+        Ladder ladder = new Ladder(numPerson, numLine);
+        Output.printLadder(ladder);
     }
 }
