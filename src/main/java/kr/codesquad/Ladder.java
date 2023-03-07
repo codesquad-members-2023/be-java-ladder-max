d7ae7391314e90c8095e669dea830612445eaa4d
codemojae <116280283+codemojae@users.noreply.github.com>
2023-03-07 17:31:48 +0900

    LadderGame 클래스 생성, Ladder에 있던 메소드 분리
    Ladder 클래스에 사다리 정보를 저장하기 위해서 메소드의 분리가 필요했다.

--- src/main/java/kr/codesquad/Ladder.java
+++ src/main/java/kr/codesquad/Ladder.java
@@ -1,34 +1,14 @@
 package kr.codesquad;
 
 public class Ladder {
-    private final Output output = new Output();
-    private final Input input = new Input();
 
-    public void start() {
-        int[] userInput = getInput();
+    private String[][] ladder;
 
-        String[][] ladder = makeLadderShape(userInput);
-
-        output.printLadder(ladder);
-    }
-
-    private int[] getInput() {
-        output.printNumberOfPeople();
-        int people = Integer.parseInt(input.getInput());
-
-        output.printHeightOfLadder();
-        int height = Integer.parseInt(input.getInput());
-
-        return new int[]{people, height};
-    }
-
-    private String[][] makeLadderShape(int[] userInput) {
-        String[][] customLadder = new String[userInput[1]][userInput[0] * 2 - 1];
-        for (int i = 0; i < customLadder.length; i++){
-            fillLadderLine(customLadder[i]);
+    public void makeLadderShape(int[] userInput) {
+        ladder = new String[userInput[1]][userInput[0] * 2 - 1];
+        for (int i = 0; i < ladder.length; i++){
+            fillLadderLine(ladder[i]);
         }
-
-        return customLadder;
     }
 
     private void fillLadderLine(String[] customLadder) {
@@ -44,5 +24,9 @@ public class Ladder {
         double rand = Math.random();
         String result = (rand <= 0.5) ? " " : "-";
         return result;
+    }
+
+    public String[][] getLadder() {
+        return this.ladder;
     }
 }
--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,10 +1,8 @@
 package kr.codesquad;
 
-import kr.codesquad.Ladder;
-
 public class Main {
     public static void main(String[] args) {
-        Ladder ladder = new Ladder();
-        ladder.start();
+        LadderGame ladderGame = new LadderGame();
+        ladderGame.start();
     }
 }
