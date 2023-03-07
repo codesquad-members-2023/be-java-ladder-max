86d8432a1bb773f59f8615dbe1c212b958c27456
pinetree <eodnjs568@gmail.com>
2023-03-07 16:34:15 +0900

    fix "Ladder 클래스 내 매개변수명, 메서드 이름 변경"
    1. 생성자의 매개변수 n,m -> peopleNumber, ladderHeight 명시
    2. 2차원 배열을 소유하고 있는 하나의 메서드(makeLadder()) 내에서
       makeBasicPeopleLine을 통해 사다리 라인이 생성되지 않게끔 만들어주고
       makeRandomLadderLine을 통해 랜덤으로 사다리 라인이 생성되게 만들어줌
    3. 또한 모든 메서드의 매개변수를 row, column을 통일

--- src/main/java/kr/codesquad/Ladder.java
+++ src/main/java/kr/codesquad/Ladder.java
@@ -5,36 +5,41 @@ import java.util.Random;
 public class Ladder {
     private String[][] laddder;
 
-    Ladder(int n, int m) {
-        this.laddder = new String[m][2 * n - 1];
-
+    Ladder(int peoleNumber, int ladderHeight) {
+        this.laddder = new String[ladderHeight][2 * peoleNumber - 1];
     }
 
-    void makeBasicLadder() {
-        for (int i = 0; i < laddder.length; i++) {
-            for (int j = 0; j < laddder[i].length; j++) {
-                extracted(i, j);
+    public String[][] makeLadder() {
+        for (int row = 0; row < laddder.length; row++) {
+            for (int column = 0; column < laddder[row].length; column++) {
+                makeBasicPeopleLine(row, column);
+                makeRandomLadderLine(row,column);
             }
         }
-    }
-
-    private void extracted(int i, int j) {
-        if (j % 2 == 0) {
-            this.laddder[i][j] = "|";
-        } else this.laddder[i][j] = " ";
+        return this.laddder;
     }
 
 
-    void makeRandomLadderLine() {
+    private void makeBasicPeopleLine(int row, int column) {
+        if (column % 2 == 0) {
+            this.laddder[row][column] = "|";
+        } this.laddder[row][column] = " ";
+    }
 
-        for (int i = 0; i < laddder.length; i++) {
-            for (int j = 0; j < laddder[i].length; j++) {
-                Random random = new Random();
-                boolean randomBoolean = random.nextBoolean();
-                if (randomBoolean && j % 2 == 1) {
-                    laddder[i][j] = "-";
-                }
-            }
+
+//    public void makeRandomLadderLine() {
+//
+//        for (int i = 0; i < laddder.length; i++) {
+//            for (int j = 0; j < laddder[i].length; j++) {
+//                makeRandomLadderLine(i, j);
+//            }
+//        }
+//    }
+
+    private void makeRandomLadderLine(int row, int column) {
+        Random random = new Random();
+        if (random.nextBoolean() && column % 2 == 1) {
+            laddder[row][column] = "-";
         }
     }
 
