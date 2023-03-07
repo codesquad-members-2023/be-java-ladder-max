5006b95e95cb1a0deb950a559631a1b36e064c5e
ALbertIM0427 <guang7226961@naver.com>
2023-03-07 21:05:10 +0900

    실행 결과를 저장
    Map을 사용해서 실행과 결과를 저장
    - 결과를 어떻게 코드를 최소한 수정을 하고 구현을 하기 위하여 생각을 많이 했다.

--- src/main/java/kr/codesquad/domain/LadderByNames.java
+++ src/main/java/kr/codesquad/domain/LadderByNames.java
@@ -1,5 +1,7 @@
 package kr.codesquad.domain;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class LadderByNames implements Ladder {
@@ -9,22 +11,53 @@ public class LadderByNames implements Ladder {
     static final String PEOPLE_DELIMITER = "|";
     static final String NEXT_LINE = "\n";
     static final String PREFIX = "   ";
+    private final Map<Integer, Integer> usersOfResult = new HashMap<>();
 
     @Override
     public String draw(int peopleCount, int ladderHeight) {
         StringBuilder result = new StringBuilder();
+        boolean[][] ladderExistStateArray = new boolean[ladderHeight][peopleCount - 1];
         for (int i = 0; i < ladderHeight; i++) {
-            connectLines(peopleCount, result);
+            connectLines(peopleCount, result, ladderExistStateArray[i]);
         }
+        moveAndSaveUsers(ladderExistStateArray);
         return result.toString();
     }
 
-    private static void connectLines(int peopleCount, StringBuilder result) {
+    private void moveAndSaveUsers(boolean[][] ladderExistStateArray) {
+        int columnLength = ladderExistStateArray[0].length;
+        for (int j = 0; j < columnLength + 1; j++) {
+            moveAndSaveUser(ladderExistStateArray, columnLength, j);
+        }
+    }
+
+    private void moveAndSaveUser(boolean[][] ladderExistStateArray, int columnLength, int position) {
+        int resultNum = position;
+        for (boolean[] aBoolean : ladderExistStateArray) {
+            if (canMoveLeft(resultNum, aBoolean)) {
+                resultNum = resultNum - 1;
+            } else if (canMoveRight(columnLength, resultNum, aBoolean)) {
+                resultNum = resultNum + 1;
+            }
+        }
+        usersOfResult.put(position, resultNum);
+    }
+
+    private static boolean canMoveRight(int columnLength, int resultNum, boolean[] aBoolean) {
+        return resultNum < columnLength && aBoolean[resultNum];
+    }
+
+    private static boolean canMoveLeft(int resultNum, boolean[] aBoolean) {
+        return resultNum > 0 && aBoolean[resultNum - 1];
+    }
+
+    private static void connectLines(int peopleCount, StringBuilder result, boolean[] aBoolean) {
         boolean possible = true;
         result.append(PREFIX)
             .append(PEOPLE_DELIMITER);
         for (int j = 0; j < peopleCount - 1; j++) {
             possible = connectLine(result, possible);
+            aBoolean[j] = !possible;
         }
         result.append(NEXT_LINE);
     }
