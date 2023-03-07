741a304be3cc0104633e2d8ce79fe4975471d185
jun <alap_u@naver.com>
2023-03-07 17:09:31 +0900

    동등성 검증을 위한 메서드 재정의

--- src/main/java/kr/codesquad/ladder/domain/Name.java
+++ src/main/java/kr/codesquad/ladder/domain/Name.java
@@ -1,5 +1,7 @@
 package kr.codesquad.ladder.domain;
 
+import java.util.Objects;
+
 public class Name {
 
     private final String NAME_LENGTH_EXCEPTION_MESSAGE = "이름은 5글자를 넘을 수 없습니다.";
@@ -21,4 +23,20 @@ public class Name {
         return name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
