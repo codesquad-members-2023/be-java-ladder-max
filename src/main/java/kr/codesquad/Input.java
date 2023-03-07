c5a92c5ff9c631491cbe0d18a88f09b8ea2c65f4
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-07 16:15:06 +0900

    Chore: .idea의 파일들 추가
    브랜치를 바꾸려고 하다 보니 되지않아 보내고 바꿔볼려고 합니다.

--- src/main/java/kr/codesquad/Input.java
+++ src/main/java/kr/codesquad/Input.java
@@ -7,7 +7,7 @@ public class Input {
     private static final String INPUT_HEIGHT_MESSAGE = "얼마나 길게 하려고?";
 
     public int inputPlayers() {
-        System.out.println(INPUT_PLAYERS_MESSAGE);;
+        System.out.println(INPUT_PLAYERS_MESSAGE);
         Scanner scanner = new Scanner(System.in);
         int players = scanner.nextInt();
 
