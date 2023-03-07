d3856ba21d91c3af5706a80da8d2075941997991
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-08 00:13:38 +0900

    modify: 기존 랜덤한 사다리를 생성하는 기능을 LadderPartGenerator 객체에게 이전함
    * 사다리는 인원수와 최대사다리 높이라는 상태를 가진 객체로써 랜덤한 사다리를 생성하는 기능은 LadderPartGenerator 객체가 하는 것이 더 적절하다고 생각하여 사다리(Ladder) 객체를 도메인 객체로써 수정하였습니다.
    * 사다리 객체는 자신의 상태인 인원수와 사다리최대높이를 이용하여 인원수와 높이에 따른 2차원 문자열 배열을 생성하는 기능을 가지고 있습니다.

--- src/main/java/kr/codesquad/Ladder.java
+++ src/main/java/kr/codesquad/Ladder.java
@@ -1,61 +1,18 @@
 package kr.codesquad;
 
-import java.util.Random;
-
 public class Ladder {
 
-    private static final String LINE = "-";
-    private static final String EMPTY = " ";
-    private static final String BAR = "|";
+    private final int numberOfPeople;
+    private final int maximumLadderHeight;
 
-    private final int person;
-    private final int maximumHeight;
-    private final Random randomCreateLine;
-
-    public Ladder(int person, int maximumHeight) {
-        this.person = person;
-        this.maximumHeight = maximumHeight;
-        this.randomCreateLine = new Random();
+    public Ladder(int numberOfPeople, int maximumLadderHeight) {
+        this.numberOfPeople = numberOfPeople;
+        this.maximumLadderHeight = maximumLadderHeight;
     }
 
-    public String[][] generate() {
-        int height = maximumHeight;
-        int width = calcLadderWidth();
-        String[][] board = new String[height][width];
-        initLadderBar(board);
-        initLadderLine(board);
-        return board;
-    }
-
-    private int calcLadderWidth() {
-        return person * 2 - 1;
-    }
-
-    private void initLadderBar(String[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = isBarPosition(j) ? BAR : EMPTY;
-            }
-        }
-    }
-
-    private boolean isBarPosition(int n) {
-        return n % 2 == 0;
-    }
-
-    private void initLadderLine(String[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = isLinePosition(j) && isLineNeeded() ? LINE : board[i][j];
-            }
-        }
-    }
-
-    private boolean isLinePosition(int n) {
-        return n % 2 == 1;
-    }
-
-    private boolean isLineNeeded() {
-        return randomCreateLine.nextBoolean();
+    public String[][] createLadderBoard() {
+        int height = maximumLadderHeight;
+        int width = numberOfPeople * 2 - 1;
+        return new String[height][width];
     }
 }
