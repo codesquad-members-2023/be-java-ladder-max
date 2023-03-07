cffdc17c129fc6bd6303991c991346e1e2ce5453
jinny-l <jinny.l.dev@gmail.com>
2023-03-07 18:16:20 +0900

    :sparkles:feat: 사다리 출력 시 이름도 같이 출력하는 기능 구현
    * 참고
      - 이름 중앙 정렬은 못함
      - 이름을 출력함에 따라 사다리 길이 조정

--- src/main/java/kr/codesquad/config/LadderConfig.java
+++ src/main/java/kr/codesquad/config/LadderConfig.java
@@ -1,10 +1,12 @@
 package kr.codesquad.config;
 
+import kr.codesquad.domain.Player;
+
 public enum LadderConfig {
 
     AFFIX(-1, "|"), // TODO: enum에서 숫자를 비워두는 방법...이 있나?
-    EMPTY(0, " "),
-    LADDER(1, "-");
+    EMPTY(0, " ".repeat(Player.NAME_MAX_LENGTH)),
+    LADDER(1, "-".repeat(Player.NAME_MAX_LENGTH));
 
     private final int number;
     private final String expression;
--- src/main/java/kr/codesquad/view/OutputView.java
+++ src/main/java/kr/codesquad/view/OutputView.java
@@ -2,13 +2,23 @@ package kr.codesquad.view;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import kr.codesquad.domain.Player;
 
 public class OutputView {
 
-    public static void printLadder(List<List<String>> ladder) {
+    public static void printLadder(List<Player> players, List<List<String>> ladder) {
+        System.out.println("\n사다리 결과: \n");
         String result = ladder.stream()
                 .map(oneLine -> String.join("", oneLine))
                 .collect(Collectors.joining("\n"));
+        printPlayers(players);
         System.out.println(result);
+    }
+
+    private static void printPlayers(List<Player> players) {
+        String oneLine = players.stream()
+                .map(player -> String.format("%-6s", player))
+                .collect(Collectors.joining());
+        System.out.println(oneLine);
     }
 }
