06483bc9eee938d69cb882eee474d0f97553b6f0
pinetree <eodnjs568@gmail.com>
2023-03-07 16:04:41 +0900

    fix "InputView 수정"
    클래스 이름에 맞춰 InputView 내에선 Input역할만 하게끔 클래스 수정.
    사람 숫자를 받는 inputPeopleNumber 메서드와
    사다리높이를 받는 inputLadderHeight 메서드로 분리

--- src/main/java/kr/codesquad/InputView.java
+++ src/main/java/kr/codesquad/InputView.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 public class InputView {
     public static final Scanner scanner = new Scanner(System.in);
 
-    public static String inputNumber(){
-        return scanner.nextLine();
+
+    public static int inputPeopleNumber() {
+        System.out.println("참여할 사람은 몇 명인가요?");
+        return Integer.valueOf(scanner.nextLine());
     }
 
-
+    public static int inputLadderHeight() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        return Integer.valueOf(scanner.nextLine());
+    }
 
 }
