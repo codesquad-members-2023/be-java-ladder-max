98c4b5a115c16d6fcaad658fd04d4863466d3a44
codemojae <116280283+codemojae@users.noreply.github.com>
2023-03-07 13:28:40 +0900

    실행 초반 입력을 받기위한 출력양식 작성

---
+++ src/main/java/kr/codesquad/Main.java
@@ -0,0 +1,6 @@
+package kr.codesquad;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/kr/codesquad/Output.java
@@ -0,0 +1,11 @@
+package kr.codesquad;
+
+public class Output {
+    public void printNumberOfPeople () {
+        System.out.println("참여하는 사람은 몇명?");
+    }
+
+    public void printHeightOfLadder () {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+    }
+}
