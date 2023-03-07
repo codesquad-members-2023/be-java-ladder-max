88f895075f72ff58cea4504030c7aa1d7188697f
pinetree <eodnjs568@gmail.com>
2023-03-07 16:11:23 +0900

    fix "Ouputview 내 메서드 이름 리팩토링"
    printLadder 메서드로 이름 변경

--- src/main/java/kr/codesquad/Outputview.java
+++ src/main/java/kr/codesquad/Outputview.java
@@ -1,7 +1,7 @@
 package kr.codesquad;
 
 public class Outputview {
-    public static void printPeopleFormat(String[][] ladder) {
+    public static void printLadder(String[][] ladder) {
         for (int row = 0; row < ladder.length; row++) {
             for (int column = 0; column < ladder[row].length; column++) {
                 System.out.print(ladder[row][column]);
