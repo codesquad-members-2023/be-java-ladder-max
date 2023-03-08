62e9066bec12fa318efffbdbd65ddd1dd732b05d
crtEvent <crtevn@gmail.com>
2023-03-08 15:25:49 +0900

    add: InputView에 플레이어들의 이름을 입력받는 inputPlayerNames() 메서드 추가
    - 이름은 ,로 구분하여 문자열 배열로 반환한다

--- src/main/java/kr/codesquad/view/InputView.java
+++ src/main/java/kr/codesquad/view/InputView.java
@@ -13,6 +13,11 @@ public class InputView {
         return Integer.parseInt(br.readLine());
     }
 
+    public String[] inputPlayerNames() throws IOException {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        return br.readLine().replace(" ", "").split(",");
+    }
+
     public int inputLadderHeight() throws IOException, NumberFormatException {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         return Integer.parseInt(br.readLine());
