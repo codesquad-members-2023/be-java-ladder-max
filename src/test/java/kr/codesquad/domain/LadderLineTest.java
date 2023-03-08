012a9e2b7b9e067ce726f40907e4b574a36af2dc
CDchan <kdjin1215@Naver.com>
2023-03-09 02:14:05 +0900

    test: pointsValidate를 테스트할 Test코드 추가
    pointsValidate가 연속된 true를 가지는 points에대해 예외를 발생시키는지 확인하는 테스트코드를 추가했다.

---
+++ src/test/java/kr/codesquad/domain/LadderLineTest.java
@@ -0,0 +1,30 @@
+package kr.codesquad.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LadderLineTest {
+
+    @Test
+    @DisplayName("사람이 10000명일때 pointsValidate 성공 테스트")
+    void pointsValidateSuccessTest() {
+        LadderLine line = new LadderLine(10000);
+        line.createLine();
+        assertDoesNotThrow(() -> line.pointsValidate());
+    }
+
+    @Test
+    @DisplayName("사람이 10000명일때 pointsValidate 실패 테스트")
+    void pointsValidatefailTest() {
+        LadderLine line = new LadderLine(10000);
+        line.createLine();
+        line.points.add(true);
+        line.points.add(true);
+        assertThrows(IllegalArgumentException.class, () -> line.pointsValidate());
+    }
+}
