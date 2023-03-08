281ffc9e1fad70a20ea9c7fc14633916a34265cc
JIHYE <jdh324@naver.com>
2023-03-08 11:21:03 +0900

    하나의 사다리 배열로 변경함에 따라 출력 코드 변경

--- src/main/java/kr/codesquad/Output.java
+++ src/main/java/kr/codesquad/Output.java
@@ -1,13 +1,11 @@
 package kr.codesquad;
 
+import java.util.Arrays;
+
 public class Output {
     public static void printLadder(Ladder ladder) {
-        for (int i = 0; i < ladder.line.length; i++) {
-            for (int j = 0; j < ladder.person[0].length; j++) {
-                System.out.print(ladder.person[i][j]);
-                if(j < ladder.line[0].length) System.out.print(ladder.line[i][j]);
-            }
-            System.out.println();
-        }
+        Arrays.stream(ladder.ladder)
+                .map(a -> String.join("", a))
+                .forEach(System.out::println);
     }
 }
