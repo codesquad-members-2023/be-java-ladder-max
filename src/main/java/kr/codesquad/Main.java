e7a6d09617dde3db449ecf998cd7eb5dfcc8143d
adorableee <yejinh_h@naver.com>
2023-03-08 12:11:20 +0900

    feat : 사용자부터 입력을 받는 기능 추가

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,20 @@
 package kr.codesquad;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static OutputView outputView;
+    static InputView inputView;
+    public static void main(String[] args) throws IOException {
+        int countOfUser = getCountOfUser();
+        int heightOfLadder = getHeightOfLadder();
+    }
+    public static int getCountOfUser() {
+        outputView.inputPeople();
+        return Integer.parseInt(inputView.getInput());
+    }
+    public static int getHeightOfLadder() {
+        outputView.inputLadder();
+        return Integer.parseInt(inputView.getInput());
     }
 }
---
+++ src/main/java/kr/codesquad/OutputView.java
@@ -0,0 +1,13 @@
+package kr.codesquad;
+
+public class OutputView {
+    public void inputPeople() {
+        System.out.println("참여할 사람은 몇 명인가요 ?");
+    }
+    public void inputLadder() {
+        System.out.println("최대 사다리 높이는 몇개 인가요 ? ");
+    }
+    public void printLadder(StringBuilder sb) {
+        System.out.println(sb);
+    }
+}
