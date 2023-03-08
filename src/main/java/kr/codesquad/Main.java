d65705ecf6f8e6c45da75475381eef676d526c9b
JIHYE <jdh324@naver.com>
2023-03-08 11:38:29 +0900

    메인 클래스의 가로선과 세로선 변수명 변경

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("참여할 사람은 몇 명인가요?");
-        int numPerson = Input.input();
+        int verticalLine = Input.input();
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int numLine = Input.input();
+        int horizontalLine = Input.input();
 
-        Ladder ladder = new Ladder(numPerson, numLine);
+        Ladder ladder = new Ladder(verticalLine, horizontalLine);
         Output.printLadder(ladder);
     }
 }
