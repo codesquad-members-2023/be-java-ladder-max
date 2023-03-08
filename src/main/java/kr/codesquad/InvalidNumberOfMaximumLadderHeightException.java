a64963c7c1fe5e91655f53b024dba306919995d5
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-08 23:45:50 +0900

    modify: 예외 클래스명 변경
    * 인원수와 최대사다리 높이 숫자가 유효하지 않은경우라는 것을 알 수 있도록 이름을 변경하였습니다.
    * 클래스명 끝에 Exception을 붙여서 예외 클래스임을 알 수 있도록 하였습니다.

--- src/main/java/kr/codesquad/InvalidNumberOfMaximumLadderHeightException.java
+++ src/main/java/kr/codesquad/InvalidNumberOfMaximumLadderHeightException.java
@@ -1,10 +1,10 @@
 package kr.codesquad;
 
-public class InvalidMaximumLadderHeight extends RuntimeException {
+public class InvalidNumberOfMaximumLadderHeightException extends RuntimeException {
 
     private static final String message = "적절하지 않은 입력입니다. 1 이상의 사다리 높이를 입력해주세요.";
 
-    public InvalidMaximumLadderHeight() {
+    public InvalidNumberOfMaximumLadderHeightException() {
         super(message);
     }
 }
--- src/main/java/kr/codesquad/InvalidNumberOfPeopleException.java
+++ src/main/java/kr/codesquad/InvalidNumberOfPeopleException.java
@@ -1,10 +1,10 @@
 package kr.codesquad;
 
-public class InvalidPersonNumber extends RuntimeException {
+public class InvalidNumberOfPeopleException extends RuntimeException {
 
     private static final String message = "적절하지 않은 입력입니다. 최소 2명 이상의 사람을 입력해주세요.";
 
-    public InvalidPersonNumber() {
+    public InvalidNumberOfPeopleException() {
         super(message);
     }
 
