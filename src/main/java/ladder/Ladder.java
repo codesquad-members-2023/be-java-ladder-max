0a6cb6bd9ad9bb41d0c8f80afc5a785f103ffd6c
yeon <cerealnum@gmail.com>
2023-03-08 15:56:15 +0900

    feat : List<List<Boolean>> ladders 객체를 List<Line> lines로 변경
    Ladder 객체의 기존 메서드 중 Line 객체가 처리하기에 더 적합하다고 판단한 메서드를 Line 객체 안으로 이동합니다.

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -6,69 +6,28 @@ import java.util.List;
 public class Ladder {
     private final int space;
     private final int height;
-    private final List<List<Boolean>> ladders;
+    private final List<Line> lines;
 
     public Ladder(int space, int height) {
         this.space = space;
         this.height = height;
-        this.ladders = new ArrayList<>();
+        this.lines = new ArrayList<>();
         this.makeRandomLadder();
     }
 
     private void makeRandomLadder() {
         for (int i = 0; i < height; i++) {
-            ladders.add(makeRandomRung());
+            lines.add(new Line(space));
         }
-    }
-
-    private List<Boolean> makeRandomRung() {
-        List<Boolean> hasRungs = new ArrayList<>();
-        for (int i = 0; i < space; i++) {
-            hasRungs.add(getRandomBoolean());
-        }
-        return checkContinuousRungs(hasRungs);
-    }
-
-    private List<Boolean> checkContinuousRungs(List<Boolean> rungs) {
-        for (int i = 0; i < space; i++) {
-            if (i == 0) continue;
-            if (rungs.get(i - 1)) {
-                rungs.set(i, false);
-            }
-        }
-        return rungs;
-    }
-
-    private boolean getRandomBoolean() {
-        boolean[] random = {false, true};
-        return random[(int) (Math.random() * random.length)];
     }
 
     public void drawLadder() {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < height; i++) {
             builder.append(" ".repeat(2));
-            drawingSameHeight(builder, ladders.get(i));
+            lines.get(i).drawLine(builder);
             builder.append(System.lineSeparator());
         }
         System.out.println(builder);
-    }
-
-    private void drawingSameHeight(StringBuilder builder, List<Boolean> spaces) {
-        for (int j = 0; j < space; j++) {
-            drawLeftLine(builder, j);
-            builder.append(spaces.get(j) ? "-----" : "     ");
-            drawRightLine(builder);
-        }
-    }
-
-    private void drawLeftLine(StringBuilder builder, int space) {
-        if (space == 0) {
-            drawRightLine(builder);
-        }
-    }
-
-    private void drawRightLine(StringBuilder builder) {
-        builder.append("|");
     }
 }
---
+++ src/main/java/ladder/Line.java
@@ -0,0 +1,53 @@
+package ladder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Line {
+    private final List<Boolean> rungs;
+
+    public Line(int space) {
+        rungs = makeRandomRung(space);
+    }
+
+    private List<Boolean> makeRandomRung(int space) {
+        List<Boolean> hasRungs = new ArrayList<>();
+        for (int i = 0; i < space; i++) {
+            hasRungs.add(getRandomBoolean());
+        }
+        return checkContinuousRungs(hasRungs, space);
+    }
+
+    private List<Boolean> checkContinuousRungs(List<Boolean> rungs, int space) {
+        for (int i = 0; i < space; i++) {
+            if (i == 0) continue;
+            if (rungs.get(i - 1)) {
+                rungs.set(i, false);
+            }
+        }
+        return rungs;
+    }
+
+    private boolean getRandomBoolean() {
+        boolean[] random = {false, true};
+        return random[(int) (Math.random() * random.length)];
+    }
+
+    public void drawLine(StringBuilder builder) {
+        for (int j = 0; j < rungs.size(); j++) {
+            drawLeft(builder, j);
+            builder.append(rungs.get(j) ? "-----" : "     ");
+            drawRight(builder);
+        }
+    }
+
+    private void drawLeft(StringBuilder builder, int space) {
+        if (space == 0) {
+            drawRight(builder);
+        }
+    }
+
+    private void drawRight(StringBuilder builder) {
+        builder.append("|");
+    }
+}
