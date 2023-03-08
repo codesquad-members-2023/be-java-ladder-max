91670eea4a9c8e3cbea0a48427f59801bfcd21e4
sudago <sudago22@gmail.com>
2023-03-09 01:31:44 +0900

    파라미터 타입 변경
    Ladder는 List<List<String>> 으로 변경.
    personName은 String[] 으로 변경하고, ladder 간격에 맞춰 List로 다시 짜는 로직 추가.

--- src/mission/view/OutputView.java
+++ src/mission/view/OutputView.java
@@ -9,23 +9,35 @@ public class OutputView {
          sb = new StringBuilder();
     }
 
-    public void printPeopleNameAndLadder(List<String> peopleNames, List<String> ladder){
-        appendPeopleName(peopleNames);
+    public void printPeopleNameAndLadder(String[] peopleNames,int ladderHeight, List<List<String>> ladder){
+        appendPeopleName(peopleNames, ladderHeight);
         appendLadder(ladder);
 
         System.out.println(sb); // 모아서 출력
         sb.setLength(0); // stringBuilder 초기화 (리셋)
     }
 
-    public void appendPeopleName(List<String> peopleNames){
-        for (String str : peopleNames) {
-            sb.append(str);
+    public void appendPeopleName(String[] peopleNames, int ladderHeight){
+        for (String peopleName : peopleNames) {
+            sb.append(peopleName);
+            addBlank(peopleName, ladderHeight);
+        }
+        sb.append("\n");
+    }
+
+    public void addBlank(String peopleName, int ladderHeight){
+        for (int i = 0; i < 5 - peopleName.length(); i++) {
+            sb.append(" ");
         }
     }
 
-    public void appendLadder(List<String> ladder){
-        for (String str : ladder) {
-            sb.append(str);
+    public void appendLadder(List<List<String>> ladders){ // 일단 2중 for문 사용
+        for (List<String> ladder : ladders) {
+            for (String str : ladder) {
+                sb.append(str);
+            }
+            sb.append("\n");
         }
     }
+
 }
