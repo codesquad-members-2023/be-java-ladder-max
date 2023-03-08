b51c08215f870708bbcc14222cee5ecd5075eb19
yeon <cerealnum@gmail.com>
2023-03-08 22:38:48 +0900

    refactor : Worker 객체를 LadderGame 객체로 통합
    하나의 LadderGame 객체로 통합하면서 지역 변수인 participants를 제거합니다.

--- src/main/java/ladder/domain/LadderGame.java
+++ src/main/java/ladder/domain/LadderGame.java
@@ -1,14 +1,39 @@
 package ladder.domain;
 
+import ladder.view.View;
+
 public class LadderGame {
-    private final Worker worker;
+    private final View view;
+    private Ladder ladder;
 
     public LadderGame() {
-        this.worker = new Worker();
+        this.view = new View();
+    }
+
+    public void makeLadder(int space, int height) {
+        ladder = new Ladder(space, height);
+        ladder.makeRandomLadder();
+    }
+
+    public String makeNameLabel(String[] names) {
+        StringBuilder builder = new StringBuilder();
+        for (String name : names) {
+            int frontBlank = (6 - name.length()) / 2;
+            int backBlank = (6 - name.length()) % 2;
+            boolean isNotMaxLength = name.length() < 6;
+            builder.append(" ".repeat(frontBlank));
+            builder.append(name);
+            builder.append(" ".repeat(frontBlank == 0 && isNotMaxLength ? backBlank : frontBlank));
+        }
+        return builder.toString();
     }
 
     public void start() {
-        worker.makeLadder();
-        worker.showLadder();
+        String[] participants = view.getNamesOfParticipants();
+        int space = participants.length - 1;
+        int height = view.getNumberOfLadderHeight();
+        makeLadder(space, height);
+        view.printLadder(makeNameLabel(participants), ladder.drawLadder());
+        view.stop();
     }
 }
