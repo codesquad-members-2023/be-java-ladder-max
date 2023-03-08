ac385f56f5feb39e9fbea2839cc14a0cdfb945fe
JIHYE <jdh324@naver.com>
2023-03-08 11:17:03 +0900

    사다리 가로선과 세로선 배열을 하나의 사다리 2차원 배열로 변경

--- src/main/java/kr/codesquad/Ladder.java
+++ src/main/java/kr/codesquad/Ladder.java
@@ -2,29 +2,27 @@ package kr.codesquad;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Ladder {
-    protected String[][] line;
-    protected String[][] person;
+    protected String[][] ladder;
 
     public Ladder(int numPerson, int numLine) {
-        line = new String[numLine][numPerson - 1];
-        person = new String[numLine][numPerson];
+        ladder = new String[numLine][(2 * numPerson) - 1];
 
         fillPerson();
         fillLine();
     }
 
     public void fillPerson() {
-        Arrays.stream(person).forEach(row -> Arrays.fill(row, "|"));
+        Arrays.stream(ladder).forEach(row -> Arrays.fill(row, "|"));
     }
 
     public void fillLine() {
         String[] str = {"-", " "};
-        for (int i = 0; i < line.length; i++) {
-            for (int j = 0; j < line[0].length; j++) {
-                line[i][j] = str[new Random().nextInt(2)];
-            }
-        }
+        IntStream.range(0, ladder.length)
+                .forEach(x -> IntStream.range(0, ladder[0].length)
+                        .filter(y -> y % 2 == 1)
+                        .forEach(y -> ladder[x][y] = str[new Random().nextInt(2)]));
     }
 }
