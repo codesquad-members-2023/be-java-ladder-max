0d9184c9a01be856f8e0fc771b338eb2ecf30848
crtEvent <crtevn@gmail.com>
2023-03-08 23:02:03 +0900

    add: Main에서 IllegalArgumentException 예외처리 추가
    - 예외 발생시 메시지를 출력하고 다시 입력받도록 함

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -13,6 +13,8 @@ public class Main {
             controller.runLadderGame();
         } catch (NumberFormatException e) {
             System.out.println("잘못된 값을 입력하였습니다. 다시 입력해 주세요.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
