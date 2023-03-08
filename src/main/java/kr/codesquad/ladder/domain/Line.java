a10367fad972537302b1f8dbce42cbfa2b04e49b
jun <alap_u@naver.com>
2023-03-08 12:30:39 +0900

    Line의 메서드를 분리

--- src/main/java/kr/codesquad/ladder/domain/Line.java
+++ src/main/java/kr/codesquad/ladder/domain/Line.java
@@ -1,8 +1,5 @@
 package kr.codesquad.ladder.domain;
 
-import static kr.codesquad.ladder.domain.BooleanGenerator.randomBoolean;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,25 +7,8 @@ public class Line {
 
     private final List<Boolean> line;
 
-    private Line(List<Boolean> line) {
+    public Line(List<Boolean> line) {
         this.line = line;
-    }
-
-    public static Line createLine(int count) {
-        List<Boolean> booleans = new ArrayList<>();
-
-        for (int index = 0; index < count; index++) {
-            checkDoubleTrue(booleans, index);
-        }
-        return new Line(booleans);
-    }
-
-    private static void checkDoubleTrue(List<Boolean> booleans, int index) {
-        boolean randomBoolean = randomBoolean();
-        booleans.add(randomBoolean);
-        if (index >= 1 && booleans.get(index - 1)) {
-            booleans.set(index, false);
-        }
     }
 
     @Override
