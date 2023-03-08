04a8b40aa04676be746b1f67ab25068b66fdf855
jwp345 <jwp135@naver.com>
2023-03-08 11:06:30 +0900

    문자열이 중복 되는 부분이 있어 인스턴스 변수로 뺌

--- src/main/java/kr/codesquad/domain/Line.java
+++ src/main/java/kr/codesquad/domain/Line.java
@@ -5,23 +5,25 @@ import java.util.Random;
 
 public class Line {
     private final ArrayList<String> points;
+    private final String LEG = "-----";
+    private final String BLANK = "     ";
 
     public Line() {
         this.points = new ArrayList<>();
     }
 
     public boolean canDraw (int countOfPerson) {
-        return countOfPerson == 0 || points.get(countOfPerson - 1).equals("     ");
+        return countOfPerson == 0 || points.get(countOfPerson - 1).equals(BLANK);
     }
 
     public void drawLine(int countOfPerson) {
         Random random = new Random();
         boolean draw = random.nextBoolean();
         if(draw && canDraw(countOfPerson)) {
-            addLine("-----");
+            addLine(LEG);
             return;
         }
-        addLine("     ");
+        addLine(BLANK);
     }
 
     public void addLine(String s) {
---
+++ src/test/java/OutputViewTest.java
@@ -0,0 +1,15 @@
+import kr.codesquad.view.OutputView;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class OutputViewTest {
+    OutputView outputView;
+
+    @BeforeEach
+    void setUp() {
+        outputView = new OutputView();
+    }
+
+
+}
