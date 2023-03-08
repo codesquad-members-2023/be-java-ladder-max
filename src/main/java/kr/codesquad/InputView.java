c2c4489724ea3cb394c8e4085950ddb4dd5f13c8
pinetree <eodnjs568@gmail.com>
2023-03-09 07:51:25 +0900

    FIX "Inputview 클래스 수정"
    1. Inputview 내 static으로 선언했던 메서드 비 static으로 선언
    2. 고정적으로 사용하는 출력형식(상수값) 등은 private  static final로선언
    3. inputPeopleName 메서드 수정.
    4. 정규표현식을 통해서 유효성 검증 진행
    
     # Please enter the commit message for your changes. Lines starting

--- src/main/java/kr/codesquad/InputView.java
+++ src/main/java/kr/codesquad/InputView.java
@@ -1,22 +1,40 @@
 package kr.codesquad;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class InputView {
-    public static final Scanner SCANNER = new Scanner(System.in);
+    public final Scanner SCANNER = new Scanner(System.in);
+    private static final String NAME_FORMAT = "^[a-z]+(,[a-z]+)+$";
+    private static final String INVALID_INPUT_NAMEFORMAT = "유효하지 않은 형식입니다.";
+    private static final String INPUT_PEOPLE_NAME = "참여할 사람 이름을 입력하세요.(이름은 쉼표로 구분하세요.)";
 
 
-    public static int inputPeopleNumber() {
-        System.out.println("참여할 사람은 몇 명인가요?");
-        return SCANNER.nextInt();
+
+    public List<String> inputPeopleName() {
+        System.out.println(INPUT_PEOPLE_NAME);
+        String input = SCANNER.nextLine();
+            if (validateNameFormat(input)) {
+                return Arrays.stream(input.split(",")).collect(Collectors.toList());
+            }
+        System.out.println(INVALID_INPUT_NAMEFORMAT);
+        return inputPeopleName();
     }
 
-    public static int inputLadderHeight() {
+    private boolean validateNameFormat(String input) {
+        boolean result = Pattern.matches(NAME_FORMAT, input);
+        return result;
+    }
+
+    public int inputLadderHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         return SCANNER.nextInt();
     }
 
-    public static void close(){
+    public void close() {
         SCANNER.close();
     }
 
