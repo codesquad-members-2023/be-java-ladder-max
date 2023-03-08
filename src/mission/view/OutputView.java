82932e35253eb985b7c03167ad17f859b2bb20f7
sudago <sudago22@gmail.com>
2023-03-08 21:53:21 +0900

    OutputView 수정
    각각의 print 메서드에서 stringBuilder를 출력하고 0으로 초기화를 할까 고민했지만, 사람수와 사다리 다 print 하는 메서드를 만들고 그 안에서 stringBuilder를 출력하고 초기화(리셋)해주었다.

--- src/mission/view/OutputView.java
+++ src/mission/view/OutputView.java
@@ -1,14 +1,31 @@
 package mission.view;
 
-public class OutputView {
+import java.util.List;
 
-    public void printStrings(String[][] strings){
-        for (String[] row: strings) {
-            for (String column: row) {
-                System.out.print(column);
-            }
-            System.out.println();
+public class OutputView {
+    StringBuilder sb;
+
+    public OutputView(){
+         sb = new StringBuilder();
+    }
+
+    public void printPeopleNameAndLadder(List<String> peopleNames, List<String> ladder){
+        appendPeopleName(peopleNames);
+        appendLadder(ladder);
+
+        System.out.println(sb); // 모아서 출력
+        sb.setLength(0); // stringBuilder 초기화 (리셋)
+    }
+
+    public void appendPeopleName(List<String> peopleNames){
+        for (String str : peopleNames) {
+            sb.append(str);
         }
     }
 
+    public void appendLadder(List<String> ladder){
+        for (String str : ladder) {
+            sb.append(str);
+        }
+    }
 }
