d9bc58931e1d3bd6149cd280b3de24dafc1f0c05
jun <alap_u@naver.com>
2023-03-08 22:03:00 +0900

    결과를 확인할 이름을 입력받음

--- src/main/java/kr/codesquad/ladder/view/InputView.java
+++ src/main/java/kr/codesquad/ladder/view/InputView.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 public class InputView {
 
-    private static final String REQUEST_RESULT_NAME_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private final String REQUEST_RESULT_NAME_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private final String REQUEST_NAME_MESSAGE = "결과를 보고 싶은 사람은?";
     private final String REQUEST_PARTICIPANT_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private final String REQUEST_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private final String NAME_SEPARATOR = ",";
@@ -27,6 +28,11 @@ public class InputView {
     public List<String> inputResult() throws IOException {
         System.out.println(REQUEST_RESULT_NAME_MESSAGE);
         return Arrays.asList(bufferedReader.readLine().split(NAME_SEPARATOR));
+    }
+
+    public String inputResultName() throws IOException {
+        System.out.println(REQUEST_NAME_MESSAGE);
+        return bufferedReader.readLine();
     }
 
 }
