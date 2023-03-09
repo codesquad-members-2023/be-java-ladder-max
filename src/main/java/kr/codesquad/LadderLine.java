0b5f1ee947466d10c5929e90a2a13662a5e14832
sejeong00 <buttermelt@daum.net>
2023-03-09 17:41:33 +0900

    feat: LadderLine.isProperLocation() 구현
    "-", "|" 가 들어갈 자리인지 판별하는 isProperLocation()을 구현했다.
    "|"는 홀수열마다 나타난다.
    "-"는 짝수열에 1/5의 확률로 나타나게 구현했다.

--- src/main/java/kr/codesquad/LadderLine.java
+++ src/main/java/kr/codesquad/LadderLine.java
@@ -1,12 +1,37 @@
 package kr.codesquad;
 
+import java.util.Random;
+
 public enum LadderLine {
-    VERTICAL("|"),
-    HORIZONTAL("-");
+    VERTICAL("|"){
+        @Override
+        boolean isProperLocation(int index) {
+            return isOdd(index);
+        }
+    },
+    HORIZONTAL("-"){
+        @Override
+        boolean isProperLocation(int index) {
+            return !isOdd(index) && isRand(index);
+        }
+    };
     
-    private String line;
+    private final String line;
 
     LadderLine(String line) {
         this.line = line;
     }
+
+    abstract boolean isProperLocation(int index);
+
+    private static boolean isOdd(int index){
+        return index%2 == 0;
+    }
+
+    private static boolean isRand(int index){
+        Random rand = new Random();
+
+        return (rand.nextInt()%5) == 0;
+    }
+
 }
