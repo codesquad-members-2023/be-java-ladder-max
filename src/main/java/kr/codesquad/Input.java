4f4703db6faf59106425d7f8242e37c4a8c17a86
JIHYE <jdh324@naver.com>
2023-03-09 14:55:48 +0900

    플레이어의 이름을 최대 5글자까지 부여

--- src/main/java/kr/codesquad/Input.java
+++ src/main/java/kr/codesquad/Input.java
@@ -2,16 +2,32 @@ package kr.codesquad;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Input {
-    public static int input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static int input(BufferedReader br) throws IOException {
         try {
             return Integer.parseInt(br.readLine());
-        }catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             System.out.println("정수가 아닙니다.");
-            return input();
+            return input(br);
+        }
+    }
+
+    public static ArrayList<String> inputNames(BufferedReader br) throws IOException {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        String[] names = br.readLine().replaceAll(" ", "").split(",");
+        for (String name : names) {
+            limitNameSize(name, br);
+        }
+        return new ArrayList<>(Arrays.asList(names));
+    }
+
+    private static void limitNameSize(String name, BufferedReader br) throws IOException {
+        if (name.length() > 5) {
+            System.out.println("플레이어의 이름은 최대 5글자까지 가능합니다.");
+            inputNames(br);
         }
     }
 }
