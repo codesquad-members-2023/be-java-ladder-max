efb06855b354a5e8bb52b72e6a87ad3f444292f8
HyowonSin <tlsgydnjs@naver.com>
2023-03-09 18:25:04 +0900

    feat: 입력 메서드
    인원, 사다리 높이 관련 입력 메서드

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,36 @@
 package kr.codesquad;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        int n = main.inputPeople();
+        int m = main.inputLadder();
+    private int inputPeople() throws IOException {
+        printInputPeople();
+        return input();
+    }
+
+    private int inputLadder() throws IOException {
+        printInputLadder();
+        return input();
+    }
+
+    private int input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(br.readLine());
+    }
+
+    private void printInputPeople() {
+        System.out.println("참여할 사람은 몇 명 인가요?");
+    }
+
+    private void printInputLadder() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
     }
 }
