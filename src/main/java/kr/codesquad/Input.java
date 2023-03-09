18a292383da35f547bf8a893dceaa82a74fdca51
JIHYE <jdh324@naver.com>
2023-03-09 15:16:19 +0900

    메소드 변경으로 인한 프로그램 주요 로직 변경

--- src/main/java/kr/codesquad/Input.java
+++ src/main/java/kr/codesquad/Input.java
@@ -6,8 +6,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Input {
-    public static int input(BufferedReader br) throws IOException {
+    public int input(BufferedReader br) throws IOException {
         try {
+            System.out.println("최대 사다리 높이는 몇 개인가요?");
             return Integer.parseInt(br.readLine());
         } catch (NumberFormatException e) {
             System.out.println("정수가 아닙니다.");
@@ -15,7 +16,7 @@ public class Input {
         }
     }
 
-    public static ArrayList<String> inputNames(BufferedReader br) throws IOException {
+    public ArrayList<String> inputNames(BufferedReader br) throws IOException {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String[] names = br.readLine().replaceAll(" ", "").split(",");
         for (String name : names) {
@@ -24,7 +25,7 @@ public class Input {
         return new ArrayList<>(Arrays.asList(names));
     }
 
-    private static void limitNameSize(String name, BufferedReader br) throws IOException {
+    private void limitNameSize(String name, BufferedReader br) throws IOException {
         if (name.length() > 5) {
             System.out.println("플레이어의 이름은 최대 5글자까지 가능합니다.");
             inputNames(br);
