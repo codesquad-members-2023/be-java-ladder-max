77b8149a886ab6fcd6535a0e9443c1f786198c5b
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-09 15:38:15 +0900

    modify: 유효하지않은 사람인원수 예외의 메시지 변경
    * 동작 예시에 맞추어서 변경하였습니다.

--- src/main/java/kr/codesquad/InvalidNumberOfPeopleException.java
+++ src/main/java/kr/codesquad/InvalidNumberOfPeopleException.java
@@ -2,7 +2,7 @@ package kr.codesquad;
 
 public class InvalidNumberOfPeopleException extends RuntimeException {
 
-    private static final String message = "적절하지 않은 입력입니다. 최소 2명 이상의 사람을 입력해주세요.";
+    private static final String message = "참여할 사람은 최소 2명 이상이어야 합니다. 다시 입력해주세요.";
 
     public InvalidNumberOfPeopleException() {
         super(message);
