dfabeb675d85e3ce9dd9e207e12ddaec696dbdca
LaStella <jingyu.ham94@gmail.com>
2023-03-09 15:38:40 +0900

    refactor: Console클래스 리팩토링
    입력값이 없을 시 IOException을 날리지 않고 다시 입력을 받도록 재귀호출로 변경하였습니다.

--- src/main/java/kr/codesquad/Console.java
+++ src/main/java/kr/codesquad/Console.java
@@ -3,13 +3,18 @@ package kr.codesquad;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Optional;
 
 public class Console {
   private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-  public String input(String prompt) throws IOException {
+  public String input(String prompt) {
     System.out.println(prompt);
-    return br.readLine();
+    try {
+      return br.readLine();
+    } catch (IOException e) {
+      return input(prompt);
+    }
   }
 
   public void inputError() {
