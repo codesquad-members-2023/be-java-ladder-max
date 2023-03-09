0c73b06d6fb0d51d5561f6319eeec5b5ccea3223
yeon <cerealnum@gmail.com>
2023-03-09 18:21:12 +0900

    test : 참가자의 수가 2명 미만일 경우 오류를 발생하는지 검증하는 테스트 추가

---
+++ src/test/java/ladder/domain/PlayersTest.java
@@ -0,0 +1,23 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PlayersTest {
+    @Test
+    @DisplayName("참가자의 수가 2명 미만일 경우 오류를 발생하는지 검증하는 테스트")
+    void checkMinPlayer() {
+        // given
+        List<String> names = new ArrayList<>();
+        names.add("alone");
+
+        // when
+        // then
+        assertThatThrownBy(() -> new Players(names)).isInstanceOf(IllegalArgumentException.class).hasMessage("2명 이상 참여해야 시작할 수 있습니다.");
+    }
+}
