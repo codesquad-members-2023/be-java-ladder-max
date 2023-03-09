e9a045e9519bb8acf80f36e4bf6647f2b4312737
sejeong00 <buttermelt@daum.net>
2023-03-09 18:09:55 +0900

    feat: Ladder 클래스, 생성자, Ladder.init() 구현
    Ladder.init()은 ladder 에 값을 넣어준다.

---
+++ src/main/java/kr/codesquad/Ladder.java
@@ -0,0 +1,33 @@
+package kr.codesquad;
+
+public class Ladder {
+
+    final int ROW_SIZE;
+    final int COLUMN_SIZE;
+    private final LadderLine[][] ladder;
+
+    public Ladder(int n, int m) {
+        ROW_SIZE = n * 2 + 1;
+        COLUMN_SIZE = m;
+        ladder = new LadderLine[ROW_SIZE][COLUMN_SIZE];
+        init();
+    }
+
+    private void init(){
+        for(int rowIndex = 0; rowIndex < ROW_SIZE; rowIndex++){
+            initColumn(rowIndex);
+        }
+    }
+
+    private void initColumn(int rowIndex){
+        for(int columnIndex = 0; columnIndex < COLUMN_SIZE; columnIndex++){
+            ladder[rowIndex][columnIndex] = LadderLine.VERTICAL.isProperLocation(columnIndex) ? LadderLine.VERTICAL
+                    : LadderLine.HORIZONTAL.isProperLocation(columnIndex) ? LadderLine.HORIZONTAL
+                    : null ;
+        }
+    }
+
+
+
+
+}
