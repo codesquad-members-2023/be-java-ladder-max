9eb624d5bb76f190b4ea06ae961130fbc6252cbd
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-03-09 18:02:27 +0900

    라인 유무를 확인하는 테스트와 이름 최대 5자 확인 테스트 코드를 작성했다.
    LadderRow 클래스에 isRowHasLine()메소드를 구현해서 row에 라인이 있는지를 확인할 수 있도록 했다.

--- src/test/java/LadderRowTest.java
+++ src/test/java/LadderRowTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.*;
 public class LadderRowTest {
     @DisplayName("사다리 한 로우가 선을 가지고 있는지 확인")
     @Test
-    public void isRadderRowHasLine() {
+    public void isLadderRowHasLine() {
         LadderRow ladderRow = new LadderRow(3);
 
         assertThat(ladderRow.isRowHasLine()).isTrue();
