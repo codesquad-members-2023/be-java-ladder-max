03aeee8642d9d2731cc08100672ebe8bc4da509b
sudago <sudago22@gmail.com>
2023-03-09 17:46:01 +0900

    Ladder 클래스 지역 변수 생성 및 메서드 분리
    lastLadderGroupNumber 현재가 마지막 그룹인지 판별
    beforeGroupChar 이전 그룹의 문자
    chooseFirstCharOfGroup() 그룹의 시작 문자를 판별하는 3가지 메서드를 묶은 메서드
    repeatFirstCharOfGroup 위의 메서드에서 판별한 문자를 반복해서 삽입하는 메서드

--- src/mission/model/Ladder.java
+++ src/mission/model/Ladder.java
@@ -42,16 +42,26 @@ public class Ladder {
 
     // 여기서 그룹이란! pipe(|)를 기준으로 나눴을 때, 나머지 부분을 이야기 한다.
     private void makeHyphenOrBlank(List<String> ladderLine, int i, int j, int peopleNumber){
-        // 뭉터기의 시작을 판단하는 3가지 메서드
+        int lastLadderGroupNumber = peopleNumber - 2;
+        String beforeGroupChar = ladderLine.get(ladderLine.size() - 3);
+
         // 그룹의 시작 문자를 판단하는 3가지 메서드
+        chooseFirstCharOfGroup(ladderLine, i, j, lastLadderGroupNumber, beforeGroupChar);
+        // 판단한 시작 문자로 반복해서 넣는 메서드
+        repeatFirstCharOfGroup(ladderLine, i, j);
+    }
+    private void chooseFirstCharOfGroup(List<String> ladderLine, int i, int j, int lastLadderGroupNumber, String beforeGroupChar) {
         // 공백이나 하이픈을 랜덤으로 넣어도 되는 경우
         if (j == 1) {
-            checkRandomHyphenOrBlank(ladderLine, i, peopleNumber);
+            checkRandomHyphenOrBlank(ladderLine, i, lastLadderGroupNumber, beforeGroupChar);
             // 공백을 넣어야만 하는 경우
-            checkBlank(ladderLine, i);
+            checkBlank(ladderLine, i, beforeGroupChar);
             // 하이픈을 넣어야만 하는 경우
-            checkHyphen(ladderLine, i, peopleNumber);
+            checkHyphen(ladderLine, i, lastLadderGroupNumber);
         }
+    }
+
+    private void repeatFirstCharOfGroup(List<String> ladderLine, int i, int j) {
         if (j > 1) {
             // 그룹의 중간~끝 부분일 경우 (그룹의 시작이 아닐 경우), 그룹의 시작(윗부분에서 판가름 해서 넣은)을 반복해서 넣는다.
             addFirstChar(ladderLine, i);
@@ -67,20 +77,22 @@ public class Ladder {
     }
 
     // 맨 앞일 경우 || (checkHyphen의 경우가 아니면서 && 그 전의 그룹이 공백이었을 경우)
-    private void checkRandomHyphenOrBlank(List<String> ladderLine, int i, int peopleNumber){
-        if (i == 0 || ladderLine.get(ladderLine.size() - 3).equals(" ")) {
+    private void checkRandomHyphenOrBlank(List<String> ladderLine, int i, int lastLadderGroupNumber, String beforeGroupChar){
+        if (i == 0
+                || (!((i == lastLadderGroupNumber) && !ladderLine.contains("-"))
+                    && beforeGroupChar.equals(" "))) {
             ladderLine.add(randomChar());
         }
     }
     // 맨 앞이 아니면서 && 그 전의 그룹이 Hyphen 이었을 경우
-    private void checkBlank(List<String> ladderLine, int i){
-        if (i != 0 && ladderLine.get(ladderLine.size()-3).equals("-")) {
+    private void checkBlank(List<String> ladderLine, int i, String beforeGroupChar){
+        if (i != 0 && beforeGroupChar.equals("-")) {
             ladderLine.add(" ");
         }
     }
     // 맨 마지막 그룹이면서 (사람수-2 == i) && 여태까지 "-"가 한번도 넣어진 적 없는 경우
-    private void checkHyphen(List<String> ladderLine, int i, int peopleNumber){
-        if ((i == peopleNumber - 2) && !ladderLine.contains("-")) {
+    private void checkHyphen(List<String> ladderLine, int i, int lastLadderGroupNumber){
+        if ((i == lastLadderGroupNumber) && !ladderLine.contains("-")) {
             ladderLine.add("-");
         }
     }
