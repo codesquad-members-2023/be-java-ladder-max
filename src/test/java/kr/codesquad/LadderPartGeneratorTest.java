5acc8facc38edf7f1c32497bd388a9d3d8b063b9
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-09 15:58:59 +0900

    test: 사다리의 한 라인에 브리지가 2개 이상 연속적으로 생성되지 않는 테스트케이스 추가
    * 테스트는 인위적으로 한 라인에 브리지가 연속적으로 2개가 나올때 이를 검사하고 공백으로 저장하는지 테스트입니다.

--- src/test/java/kr/codesquad/LadderPartGeneratorTest.java
+++ src/test/java/kr/codesquad/LadderPartGeneratorTest.java
@@ -6,7 +6,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Field;
 import java.util.Random;
 
 import static org.mockito.Mockito.mock;
@@ -22,7 +21,7 @@ class LadderPartGeneratorTest {
 
     @Test
     @DisplayName("사람 2명과 사다리 높이2가 주어질 때 랜덤한 사다리 생성하는지 테스트")
-    public void generate_testcase1() {
+    public void generateLadderPart_testcase1() {
         //given
         List<String> namesOfPeople = new ArrayList<>(List.of("pobi", "honux"));
         m = 2;
@@ -33,18 +32,17 @@ class LadderPartGeneratorTest {
         when(mockRandom.nextBoolean()).thenReturn(true, false);
         generator = new LadderPartGenerator(mockRandom);
         //when
-        String[][] actual = generator.generate(ladder);
+        List<List<String>> actual = generator.generateLadderPart(ladder);
         //then
-        String[][] expected = {
-            {"pobi", "", "honux"},
-            {"|", "-----", "|"},
-            {"|", "     ", "|"}};
+        List<List<String>> expected = new ArrayList<>();
+        expected.add(new ArrayList<>(List.of("|", "-----", "|")));
+        expected.add(new ArrayList<>(List.of("|", "     ", "|")));
         Assertions.assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     @DisplayName("사람 3명과 사다리 높이5가 주어질 때 랜덤한 사다리 생성하는지 테스트")
-    public void generate_testcase2() {
+    public void generateLadderPart_testcase2() {
         //given
         List<String> namesOfPeople = new ArrayList<>(List.of("pobi", "honux", "crong"));
         m = 5;
@@ -56,15 +54,36 @@ class LadderPartGeneratorTest {
             true, false);
         generator = new LadderPartGenerator(mockRandom);
         //when
-        String[][] actual = generator.generate(ladder);
+        List<List<String>> actual = generator.generateLadderPart(ladder);
         //then
-        String[][] expected = {
-            {"pobi", "", "honux", "", "crong"},
-            {"|", "-", "|", " ", "|"},
-            {"|", " ", "|", "-", "|"},
-            {"|", "-", "|", "-", "|"},
-            {"|", " ", "|", "-", "|"},
-            {"|", "-", "|", " ", "|"}};
+        List<List<String>> expected = new ArrayList<>();
+        expected.add(new ArrayList<>(List.of("|", "-----", "|", "     ", "|")));
+        expected.add(new ArrayList<>(List.of("|", "     ", "|", "-----", "|")));
+        expected.add(new ArrayList<>(List.of("|", "-----", "|", "-----", "|")));
+        expected.add(new ArrayList<>(List.of("|", "     ", "|", "-----", "|")));
+        expected.add(new ArrayList<>(List.of("|", "-----", "|", "     ", "|")));
         Assertions.assertThat(actual).isEqualTo(expected);
     }
+
+    @Test
+    @DisplayName("사다리의 한 라인에 브리지가 2개 이상 연속적으로 나오지 않는지 테스트")
+    public void generateLadderPart_testcase3() {
+        //given
+        List<String> namesOfPeople = new ArrayList<>(List.of("pobi", "honux"));
+        m = 1;
+        random = new Random();
+        ladder = new Ladder(namesOfPeople, m);
+        //mocking
+        Random mockRandom = mock(random.getClass());
+        when(mockRandom.nextBoolean()).thenReturn(true, true, false);
+        generator = new LadderPartGenerator(mockRandom);
+        //when
+        List<List<String>> actual = generator.generateLadderPart(ladder);
+        //then
+        List<List<String>> expected = new ArrayList<>();
+        expected.add(new ArrayList<>(List.of("|", "-----", "|", "     ", "|")));
+        Assertions.assertThat(actual).isEqualTo(expected);
+
+    }
+
 }
