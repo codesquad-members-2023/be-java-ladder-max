54e1278d365d7b6b5bbe7d3aa6d0153ca278ea0a
CDchan <kdjin1215@Naver.com>
2023-03-09 21:59:32 +0900

    refactor: getNameAndHeightFromUser method  분리
    기존엔 getNameAndHeightFromUser 메서드 하나에서 사용자로부터 player이름, ladder height를 받도록했다.
    
    getName,getHeight메서드로 기능을 분리하여 추후 test를 좀더 용이하게 할수있도록 구현했다.

--- src/main/java/kr/codesquad/controller/InputOutputController.java
+++ src/main/java/kr/codesquad/controller/InputOutputController.java
@@ -1,43 +1,42 @@
 package kr.codesquad.controller;
 
-import kr.codesquad.domain.InputParser;
 import kr.codesquad.domain.Ladder;
+import kr.codesquad.domain.NameValidator;
 import kr.codesquad.view.InputView;
 import kr.codesquad.view.OutputView;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class InputOutputController {
 
     private OutputView outputHandler;
     private InputView inputHandler;
-    private InputParser inputParser;
+    private NameValidator inputParser;
 
-    public InputOutputController(){
+    public InputOutputController() {
         this.inputHandler = new InputView();
         this.outputHandler = new OutputView();
-        this.inputParser = new InputParser();
+        this.inputParser = new NameValidator();
     }
 
-    public ArrayList getNameAndHeightFromUser() throws IOException {
+    public ArrayList getNameAndHeightFromUser(){
         ArrayList listForNameAndHeight = new ArrayList();
-        outputHandler.outputParticipantNamePrompt();
-        AddNameToList(listForNameAndHeight);
-
-        outputHandler.outputLadderHeightPrompt();
-        listForNameAndHeight.add(inputHandler.getInput());
+        getName(listForNameAndHeight);
+        getHeight(listForNameAndHeight);
         return listForNameAndHeight;
     }
 
-    private void AddNameToList(ArrayList listForNameAndHeight) throws IOException {
-        while(true){
-            if(inputParser.parseAndValidate(listForNameAndHeight,inputHandler.getInput()))
-                break;
-        }
+    void getName(ArrayList listForNameAndHeight){
+        outputHandler.outputParticipantNamePrompt();
+        listForNameAndHeight.addAll(inputParser.getValidNameFromUser(inputHandler));
     }
 
-    public void printLadder(Ladder ladder){
+    void getHeight(ArrayList listForNameAndHeight){
+        outputHandler.outputLadderHeightPrompt();
+        listForNameAndHeight.add(inputParser.getValidHeightFromUser(inputHandler));
+    }
+
+    public void printLadder(Ladder ladder) {
         outputHandler.printLadder(ladder);
     }
 }
