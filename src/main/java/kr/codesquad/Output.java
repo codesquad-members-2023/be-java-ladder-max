7d56b1c08bafd63d1c0f782ce07bb39e9f4e1b56
JIHYE <jdh324@naver.com>
2023-03-09 15:00:34 +0900

    이름 출력 메소드 구현

--- src/main/java/kr/codesquad/Output.java
+++ src/main/java/kr/codesquad/Output.java
@@ -1,8 +1,14 @@
 package kr.codesquad;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Output {
+    public static void printNames(ArrayList<String> names) {
+        names.forEach(name -> System.out.print(name + " "));
+        System.out.println();
+    }
+
     public static void printLadder(Ladder ladder) {
         ladder.ladder.stream()
                 .map(a -> String.join("", a))
