23f49c28c39b1a6f258c00934fef15e7f47b5280
chung <chunghye1998>
2023-03-09 16:27:10 +0900

    test: LineTest 생성 및 isLine 테스트코드 구현

---
+++ src/test/java/kr/codesquad/domain/LineTest.java
@@ -0,0 +1,23 @@
+package kr.codesquad.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LineTest {
+
+    @Test
+    @DisplayName("선을 그릴 수 있는지 여부를 판단")
+    public void isLine () {
+        // given
+        Line line = new Line();
+        String before = "-----";
+
+        // when
+        boolean checkedLine = line.isLine(before);
+
+        // then
+        assertThat(checkedLine).isEqualTo(true);
+    }
+}
