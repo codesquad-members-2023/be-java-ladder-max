2ee6700e0db0cf5b8a4bd912aceee2bda2474a70
sejeong00 <buttermelt@daum.net>
2023-03-11 00:20:13 +0900

    refactor Scanner를 static으로 변경
    메소드 호출을 줄이고 가독성을 높였다.

--- src/main/java/kr/codesquad/Input.java
+++ src/main/java/kr/codesquad/Input.java
@@ -3,17 +3,13 @@ package kr.codesquad;
 import java.util.Scanner;
 
 public class Input {
-    int inputNumberOfPlayers() {
-        return inputIntValue();
+    static Scanner sc = new Scanner(System.in);
+
+    static int inputNumberOfPlayers() {
+        return sc.nextInt();
     }
 
     static int inputLadderHeight(){
-        return inputIntValue();
-    }
-
-    private static int inputIntValue(){
-        Scanner sc = new Scanner(System.in);
-
         return sc.nextInt();
     }
 }
