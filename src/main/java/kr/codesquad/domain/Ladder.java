1d2c8fb416b32727584354df05e808cc6f301e69
JIHYE <jdh324@naver.com>
2023-03-10 14:42:12 +0900

    Ladder 클래스의 tostring 오버라이드

--- src/main/java/kr/codesquad/domain/Ladder.java
+++ src/main/java/kr/codesquad/domain/Ladder.java
@@ -1,6 +1,7 @@
 package kr.codesquad.domain;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Ladder {
     private ArrayList<Line> ladder = new ArrayList<>();
@@ -9,5 +10,12 @@ public class Ladder {
         for (int i = 0; i < countOfLadder; i++) {
             ladder.add(new Line(countOfPerson));
         }
+    }
+
+    @Override
+    public String toString() {
+        return ladder.stream()
+                .map(Line::toString)
+                .collect(Collectors.joining("\n"));
     }
 }
