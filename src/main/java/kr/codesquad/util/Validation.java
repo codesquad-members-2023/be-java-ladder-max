3ca0956f245586f0a698776cd0016186214559cb
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-03-10 14:37:42 +0900

    refactor : util 패키지 생성하고 Validation 클래스 생성
    예외처리는 Input 클래스에 있는 것 보다는 따로 빼주는 것이 더 간결해지고 역할도 나누어져서 입력받은 데이터에 대한 예외 처리를 해주는 메서드를 Validation 이라는 클래스를 생성해서 Validation으로 이동시켰다.

---
+++ src/main/java/kr/codesquad/util/Validation.java
@@ -0,0 +1,17 @@
+package kr.codesquad.util;
+
+public class Validation {
+    public static String validateName(String inputData) {
+        if(!inputData.matches(".+,.+")){
+            throw new RuntimeException("참여할 사람은 2명 이상 이어야 합니다.");
+        }
+        return inputData;
+    }
+
+    public static int validateHeight(int height) {
+        if(height < 0){
+            throw new RuntimeException("사다리의 높이는 1이상 이어야 합니다.");
+        }
+        return height;
+    }
+}
--- src/main/java/kr/codesquad/view/Input.java
+++ src/main/java/kr/codesquad/view/Input.java
@@ -1,5 +1,7 @@
 package kr.codesquad.view;
 
+import kr.codesquad.util.Validation;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,25 +11,11 @@ public class Input {
 
     public String inputName() throws IOException {
         System.out.println("참여할 사람 이름을 입력하세요.(이름은 쉼표(,)로 구분)");
-        return validateName(br.readLine());
+        return Validation.validateName(br.readLine());
     }
 
     public int inputHeight() throws IOException {
         System.out.println("사다리 높이를 입력하세요.");
-        return validateHeight(Integer.parseInt(br.readLine()));
-    }
-
-    public String validateName(String inputData) {
-        if(!inputData.matches(".+,.+")){
-            throw new RuntimeException("참여할 사람은 2명 이상 이어야 합니다.");
-        }
-        return inputData;
-    }
-
-    public int validateHeight(int height) {
-        if(height < 0){
-            throw new RuntimeException("사다리의 높이는 1이상 이어야 합니다.");
-        }
-        return height;
+        return Validation.validateHeight(Integer.parseInt(br.readLine()));
     }
 }
