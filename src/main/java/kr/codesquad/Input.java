071dd14617243d5d2c4d5cbbe144df39d00725b9
sejeong00 <buttermelt@daum.net>
2023-03-10 09:31:06 +0900

    feat: 입력 받는 메소드 구현
    InputNumberOfPlayers() : 플레이어 수 입력
    InputLadderHeight() : 사다리 높이 입력
    InputIntValue() : 정수 값 입력

--- src/main/java/kr/codesquad/Input.java
+++ src/main/java/kr/codesquad/Input.java
@@ -3,5 +3,17 @@ package kr.codesquad;
 import java.util.Scanner;
 
 public class Input {
+    int InputNumberOfPlayers () {
+        return inputIntValue();
+    }
 
+    int InputLadderHeight(){
+        return inputIntValue();
+    }
+
+    int inputIntValue(){
+        Scanner sc = new Scanner(System.in);
+
+        return sc.nextInt();
+    }
 }
