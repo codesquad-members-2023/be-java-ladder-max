4c343caf59eb9f872346e8e51cae855337537cde
sejeong00 <buttermelt@daum.net>
2023-03-10 09:16:23 +0900

    feat: 입력 UI 담당하는 출력 메소드 생성
    printInputNumberOfPlayerQuestion() : 참여할 사람 수 묻는 질문 출력
    printInputLadderHeightQuestion() : 사다리 높이 묻는 질문 출력

--- src/main/java/kr/codesquad/Output.java
+++ src/main/java/kr/codesquad/Output.java
@@ -1,5 +1,13 @@
 package kr.codesquad;
 
 public class Output {
+    void printInputNumberOfPlayerQuestion(){
+        System.out.println("참여할 사람은 몇 명인가요?");
+    }
+
+    void printInputLadderHeightQuestion(){
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+    }
+
 
 }
