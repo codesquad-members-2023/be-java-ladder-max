cfda959e730844047826ff036b9b3e7ed9b72d99
jun <alap_u@naver.com>
2023-03-10 14:12:25 +0900

    이름을 찾아주는 메서드를 생성

--- src/main/java/kr/codesquad/ladder/domain/Names.java
+++ src/main/java/kr/codesquad/ladder/domain/Names.java
@@ -2,6 +2,7 @@ package kr.codesquad.ladder.domain;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Names {
 
@@ -15,6 +16,17 @@ public class Names {
         return new Names(names.stream()
                 .map(Name::new)
                 .collect(Collectors.toUnmodifiableList()));
+    }
+
+    public int getIndex(String name) {
+        return IntStream.range(0, names.size())
+                .filter(index -> names.get(index).isSame(name))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    public String getName(int index) {
+        return names.get(index).getName();
     }
 
     public int getSize() {
