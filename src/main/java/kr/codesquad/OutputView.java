4cec78efb95609f26f39a40926ede9484018d270
dokkisan <dokkisan.wang@gmail.com>
2023-03-10 14:59:47 +0900

    rename: 변수명 변경 (PARTICIPANTS_PROMPT -> PLAYERS_PROMPT)

--- src/main/java/kr/codesquad/OutputView.java
+++ src/main/java/kr/codesquad/OutputView.java
@@ -1,11 +1,11 @@
 package kr.codesquad;
 
 public class OutputView {
-    private final String PARTICIPANTS_PROMPT = "참여할 사람은 몇 명인가요?";
+    private final String PLAYERS_PROMPT = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private final String MAX_HEIGHT_PROMPT = "최대 사다리 높이는 몇 개인가요?";
 
     public void printParticipantsPrompt() {
-        System.out.println(PARTICIPANTS_PROMPT);
+        System.out.println(PLAYERS_PROMPT);
     }
 
     public void printMaxHeightPrompt() {
