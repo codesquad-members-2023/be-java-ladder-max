22c13fbf618ed021ac625fcb50b569e1047a2cc8
peony <peony@bluehole.net>
2023-03-10 13:33:14 +0900

    refactor 위치 변경

---
+++ src/step1/InputView.java
@@ -0,0 +1,14 @@
+package main.java.kr.codesquad.step1;
+
+import java.util.Scanner;
+
+public class InputView {
+    static Scanner scanner = new Scanner(System.in);
+    public static int inputParticipatePeople(){
+        return scanner.nextInt();
+    }
+
+    public static int inputNumTheMostLadderHeight(){
+        return scanner.nextInt();
+    }
+}
