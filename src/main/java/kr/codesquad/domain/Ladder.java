28bc1eba75c4e7c2510b8eb30d394584b1e806eb
JIHYE <jdh324@naver.com>
2023-03-10 14:17:29 +0900

    Line 클래스 구현에 따른 Ladder 클래스 리팩토링

--- src/main/java/kr/codesquad/domain/Ladder.java
+++ src/main/java/kr/codesquad/domain/Ladder.java
@@ -1,40 +1,13 @@
 package kr.codesquad.domain;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Ladder {
-    private ArrayList<ArrayList<String>> ladder = new ArrayList<>();
+    private ArrayList<Line> ladder = new ArrayList<>();
 
-    public Ladder(int verticalLine, int horizontalLine) {
-        makeLadder(verticalLine, horizontalLine);
-    }
-
-    private void makeLadder(int verticalLine, int horizontalLine) {
-        for (int i = 0; i < horizontalLine; i++) {
-            ladder.add(new ArrayList<>());
-            fillLine(i, verticalLine);
+    public Ladder(int countOfPerson, int countOfLadder) {
+        for (int i = 0; i < countOfLadder; i++) {
+            ladder.add(new Line(countOfPerson));
         }
-    }
-
-    private void fillLine(int row, int column) {
-        for (int j = 0; j < column - 1; j++) {
-            fillVerticalLine(row);
-            fillHorizontalLine(row, j);
-        }
-        fillVerticalLine(row);
-    }
-
-    private void fillVerticalLine(int row) {
-        ladder.get(row).add("|");
-    }
-
-    private void fillHorizontalLine(int row, int column) {
-        String[] str = {"-----", "     "};
-        if (column >= 1 && ladder.get(row).get((2 * column) - 1).equals("-----")) {
-            ladder.get(row).add("     ");
-            return;
-        }
-        ladder.get(row).add(str[new Random().nextInt(2)]);
     }
 }
