f29694c4fe134f91925f9a287c266a5867a723b0
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-03-10 21:54:57 +0900

    test: moveLine() 테스트 코드 작성
    사다리의 로우에서 입력된 index에서 라인의 유무에 따라 이동한 index를 제대로 출력하는지 확인하는 테스트 코드 작성

--- src/test/java/LadderRowTest.java
+++ src/test/java/LadderRowTest.java
@@ -2,15 +2,17 @@ import kr.codesquad.domain.LadderRow;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
-
 public class LadderRowTest {
-    @DisplayName("사다리 한 로우가 선을 가지고 있는지 확인")
+    @DisplayName("사다리의 한 로우에서 라인이 있으면 이동한 인덱스 아니면 현재 인덱스 출력")
     @Test
-    public void isLadderRowHasLine() {
+    public void moveLineTest() {
         LadderRow ladderRow = new LadderRow(3);
+        System.out.println(ladderRow);
 
-        assertThat(ladderRow.isRowHasLine()).isTrue();
+        System.out.println("0에서 이동한 index : " + ladderRow.moveLine(0));
+        System.out.println("1에서 이동한 index : " + ladderRow.moveLine(1));
+        System.out.println("2에서 이동한 index : " + ladderRow.moveLine(2));
+        System.out.println("3에서 이동한 index : " + ladderRow.moveLine(3));
     }
 
 }
