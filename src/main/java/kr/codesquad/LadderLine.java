fe522fa2345756aa0ec3e9293a54cc406bd89d56
sejeong00 <buttermelt@daum.net>
2023-03-10 23:33:10 +0900

    refactor 조건 확인하는 기능을 Ladder 클래스로 옮김
     LadderLine에 구현할 추상 메서드는 구현된 LadderLine 객체에 대한 정보를 전달하는 것이 맞다.
     하지만 validate()는 LadderLine 객체가 생성되기 전에 어떤 LadderLine 객체가 생성되어야 하는지 결정해주는 역할을 한다.
     이는 해당 LadderLine 객체에 대한 정보를 전달하는 것이라고 볼 수 없다.
     그러므로 이 기능은 LadderLine 을 생성하는 클래스에 포함되는 것이 맞다.

--- src/main/java/kr/codesquad/LadderLine.java
+++ src/main/java/kr/codesquad/LadderLine.java
@@ -3,41 +3,12 @@ package kr.codesquad;
 import java.util.Random;
 
 public enum LadderLine {
-    VERTICAL("|"){
-        @Override
-        boolean validate(int index) {
-            return isEven(index);
-        }
-    },
-    HORIZONTAL("-----"){
-        @Override
-        boolean validate(int index) {
-            return !isEven(index) && isPicked();
-        }
-    },
-    BLANK("     "){
-        @Override
-        boolean validate(int index) {
-            return true;
-        }
-    };
+    VERTICAL("|"), HORIZONTAL("-----"), BLANK("     ");
     
     private final String line;
 
     LadderLine(String line) {
         this.line = line;
-    }
-
-    abstract boolean validate(int index);
-
-    private static boolean isEven(int index){
-        return index%2 == 0;
-    }
-
-    private static boolean isPicked(){
-        Random rand = new Random();
-
-        return (rand.nextInt()%5) == 0;
     }
 
     public String getLine() {
