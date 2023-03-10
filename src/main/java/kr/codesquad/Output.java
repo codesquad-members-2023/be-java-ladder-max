b07e79fb7257dd164631ce4b398964b2a1160d90
sejeong00 <buttermelt@daum.net>
2023-03-11 00:05:57 +0900

    refactor 메소드 static으로 변경
    매개변수가 없거나 불변값을 매개변수로 갖는, 상태를 갖지 않는 메소드이므로 static으로 선언하는 것이 좋겠다는 피드백을 받아 static으로 수정했다.

--- src/main/java/kr/codesquad/Output.java
+++ src/main/java/kr/codesquad/Output.java
@@ -1,19 +1,16 @@
 package kr.codesquad;
 
 public class Output {
-    void printInputNumberOfPlayerQuestion(){
+    static void printInputNumberOfPlayerQuestion(){
         System.out.println("참여할 사람은 몇 명인가요?");
     }
 
-    void printInputLadderHeightQuestion(){
+    static void printInputLadderHeightQuestion(){
         System.out.println("최대 사다리 높이는 몇 개인가요?");
     }
 
-    //TODO: Ladder.print()를 여기로 옮기기 로직 없이 출력만 하도록(String을 받는 것으로)
-    //질문 : ladder를 String으로 받으면 printLadder 매개변수로 String 타입이면 다 들어갈 수 있게 돼서 안되지 않나요?
-    void printLadder(Ladder ladder){
-        System.out.println(ladder.toString());
+    static void printLadder(String ladderString){
+        System.out.println(ladderString);
     }
-
 
 }
