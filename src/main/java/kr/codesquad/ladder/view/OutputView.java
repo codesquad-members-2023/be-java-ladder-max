34cb865a730a4e763b753fe3630043dda0fc6766
jun <alap_u@naver.com>
2023-03-10 14:11:31 +0900

    실행결과를 출력함

--- src/main/java/kr/codesquad/ladder/view/OutputView.java
+++ src/main/java/kr/codesquad/ladder/view/OutputView.java
@@ -1,8 +1,17 @@
 package kr.codesquad.ladder.view;
 
+import kr.codesquad.ladder.domain.Reward;
+
 public class OutputView {
 
+    private static final String MATCHING_RESULT_MESSAGE = "실행 결과";
+
     public static void printString(String format) {
-        System.out.println(format);
+        System.out.println(format + System.lineSeparator());
+    }
+
+    public static void printMatchingResult(String result) {
+        System.out.println(MATCHING_RESULT_MESSAGE);
+        System.out.println(result + System.lineSeparator());
     }
 }
