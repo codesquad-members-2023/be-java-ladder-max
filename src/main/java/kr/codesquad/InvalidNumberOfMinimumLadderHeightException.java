73168ceea93c396bfe7334be697f24b219dc87c1
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-10 14:42:28 +0900

    modify: 클래스명 변경
    * 예외가 발생하는 상황은 사용자가 최대 사다리 높이의 입력범위중 최소 값보다 작은 값을 입력하였기 때문에 유효하지 않은 최대 사다리 높이의 숫자보다 유효하지 않은 사다리 높이의 최소값이 적합하다고 생각하여 변경하였습니다.

--- src/main/java/kr/codesquad/InvalidNumberOfMinimumLadderHeightException.java
+++ src/main/java/kr/codesquad/InvalidNumberOfMinimumLadderHeightException.java
@@ -1,10 +1,10 @@
 package kr.codesquad;
 
-public class InvalidNumberOfMaximumLadderHeightException extends RuntimeException {
+public class InvalidNumberOfMinimumLadderHeightException extends RuntimeException {
 
     private static final String message = "적절하지 않은 입력입니다. 1 이상의 사다리 높이를 입력해주세요.";
 
-    public InvalidNumberOfMaximumLadderHeightException() {
+    public InvalidNumberOfMinimumLadderHeightException() {
         super(message);
     }
 }
