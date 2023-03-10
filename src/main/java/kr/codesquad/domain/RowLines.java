789afc13d3a736b3da3d5724023fcc6120daaee4
road-jin <rjswmtk@gmail.com>
2023-03-10 14:46:55 +0900

    feat: 가로라인들을 여러개 만들어 주는 기능 추가
    모든 라인 생성후 비어있는 세로라인이 있으면 라인을 연결할 수 있는 상황에세만 랜덤으로 하나의 라인을 연결해줍니다.

--- src/main/java/kr/codesquad/domain/RowLines.java
+++ src/main/java/kr/codesquad/domain/RowLines.java
@@ -16,6 +16,26 @@ public class RowLines {
         this.rowLines = randomRowLinesGenerator.generate();
     }
 
+    public boolean findByIndex(int index) {
+        return this.rowLines.get(index);
+    }
+
+    public boolean isConnectLine(int index) {
+        if (index == 0) {
+            return !this.rowLines.get(index + 1);
+        }
+
+        if (index == this.rowLines.size() - 1) {
+            return !this.rowLines.get(index - 1);
+        }
+
+        return !this.rowLines.get(index - 1) && !this.rowLines.get(index + 1);
+    }
+
+    public void connect(int index) {
+        this.rowLines.set(index, true);
+    }
+
     @Override
     public String toString() {
         StringJoiner stringJoiner = new StringJoiner(COLUMN_LINE, PRE_FIX, COLUMN_LINE);
