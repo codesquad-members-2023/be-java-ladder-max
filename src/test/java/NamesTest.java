997d7354f771a951da4cf541a935549ab22cfef0
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-03-10 20:30:48 +0900

    refactor: Names 클래스 삭제
    Elements 클래스로 사용하게 변경되어 삭제

--- src/test/java/NamesTest.java
+++ src/test/java/NamesTest.java
@@ -1,17 +1,19 @@
-import kr.codesquad.domain.Names;
+import kr.codesquad.domain.Elements;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class NamesTest {
     @DisplayName("이름이 최대 5글자로 출력되는지 확인")
     @Test
     public void countOfNameLength() {
-        Names names = new Names(new String[]{"poby", "honux", "crong", "jk"});
-//        String nameRow = names.makeNameRow();
-//        System.out.println(nameRow);
-//
-//        // 5글자 + 공백1칸 = 6
-//        // 6 * 4 = 24
-//        assertThat(nameRow.length()).isEqualTo(24);
+        Elements names = new Elements(new String[]{"poby", "honux", "crong", "jk"});
+        String nameRow = names.toString();
+        System.out.println(nameRow);
+
+        // 5글자 + 공백1칸 = 6
+        // 6 * 4 = 24
+        assertThat(nameRow.length()).isEqualTo(24);
     }
 }
