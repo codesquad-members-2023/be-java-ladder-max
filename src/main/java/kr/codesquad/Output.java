9a25827e77ba01348207974b59c02b328bc42672
sejeong00 <buttermelt@daum.net>
2023-03-10 09:19:21 +0900

    feat: 사다리 출력 담당하는 메소드 구현

--- src/main/java/kr/codesquad/Output.java
+++ src/main/java/kr/codesquad/Output.java
@@ -9,5 +9,11 @@ public class Output {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
     }
 
+    //TODO: Ladder.print()를 여기로 옮기기 로직 없이 출력만 하도록(String을 받는 것으로)
+    //질문 : ladder를 String으로 받으면 printLadder 매개변수로 String 타입이면 다 들어갈 수 있게 돼서 안되지 않나요?
+    void printLadder(Ladder ladder){
+        System.out.println(ladder.toString());
+    }
+
 
 }
