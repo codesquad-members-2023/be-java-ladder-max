887b45ff7addcec20d24d9a61ae2fbf9354186a1
sejeong00 <buttermelt@daum.net>
2023-03-10 09:50:58 +0900

    feat: 게임 실행하는 메소드 구현

--- src/main/java/kr/codesquad/LadderGame.java
+++ src/main/java/kr/codesquad/LadderGame.java
@@ -1,5 +1,19 @@
 package kr.codesquad;
 
 public class LadderGame {
+    void run(){
+        Output output = new Output();
+        Input input =new Input();
+        Ladder ladder;
+        int numberOfPlayers;
+        int ladderHeight;
 
+        output.printInputNumberOfPlayerQuestion();
+        numberOfPlayers = input.inputNumberOfPlayers();
+        output.printInputLadderHeightQuestion();
+        ladderHeight = input.inputLadderHeight();
+
+        ladder = new Ladder(numberOfPlayers, ladderHeight);
+        output.printLadder(ladder);
+    }
 }
