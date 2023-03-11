25fed59b5cca941cb1d0679cae3d0ae86edfe61b
peony <peony@bluehole.net>
2023-03-12 02:35:00 +0900

    feat final 변수 추가

--- src/main/java/kr/codesquad/step3/view/InputView.java
+++ src/main/java/kr/codesquad/step3/view/InputView.java
@@ -3,7 +3,7 @@ package kr.codesquad.step3.view;
 import java.util.Scanner;
 
 public class InputView {
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static String inputParticipateNames(){
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
