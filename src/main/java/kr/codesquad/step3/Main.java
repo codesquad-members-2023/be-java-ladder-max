7c687f68bce736c4b0390257b3425a12a10c7d60
peony <peony@bluehole.net>
2023-03-12 02:36:27 +0900

    feat 메인 시작 클래스 추가

---
+++ src/main/java/kr/codesquad/step3/Main.java
@@ -0,0 +1,10 @@
+package kr.codesquad.step3;
+
+import kr.codesquad.step3.controller.LadderGame;
+
+public class Main {
+    public static void main(String[] args) {
+        LadderGame ladderGame = new LadderGame();
+        ladderGame.run();
+    }
+}
