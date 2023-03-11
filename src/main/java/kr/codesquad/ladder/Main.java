5a893454b859d02f0df7f874ed3cfcf48641f126
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-11 20:36:22 +0900

    style: 코드 정리

--- src/main/java/kr/codesquad/ladder/Main.java
+++ src/main/java/kr/codesquad/ladder/Main.java
@@ -2,23 +2,21 @@ package kr.codesquad.ladder;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import kr.codesquad.ladder.domain.LadderValidator;
 import kr.codesquad.ladder.domain.LadderGame;
 import kr.codesquad.ladder.view.LadderConsoleReader;
 import kr.codesquad.ladder.view.LadderConsoleWriter;
+import kr.codesquad.ladder.view.LadderReader;
 import kr.codesquad.ladder.view.LadderWriter;
 
 public class Main {
 
     public static void main(String[] args) {
         LadderWriter ladderWriter = new LadderConsoleWriter();
-        LadderConsoleReader ladderConsoleReader =
-            new LadderConsoleReader(
-                new BufferedReader(new InputStreamReader(System.in)),
-                new LadderValidator(2, 1),
-                ladderWriter
-            );
-        LadderGame game = new LadderGame(ladderConsoleReader, ladderWriter);
+        LadderReader ladderReader = new LadderConsoleReader(
+            new BufferedReader(new InputStreamReader(System.in)),
+            ladderWriter
+        );
+        LadderGame game = new LadderGame(ladderReader, ladderWriter);
         game.start();
     }
 }
