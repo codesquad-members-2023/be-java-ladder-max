d2e0c79876196dae9976ed28c2c8d9a65155b6a6
javavoja <javavoja@protonmail.com>
2023-03-11 20:06:21 +0900

    fix: 아직 수정 중
    이름을 입력받을 때 글자수를 체크하고 5글자 안 넘을 때까지 다시 물어보게 됨. (그 대신 사다리가 출력이 되지 않거나 에러가 발생 -> 더 fix 필요)

--- src/main/java/kr/codesquad/Inputer.java
+++ src/main/java/kr/codesquad/Inputer.java
@@ -8,31 +8,30 @@ public class Inputer {
     private String names;
     private int ladderNum;
 
-    public void seting() {
+    public ArrayList<String> seting() {
         setManValue();
+        ArrayList<String> temp = check();
         setLadderValue();
+        return temp;
     }
 
     private void setManValue() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표로 구분하세요)");
-        names = sc.nextLine();
     }
 
-    public ArrayList<String> getNames() {
+    ArrayList<String> check() {
         ArrayList<String> arrayList = new ArrayList<>();
-
-        do {
-            for (int i = 0; i < names.split(",").length; i++) {
-                if (names.split(",")[i].length() < 6) {
-                    arrayList.add(names.split(",")[i]);
-                }
-                if (names.split(",")[i].length() > 5) {
-                    System.out.println("이름 1개당 5글자까지만 됩니다. 다시 해주세요. (이름은 쉼표로 구분하세요)");
-                    i = 0;
-                }
+        names = sc.nextLine();
+        String[] str = names.split(",");
+        for (int i = 0; i < str.length; i++) {
+            arrayList.add(str[i]);
+            if (str[i].length() > 5) {
+                System.out.println("이름 1개당 5글자까지만 됩니다. 다시 해주세요. (이름은 쉼표로 구분하세요)");
+                str = null;
+                arrayList = null;
+                check();
             }
-        } while (arrayList.size() != names.split(",").length);
-
+        }
         return arrayList;
     }
 
