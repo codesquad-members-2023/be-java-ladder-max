97bceef900edf084b1d350762df554242debc62f
peony <peony@bluehole.net>
2023-03-12 02:28:18 +0900

    feat 참여자들에 대한 List를 위한 클래스

--- src/main/java/kr/codesquad/step3/domain/Participates.java
+++ src/main/java/kr/codesquad/step3/domain/Participates.java
@@ -1,35 +1,13 @@
 package kr.codesquad.step3.domain;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import kr.codesquad.step3.util.Util;
-import kr.codesquad.step3.util.Validation;
+import java.util.List;
+
 
 public class Participates {
     public final List<Participate> participatesNames;
 
     public Participates(List<Participate> participates) {
         this.participatesNames = participates;
-    }
-
-    public static Participates createParticipates(String participateNames){
-        List<String> eachParticipateName = makeEachParticipateNameGroup(participateNames);
-        List<Participate> participates = eachParticipateName.stream()
-                .map(Participate::new)
-                .collect(Collectors.toList());
-        return new Participates(participates);
-    }
-
-    private static List<String> makeEachParticipateNameGroup(String participateNames){
-        Validation.validateInputNames(participateNames);
-        List<String> eachParticipateName = stringToList(participateNames);
-        Validation.validateMinimumParticipates(eachParticipateName);
-        return eachParticipateName;
-    }
-
-    private static List<String> stringToList(String participateNames){
-        return Arrays.asList(Util.splitParticipateName(participateNames));
     }
 }
