54822d8317292f22e62f26c2f873c843bf1f1b5c
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-11 23:55:23 +0900

    feat: 한 라인에 열 추가 기능
    * 열을 입력받아 해당 열에 랜덤한 브릿지를 추가합니다.

--- src/main/java/kr/codesquad/ladder/domain/Line.java
+++ src/main/java/kr/codesquad/ladder/domain/Line.java
@@ -10,22 +10,16 @@ public class Line {
     private static final String BAR = "|";
 
     private final Points points;
-    private final Random random;
 
-    public Line(int numberOfPerson, Random random) {
-        this.random = random;
-        this.points = new Points();
-        for (int i = 0; i < numberOfPerson - 1; i++) {
-            points.add(buildRandomBridge(i));
-        }
-    }
-
-    public Line(Points points, Random random) {
+    public Line(Points points) {
         this.points = points;
-        this.random = random;
     }
 
-    private Point buildRandomBridge(int col) {
+    public void addPoint(int col, Random random) {
+        points.add(buildRandomBridge(col, random));
+    }
+
+    private Point buildRandomBridge(int col, Random random) {
         if (!existBridgeOnLeft(col) && random.nextBoolean()) {
             return Point.ofBridge();
         }
@@ -33,14 +27,7 @@ public class Line {
     }
 
     private boolean existBridgeOnLeft(int col) {
-        if (col == 0) {
-            return false;
-        }
-        return getLeftColumn(col).isBridge();
-    }
-
-    private Point getLeftColumn(int col) {
-        return points.get(col - 1);
+        return points.existBridgeOnLeft(col);
     }
 
     @Override
