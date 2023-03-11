f2e29602a6f0078ada1c009b1e8c92a916c46df5
peony <peony@bluehole.net>
2023-03-12 02:27:14 +0900

    feat 각 참여자 정보를 저장할 클래스

---
+++ src/main/java/kr/codesquad/step3/domain/Participate.java
@@ -0,0 +1,28 @@
+package kr.codesquad.step3.domain;
+
+import java.util.Objects;
+
+public class Participate {
+    private final String name;
+
+    public Participate(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Participate that = (Participate) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
