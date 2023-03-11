d201d76140b0cf0d1fc898c71fa1fb6c38ffacfb
ALbertIM0427 <guang7226961@naver.com>
2023-03-11 14:44:32 +0900

    검증 에러 메시지 추가

--- src/main/java/kr/codesquad/domain/InputForm.java
+++ src/main/java/kr/codesquad/domain/InputForm.java
@@ -2,18 +2,18 @@ package kr.codesquad.domain;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Range;
 
 public class InputForm {
 
     @NotBlank
-    @Pattern(regexp = "^[a-z]+(,[a-z]+)+$")
+    @Pattern(regexp = "^[a-z]+(,[a-z]+)+$",message = "영어이름을 잘못 입력했습니다.(이름은 쉼표(,)로 구분하세요)")
     private String names;
     @NotBlank
-    @Pattern(regexp = "^(꽝|([0-9]+[1-9]*))(,(꽝|([0-9]+[1-9]*)))*$")
+    @Pattern(regexp = "^(꽝|([0-9]+[1-9]*))(,(꽝|([0-9]+[1-9]*)))*$",message = "실행결과를 잘못 입력했습니다.(결과는 쉼표(,)로 구분하세요)")
     private String resultInfo;
 
-    @NotBlank
-    @Pattern(regexp = "^[1-9][0-9]*$")
+    @Range(min = 2,max = 30,message = "2~30 사이의 숫자를 입력하세요.")
     private int ladderHeight;
 
 
