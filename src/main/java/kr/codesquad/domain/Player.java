bc6488fbb5ca16612d1d915ba6d3ee47e81fada5
crtEvent <crtevn@gmail.com>
2023-03-12 00:15:32 +0900

    add: 플레이어 한 명의 정보를 저장할 Player 객체 추가
    - Players 객체의 이름들 속성을 분리하기 위함

---
+++ src/main/java/kr/codesquad/domain/Player.java
@@ -0,0 +1,13 @@
+package kr.codesquad.domain;
+
+public class Player {
+    private final String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
