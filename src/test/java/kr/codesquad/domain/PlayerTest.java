ed1271870b8da6348614f1ffcb7fcf5adcf77670
crtEvent <crtevn@gmail.com>
2023-03-13 00:47:12 +0900

    test: Player 객체 test 추가
    - 플레이어에 입력된 이름이 5글자보다 크면 예외 발생하는지 테스트
    - 플레이어에 이름을 입력하면 5글자로 출력하는지 테스트

---
+++ src/test/java/kr/codesquad/domain/PlayerTest.java
@@ -0,0 +1,31 @@
+package kr.codesquad.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+
+    private Player player;
+    private final String SYLLABLE = "s";
+    private final int MAX_NAME_LENGTH = 5;
+
+    @Test
+    @DisplayName("플레이어에 입력된 이름이 5글자보다 크면 예외 발생.")
+    void testGetNameThrowException() {
+        assertThatThrownBy(() -> new Player(SYLLABLE.repeat(MAX_NAME_LENGTH + 1)))
+                .isInstanceOf(Exception.class);
+    }
+
+    @Test
+    @DisplayName("플레이어에 이름을 입력하면 5글자로 출력한다.")
+    void testGetNameLength() {
+        for(int i = 1; i <= MAX_NAME_LENGTH; i++) {
+            player = new Player(SYLLABLE.repeat(i));
+            assertThat(player.getName().length()).isEqualTo(MAX_NAME_LENGTH);
+        }
+    }
+
+}
