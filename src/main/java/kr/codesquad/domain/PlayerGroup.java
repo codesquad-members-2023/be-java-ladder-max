81037fb924aa7229a6fb27f358b62efb83b13f0f
crtEvent <crtevn@gmail.com>
2023-03-13 00:38:51 +0900

    add: PlayerGroup가 최소 한명 이상의 플레이어를 입력할 수 있도록 예외처리 추가
    - validatePlayerNamesLength() 메서드를 추가하여 PlayerGroup의 생성자에서 검증

--- src/main/java/kr/codesquad/domain/PlayerGroup.java
+++ src/main/java/kr/codesquad/domain/PlayerGroup.java
@@ -7,9 +7,16 @@ public class PlayerGroup {
     private final Player[] players;
 
     public PlayerGroup(String[] playerNames) {
+        validatePlayerNamesLength(playerNames);
         players = Arrays.stream(playerNames)
                 .map(Player::new)
                 .toArray(Player[]::new);
+    }
+
+    private void validatePlayerNamesLength(String[] playerNames) {
+        if(playerNames.length == 0) {
+            throw new IllegalArgumentException("[ERROR] 최소 한명 이상의 플레이어를 입력해 주세요.");
+        }
     }
 
     public int getPlayerCount() {
