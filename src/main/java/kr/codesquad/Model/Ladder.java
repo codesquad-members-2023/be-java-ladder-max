963d809c4a75cd7bc3bf8c73f1449b6d8da50e91
sejeong00 <buttermelt@daum.net>
2023-03-13 00:24:11 +0900

    feat 문자열로 반환하는 메서드 구현

--- src/main/java/kr/codesquad/Model/Ladder.java
+++ src/main/java/kr/codesquad/Model/Ladder.java
@@ -1,46 +1,25 @@
 package kr.codesquad.Model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Ladder {
     final int ROW_SIZE;
     final int COLUMN_SIZE;
-    private final Bar[][] ladder;
+    private final List<Line> ladder = new ArrayList<>();
 
     public Ladder(int numberOfPlayer, int ladderHeight) {
         ROW_SIZE = ladderHeight;
         COLUMN_SIZE = numberOfPlayer * 2 - 1;
-        ladder = new Bar[ROW_SIZE][COLUMN_SIZE];
-        initLadder();
+        makeLadder();
     }
 
-    private void initLadder(){
+    private void makeLadder(){
         for(int rowIndex = 0; rowIndex < ROW_SIZE; rowIndex++){
-            initRow(rowIndex);
+            ladder.add(new Line(COLUMN_SIZE));
         }
-    }
-
-    private void initRow(int rowIndex){
-        for(int columnIndex = 0; columnIndex < COLUMN_SIZE; columnIndex++){
-            ladder[rowIndex][columnIndex] = getLadderLine(columnIndex);
-        }
-    }
-
-    private Bar getLadderLine(int index){
-        Random random = new Random();
-        boolean isVertical = index%2 == 0;
-        boolean isRandomPicked = random.nextBoolean();
-
-        if(isVertical){
-            return Bar.VERTICAL;
-        }
-
-        if(isRandomPicked){
-            return Bar.HORIZONTAL;
-        }
-
-        return Bar.BLANK;
     }
 
     @Override
@@ -48,23 +27,11 @@ public class Ladder {
         StringBuilder sb = new StringBuilder();
 
         for(int rowIndex = 0; rowIndex < ROW_SIZE; rowIndex++){
-            Arrays.stream(ladder[rowIndex])
-                    .forEach(s -> sb.append(s.getBar()));
+            sb.append(ladder.get(rowIndex).toString());
             sb.append("\n");
         }
 
         return sb.toString();
-    }
-
-
-    public void print(){
-        for(int rowIndex = 0; rowIndex < ROW_SIZE; rowIndex++){
-            for(int columnIndex = 0; columnIndex < COLUMN_SIZE; columnIndex++){
-                Bar line = ladder[rowIndex][columnIndex];
-                System.out.print(line == null ? " " : line.getBar());
-            }
-            System.out.println();
-        }
     }
 
 }
