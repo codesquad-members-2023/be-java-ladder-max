e529165731c475e9cbdefc8f412712c36f1f5eb8
sejeong00 <buttermelt@daum.net>
2023-03-13 00:11:10 +0900

    패키지 이동 후 충돌이 발생해 커밋이 끊기고 새 클래스를 생성한 것으로 되어버렸다.

---
+++ src/main/java/kr/codesquad/Model/Line.java
@@ -0,0 +1,37 @@
+package kr.codesquad.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Line {
+    final int numberOfBars;
+    List<Bar> bars = new ArrayList<>();
+
+    public Line(int numberOfBars) {
+        this.numberOfBars = numberOfBars;
+    }
+
+    private void makeLine(){
+        for(int index = 0; index < numberOfBars; index++){
+            bars.add(getLadderLine(index));
+        }
+    }
+
+    private Bar getLadderLine(int index){
+        Random random = new Random();
+
+        boolean isVertical = index%2 == 0;
+        boolean isRandomPicked = random.nextBoolean();
+
+        if(isVertical){
+            return Bar.VERTICAL;
+        }
+
+        if(isRandomPicked){
+            return Bar.HORIZONTAL;
+        }
+
+        return Bar.BLANK;
+    }
+}
