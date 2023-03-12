01b66038f3cda0d48242fde6bcf83635a7577976
sejeong00 <buttermelt@daum.net>
2023-03-13 00:07:59 +0900

    refactor 패키지 이동, 메서드 public으로 변경

--- src/main/java/kr/codesquad/View/Output.java
+++ src/main/java/kr/codesquad/View/Output.java
@@ -1,15 +1,15 @@
-package kr.codesquad;
+package kr.codesquad.View;
 
 public class Output {
-    static void printInputNumberOfPlayerQuestion(){
+    public static void printInputNumberOfPlayerQuestion(){
         System.out.println("참여할 사람은 몇 명인가요?");
     }
 
-    static void printInputLadderHeightQuestion(){
+    public static void printInputLadderHeightQuestion(){
         System.out.println("최대 사다리 높이는 몇 개인가요?");
     }
 
-    static void printLadder(String ladderString){
+    public static void printLadder(String ladderString){
         System.out.println(ladderString);
     }
 
