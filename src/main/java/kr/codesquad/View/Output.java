1394a50148aca84505de191fd8089f58c8a40686
sejeong00 <buttermelt@daum.net>
2023-03-13 01:17:45 +0900

    feat 플레이어를 입력받는 메세지를 출력하는 메서드 구현

--- src/main/java/kr/codesquad/View/Output.java
+++ src/main/java/kr/codesquad/View/Output.java
@@ -1,6 +1,10 @@
 package kr.codesquad.View;
 
 public class Output {
+
+    public static void printInputPlayerQuestion(){
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+    }
     public static void printInputNumberOfPlayerQuestion(){
         System.out.println("참여할 사람은 몇 명인가요?");
     }
