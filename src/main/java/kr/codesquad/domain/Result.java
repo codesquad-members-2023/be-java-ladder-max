fccd3bc500ebe48726df222b5712df99ae682a44
crtEvent <crtevn@gmail.com>
2023-03-14 04:35:20 +0900

    add: 게임 결과를 저장/관리하는 객체 추가
    - Result, ResultGroup

---
+++ src/main/java/kr/codesquad/domain/Result.java
@@ -0,0 +1,24 @@
+package kr.codesquad.domain;
+
+public class Result {
+    private String playerName;
+    private String destinationName;
+
+    public Result(String playerName, String destinationName) {
+        this.playerName = playerName;
+        this.destinationName = destinationName;
+    }
+
+    public boolean isPlayerName(String playerName) {
+        return this.playerName.equals(playerName);
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+}
---
+++ src/main/java/kr/codesquad/domain/ResultGroup.java
@@ -0,0 +1,38 @@
+package kr.codesquad.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResultGroup {
+
+    private final List<Result> results;
+
+    public ResultGroup() {
+        this.results = new ArrayList<>();
+    }
+
+    public void add(Result result) {
+        results.add(result);
+    }
+
+    public String getResult(String playerName) {
+        StringBuilder sb = new StringBuilder();
+        if(playerName.equals("all")) {
+            for(Result result : results) {
+                sb.append(result.getPlayerName())
+                        .append(" : ")
+                        .append(result.getDestinationName())
+                        .append(System.lineSeparator());
+            }
+            return sb.toString();
+        }
+
+        for (Result result : results) {
+            if (result.isPlayerName(playerName)) {
+                return result.getDestinationName() + System.lineSeparator();
+            }
+        }
+
+        throw new IllegalArgumentException("[ERROR] '" + playerName + "'은 존재하지 않습니다.");
+    }
+}
