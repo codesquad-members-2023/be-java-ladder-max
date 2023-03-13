4fde84779addea5979c5160cc23207710a0d1865
crtEvent <crtevn@gmail.com>
2023-03-13 15:30:33 +0900

    add: 도착지 이름 길이 유효성 검사 추가
    - 도착지 이름은 1글자 이상, 5글자 이하로 입력

--- src/main/java/kr/codesquad/domain/Destination.java
+++ src/main/java/kr/codesquad/domain/Destination.java
@@ -1,11 +1,19 @@
 package kr.codesquad.domain;
 
 public class Destination {
+    private final int MAX_NAME_LENGTH = 5;
+    private final int MIN_NAME_LENGTH = 1;
     private final String name;
 
     public Destination(String name) {
+        validateDestinationName(name);
         this.name = name;
     }
 
+    private void validateDestinationName(String name) {
+        if(name.length() > MAX_NAME_LENGTH || name.length() < MIN_NAME_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 도착지 이름은 1글자 이상, 5글자 이하로 입력해 주세요.(※ " + name + ")");
+        }
+    }
 
 }
