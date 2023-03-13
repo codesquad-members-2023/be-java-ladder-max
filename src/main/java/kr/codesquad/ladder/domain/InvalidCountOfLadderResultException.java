54fc76cc15baf739b3240071187be1ea7ef6a141
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-13 16:52:07 +0900

    feat: 사다리 결과 개수 예외 기능
    * 사용자로부터 사다리 실행 결과의 개수가 인원수와 같지 않은 경우에 발생하는 예외입니다.

---
+++ src/main/java/kr/codesquad/ladder/domain/InvalidCountOfLadderResultException.java
@@ -0,0 +1,10 @@
+package kr.codesquad.ladder.domain;
+
+public class InvalidCountOfLadderResultException extends RuntimeException {
+
+    private static final String message = "실행결과의 개수는 참여할 사람 이름수와 동일해야 합니다. 개수 : %d";
+
+    public InvalidCountOfLadderResultException(int countOfPeople) {
+        super(String.format(message, countOfPeople));
+    }
+}
