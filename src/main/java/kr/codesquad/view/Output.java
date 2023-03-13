6592601e405318bf7f476f9b8a02fbd95271b5fe
JIHYE <jdh324@naver.com>
2023-03-13 12:26:47 +0900

    결과 출력 구현

--- src/main/java/kr/codesquad/view/Output.java
+++ src/main/java/kr/codesquad/view/Output.java
@@ -5,10 +5,11 @@ import kr.codesquad.domain.Ladder;
 import java.util.ArrayList;
 
 public class Output {
-    public void print(ArrayList<String> names, Ladder ladder) {
+    public void print(ArrayList<String> names, Ladder ladder, ArrayList<String> results) {
         System.out.print("\n--------실행 결과--------\n\n");
         printNames(names);
         printLadder(ladder);
+        printResults(results);
     }
 
     private void printNames(ArrayList<String> names) {
@@ -19,5 +20,11 @@ public class Output {
 
     private void printLadder(Ladder ladder) {
         System.out.print(ladder);
+    }
+
+    private void printResults(ArrayList<String> results) {
+        String[] blank = {"  ", "  ", " ", "", ""};
+        results.forEach(result -> System.out.print(String.format("%-5s", blank[result.length() - 1] + result) + " "));
+        System.out.println();
     }
 }
