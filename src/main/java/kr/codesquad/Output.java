6f7efd4911eaa139e5453cca4727ff2040a7a724
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-13 15:15:18 +0900

    Feat($Output): 출력관련 기능 변경
    이중 array에서 2중 list로 상태가 변경되어 이에 맞게 메서드를
    변경했습니다.

--- src/main/java/kr/codesquad/Output.java
+++ src/main/java/kr/codesquad/Output.java
@@ -1,36 +1,44 @@
 package kr.codesquad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Output {
+    private static final int PLAYER_MAX_LENGTH = 5;
     private static final String POLE = "|";
-    private static final String RUNG = " - ";
-    private static final String BLANK = "   ";
-    
-    public void printLadder(boolean[][] rungs) {
-        for(boolean[] row : rungs) {
-            System.out.println(writeOneRow(row));
+    private static final String RUNG = "-".repeat(5);
+    private static final String BLANK = " ".repeat(5);
+
+    public void printPlayers(List<String> players) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String player : players) {
+            sb.append(String.format("%-6s", player));
         }
+        System.out.println(sb);
     }
 
-    private String writeOneRow(boolean[] rungRow) {
+    public List<String> lineToString(List<List<Boolean>> lineInformation) {
+        List<String> shape = new ArrayList<>();
+
+        for (List l : lineInformation) {
+            shape.add(encapsulateLine(l));
+        }
+        return shape;
+    }
+    private String encapsulateLine(List<Boolean> line) {
         StringBuilder sb = new StringBuilder();
 
         sb.append(POLE);
-        for(boolean rung : rungRow) {
-            sb.append(convertRungToString(rung));
+        for (boolean bool : line) {
+            if (bool) {
+                sb.append(RUNG);
+                sb.append(POLE);
+                continue;
+            }
+            sb.append(BLANK);
             sb.append(POLE);
         }
         return sb.toString();
-    }
-
-    private String convertRungToString(boolean rung) {
-        if (hasRung(rung)) {
-            return RUNG;
-        }
-        return BLANK;
-    }
-
-    private boolean hasRung(boolean rung) {
-        if(rung) return true;
-        return false;
     }
 }
