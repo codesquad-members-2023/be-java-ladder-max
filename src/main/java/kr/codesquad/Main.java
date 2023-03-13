f67c463045464cfa24edb885fcba376c8ac3a41d
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-13 15:16:21 +0900

    Feat(Main): 메인 메서드 변경
    만들어진 코드에 맞게 메인메서드를 변경했습니다.

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,13 +1,19 @@
 package kr.codesquad;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Input input = new Input();
-        int players = input.inputPlayers();
-        int height = input.inputHeight();
+        InputHandler handler = new InputHandler();
+        List<String> players = handler.inputValidatedPlayers();
+        int height = handler.inputValidatedHeight();
         Ladder ladder = new Ladder(players, height);
-        Output output= new Output();
-
-        output.printLadder(ladder.getRungs());
+        List<List<Boolean>> ladderInformation = ladder.informLadder();
+        Output output = new Output();
+        List<String> ladderShape = output.lineToString(ladderInformation);
+        output.printPlayers(players);
+        for (String shape : ladderShape) {
+            System.out.println(shape.toString());
+        }
     }
 }
