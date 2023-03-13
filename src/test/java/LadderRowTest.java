326527666ccb154481d81e20d63bb47542222306
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-03-13 17:59:11 +0900

    test: 사다리 로우의 한 위치에서 라인 유무에 따른 이동 위치 테스트 코드 작성
    랜덤으로 생성되는 사다리 로우를 만들어서 각 라인 String을 구하고 라인 유무에 따라서 특정 index에서 이동할 수 있는 위치를 테스트한다.

--- src/test/java/LadderRowTest.java
+++ src/test/java/LadderRowTest.java
@@ -2,17 +2,56 @@ import kr.codesquad.domain.LadderRow;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class LadderRowTest {
-    @DisplayName("사다리의 한 로우에서 라인이 있으면 이동한 인덱스 아니면 현재 인덱스 출력")
     @Test
+    @DisplayName("사다리 로우의 한 위치에서 라인 유무에 따른 이동 위치 테스트")
     public void moveLineTest() {
         LadderRow ladderRow = new LadderRow(3);
-        System.out.println(ladderRow);
+        String ladderString = ladderRow.toString();
+        System.out.println(ladderString);
 
-        System.out.println("0에서 이동한 index : " + ladderRow.moveLine(0));
-        System.out.println("1에서 이동한 index : " + ladderRow.moveLine(1));
-        System.out.println("2에서 이동한 index : " + ladderRow.moveLine(2));
-        System.out.println("3에서 이동한 index : " + ladderRow.moveLine(3));
+        String[] arr = ladderString.split("\\|");
+        String first = arr[1];
+        String second = arr[2];
+        String third = arr[3];
+
+        // 0에서 이동하는 위치
+        if(first.equals("-----")){
+            assertThat(ladderRow.moveLine(0)).isEqualTo(1);
+        } else {
+            assertThat(ladderRow.moveLine(0)).isEqualTo(0);
+        }
+
+        // 1에서 이동하는 위치
+        if(first.equals("-----")){
+            assertThat(ladderRow.moveLine(1)).isEqualTo(0);
+        } else {
+            if(second.equals("-----")) {
+                assertThat(ladderRow.moveLine(1)).isEqualTo(2);
+            } else {
+                assertThat(ladderRow.moveLine(1)).isEqualTo(1);
+            }
+        }
+
+        // 2에서 이동하는 위치
+        if(second.equals("-----")){
+            assertThat(ladderRow.moveLine(2)).isEqualTo(1);
+        } else {
+            if(third.equals("-----")) {
+                assertThat(ladderRow.moveLine(2)).isEqualTo(3);
+            } else {
+                assertThat(ladderRow.moveLine(2)).isEqualTo(2);
+            }
+        }
+
+        // 3에서 이동하는 위치
+        if(third.equals("-----")){
+            assertThat(ladderRow.moveLine(3)).isEqualTo(2);
+        } else {
+            assertThat(ladderRow.moveLine(3)).isEqualTo(3);
+        }
     }
 
 }
