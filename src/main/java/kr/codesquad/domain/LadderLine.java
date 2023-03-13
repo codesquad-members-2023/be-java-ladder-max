9950cbbf7008ec2b98b123cda723db34a267d48b
code-tree <dltjdqls1200@naver.com>
2023-03-13 15:55:22 +0900

    [feat]: LadderLine에서 라인 검증 로직 추가

--- src/main/java/kr/codesquad/domain/LadderLine.java
+++ src/main/java/kr/codesquad/domain/LadderLine.java
@@ -7,7 +7,28 @@ public class LadderLine {
     private final List<LadderPart> ladderParts;
 
     public LadderLine(List<LadderPart> ladderParts) {
+        validateLine(ladderParts);
         this.ladderParts = ladderParts;
+    }
+
+    private void validateLine(List<LadderPart> ladderParts) {
+        final int maxWidth = ladderParts.size();
+
+        for (int width = 0; width < maxWidth; width++) {
+            validatePart(ladderParts, width);
+        }
+    }
+
+    private void validatePart(List<LadderPart> ladderParts, int width) {
+        if (width % 2 == 0 && ladderParts.get(width) != LadderPart.BAR) {
+            throw new IllegalArgumentException();
+        }
+        if (width % 2 == 1 && ladderParts.get(width) == LadderPart.BAR) {
+            throw new IllegalArgumentException();
+        }
+        if (width > 2 && ladderParts.get(width - 2) == ladderParts.get(width)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     @Override
