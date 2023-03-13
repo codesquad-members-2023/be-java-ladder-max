97ebeefd94df48f523fed182c47b27fc903cfb5d
adorableee <yejinh_h@naver.com>
2023-03-14 01:00:44 +0900

    feat : 한줄씩 사다리를 만들어내는 클래스를 구현
    Ladder 클래스의 이차원 배열을 분리해 ArrayList로 바꾸기 위해LadderLine 클래스를 생성

--- src/main/java/kr/codesquad/Ladder.java
+++ src/main/java/kr/codesquad/Ladder.java
@@ -1,51 +1,23 @@
 package kr.codesquad;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.stream.Collectors;
-
 public class Ladder {
-    private StringBuilder sb = new StringBuilder();
     int countOfPerson;
     int height;
-    String figureLadder[][];
     // n과 m 받아서 for문 만들기
     // n은 참여할 사람 , m은 사다리의 높이
     public Ladder(int countOfPerson, int height) {
         this.countOfPerson = countOfPerson;
         this.height = height;
-        this.figureLadder = new String[height][countOfPerson+countOfPerson-1];
+//        this.ladderFeature(); // 메서드도 넣어도된다 !! -> 생성자에 메서드를 넣으면 주소값이 전달된다. -> toString 으로 오버라이드
     }
-        private void ladderFeature() {
-        for (int i = 0; i < height; i++) {
-            verticalLadder(i);
-            horizontalLadder(i);
-        }
-    }
-    private void verticalLadder(int row) {     // 짝수 column 에 | 넣기
-        for (int i = 0; i < countOfPerson*2 -1; i+=2) {
-            figureLadder[row][i] = "|";
-        }
-    }
-    private void horizontalLadder(int row) {  // 홀수 column 에 random 으로 - 넣기
-        for (int i = 1; i < countOfPerson*2 -1 ; i+=2) {
-            figureLadder[row][i] = randomLadder();
-        }
-    }
-    private String randomLadder() {
-        String[] ladderRandom = {" ", "-"};
-        Random random = new Random();
-        return ladderRandom[random.nextInt(2)];
-    }
-
     @Override
-    public String toString() {    // 한줄로 만들어진 column 을 모아서 StringBuilder 로 사다리의 길이 만큼 합쳐 만들기
+    public String toString(){
         StringBuilder sbr = new StringBuilder();
-        ladderFeature();
         for (int i = 0; i < height; i++) {
-            String collect = Arrays.stream(figureLadder[i]).collect(Collectors.joining());
-            sbr.append(collect).append("\n");
+            sbr.append(new LadderLine(countOfPerson));
+            sbr.append("\n");
         }
         return sbr.toString();
     }
 }
+
---
+++ src/main/java/kr/codesquad/LadderLine.java
@@ -0,0 +1,44 @@
+package kr.codesquad;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class LadderLine {
+// 멤버 변수로 ArrayList 만들기
+    // 한줄씩 사다리 만들어내기
+    List<String> ladder;
+    int countOfPerson;
+    public LadderLine (int countOfPerson) {
+        this.countOfPerson = countOfPerson;
+        this.ladder = remakeLadder();
+    }
+    public List<String> remakeLadder() {
+        List<String> ladder = new ArrayList<>();
+        for (int i = 0; i < countOfPerson- 1; i++) {
+            ladder.add(randomLadder(i, ladder));
+        }
+        return ladder;
+    }
+    private String randomLadder(int index, List<String> ladder) {
+        String[] indexRandom = {"      ", "------"};
+        Random random = new Random();
+        if (index == 0) {
+            return indexRandom[random.nextInt(2)];
+        }
+        if (ladder.get(index-1).equals("      ")) {
+            return indexRandom[random.nextInt(2)];
+        }
+        return "      ";
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("|");
+        for (int i = 0; i < ladder.size(); i++) {
+            sb.append(ladder.get(i))
+                    .append("|");
+        }
+        return sb.toString();
+    }
+}
