c38a9d67c3b1ed1ec4a960900b8e4cf884682cd0
code-tree <dltjdqls1200@naver.com>
2023-03-13 11:24:59 +0900

    [refactor]: LadderGame에서 맵 초기화하고 사다리 라인, 사다리 생성하도록 수정

--- src/main/java/kr/codesquad/domain/LadderLine.java
+++ src/main/java/kr/codesquad/domain/LadderLine.java
@@ -1,43 +1,13 @@
 package kr.codesquad.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LadderLine {
-    private final List<LadderPart> ladderParts = new ArrayList<>();
+    private final List<LadderPart> ladderParts;
 
-    public LadderLine(int playerNumber) {
-        if (playerNumber < 1) {
-            throw new IllegalArgumentException("사다리 라인을 만들 수 없습니다.");
-        }
-
-        addParts(playerNumber * 2 - 1);
-    }
-
-    private void addParts(int maxWidth) {
-        for (int width = 0; width < maxWidth; width++) {
-            ladderParts.add(decidePart(width));
-        }
-    }
-
-    private LadderPart decidePart(int width) {
-        if (isBarPosition(width)) {
-            return LadderPart.BAR;
-        }
-        if (isExistBridgeOnLeft(width)) {
-            return LadderPart.EMPTY;
-        }
-
-        return LadderPart.makeRandomBridge();
-    }
-
-    private boolean isBarPosition(int x) {
-        return x % 2 == 0;
-    }
-
-    private boolean isExistBridgeOnLeft(int x) {
-        return x > 2 && ladderParts.get(x - 2) == LadderPart.BRIDGE;
+    public LadderLine(List<LadderPart> ladderParts) {
+        this.ladderParts = ladderParts;
     }
 
     @Override
--- src/main/java/kr/codesquad/service/LadderGame.java
+++ src/main/java/kr/codesquad/service/LadderGame.java
@@ -1,23 +1,81 @@
 package kr.codesquad.service;
 
 import kr.codesquad.domain.Ladder;
+import kr.codesquad.domain.LadderLine;
+import kr.codesquad.domain.LadderPart;
 import kr.codesquad.view.Screen;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class LadderGame {
     private final Screen screen;
+    private Ladder ladder;
 
     public LadderGame(Screen screen) {
         this.screen = screen;
     }
 
+    public void makeLadder(int playerNumber, int height) {
+        final List<LadderLine> ladderLines = new ArrayList<>();
+
+        for (int i = 0; i < height; i++) {
+            ladderLines.add(makeLadderLine(playerNumber));
+        }
+
+        this.ladder = new Ladder(ladderLines);
+    }
+
+    private LadderLine makeLadderLine(int playerNumber) {
+        final List<LadderPart> ladderParts = new ArrayList<>();
+        int maxWidth = playerNumber * 2 - 1;
+
+        for (int width = 0; width < maxWidth; width++) {
+            ladderParts.add(makeLadderPart(width));
+        }
+
+        checkLadderParts(ladderParts);
+
+        return new LadderLine(ladderParts);
+    }
+
+    private LadderPart makeLadderPart(int width) {
+        if (width % 2 == 0) {
+            return LadderPart.BAR;
+        }
+
+        return LadderPart.makeRandomBridge();
+    }
+
+    private void checkLadderParts(List<LadderPart> ladderParts) {
+        int maxWidth = ladderParts.size();
+
+        for (int width = 1; width < maxWidth; width += 2) {
+            removeConnectedBridge(ladderParts, width);
+        }
+    }
+
+    private void removeConnectedBridge(List<LadderPart> ladderParts, int width) {
+        if (width + 2 >= ladderParts.size()) {
+            return;
+        }
+        if (isConnectedBridge(ladderParts, width)) {
+            ladderParts.remove(width + 2);
+            ladderParts.add(width + 2, LadderPart.EMPTY);
+        }
+    }
+
+    private boolean isConnectedBridge(List<LadderPart> ladderParts, int width) {
+        return ladderParts.get(width) == ladderParts.get(width + 2);
+    }
+
+
     public void run() {
         final List<String> playerNames = inputPlayerNames();
         final int height = inputLadderHeight();
 
-        final Ladder ladder = new Ladder(playerNames.size(), height);
+        makeLadder(playerNames.size(), height);
 
         showResult(playerNames, ladder);
     }
