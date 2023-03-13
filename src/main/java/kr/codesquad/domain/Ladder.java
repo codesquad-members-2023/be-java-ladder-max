1aa7ef8c7d48c4b29819362a43335e95d3e7a965
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-03-13 09:52:44 +0900

    refactor: 변수명 오타 수정
    radder -> ladder, radderResult -> ladderResult로 오타 수정

--- src/main/java/kr/codesquad/domain/Ladder.java
+++ src/main/java/kr/codesquad/domain/Ladder.java
@@ -6,15 +6,15 @@ import java.util.List;
 public class Ladder {
     private Elements names;
     private Elements results;
-    private List<LadderRow> radder = new ArrayList<>();
-    private List<Integer> radderResult = new ArrayList<>();
+    private List<LadderRow> ladder = new ArrayList<>();
+    private List<Integer> ladderResult = new ArrayList<>();
 
     public Ladder(String nameData, String resultData, int height) {
         names = new Elements(nameData.split(","));
         results = new Elements(resultData.split(","));
 
         for (int i = 0; i < height; i++) {
-            radder.add(new LadderRow(names.size() - 1));
+            ladder.add(new LadderRow(names.size() - 1));
         }
     }
 
@@ -25,11 +25,11 @@ public class Ladder {
     }
 
     public void moveDown(int index) {
-        for (int i = 0; i < radder.size(); i++) {
-            LadderRow row = radder.get(i);
+        for (int i = 0; i < ladder.size(); i++) {
+            LadderRow row = ladder.get(i);
             index = row.moveLine(index);
         }
-        radderResult.add(index);
+        ladderResult.add(index);
     }
 
     public void printRadder() {
@@ -38,8 +38,8 @@ public class Ladder {
         // 이름 row
         sb.append(names + "\n");
         // 사다리 row
-        for (int i = 0; i < radder.size(); i++) {
-            sb.append(radder.get(i));
+        for (int i = 0; i < ladder.size(); i++) {
+            sb.append(ladder.get(i));
             sb.append("\n");
         }
         // 결과 row
@@ -50,7 +50,7 @@ public class Ladder {
     public void printPlayerResult(String name) {
         StringBuilder sb = new StringBuilder();
         sb.append("\n실행 결과\n");
-        sb.append(results.get(radderResult.get(names.indexOf(name))) + "\n");
+        sb.append(results.get(ladderResult.get(names.indexOf(name))) + "\n");
         System.out.println(sb);
     }
 
@@ -58,7 +58,7 @@ public class Ladder {
         StringBuilder sb = new StringBuilder();
         sb.append("실행 결과\n");
         for (int i = 0; i < names.size(); i++) {
-            sb.append(names.get(i) + " : " + results.get(radderResult.get(i)) + "\n");
+            sb.append(names.get(i) + " : " + results.get(ladderResult.get(i)) + "\n");
         }
         System.out.println(sb);
     }
