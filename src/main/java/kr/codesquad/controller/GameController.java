d2366f6a5213cc1aa0b56af89621b4b33e72aef7
crtEvent <crtevn@gmail.com>
2023-03-14 04:39:58 +0900

    feat: GameController 에서 사다리 게임 실행결과를 출력하도록 변경

--- src/main/java/kr/codesquad/controller/GameController.java
+++ src/main/java/kr/codesquad/controller/GameController.java
@@ -1,7 +1,6 @@
 package kr.codesquad.controller;
 
-import kr.codesquad.domain.Ladder;
-import kr.codesquad.domain.PlayerGroup;
+import kr.codesquad.domain.*;
 import kr.codesquad.view.InputView;
 import kr.codesquad.view.OutputView;
 
@@ -13,9 +12,32 @@ public class GameController {
 
     public void runLadderGame() throws IOException {
         PlayerGroup playerGroup = new PlayerGroup(inputView.inputPlayerNames());
+        DestinationGroup destinationGroup = new DestinationGroup(inputView.inputDestinationNames());
         Ladder ladder = new Ladder(playerGroup.getPlayerCount(), inputView.inputLadderHeight());
+        ResultGroup resultGroup = makeResultGroup(playerGroup, destinationGroup, ladder);
         outputView.printPlayerNames(playerGroup);
         outputView.printLadder(ladder);
+        outputView.printDestination(destinationGroup);
+
+        while(true) {
+            try {
+                outputView.printResult(resultGroup, inputView.inputSelectPlayer());
+            } catch(IllegalArgumentException e) {
+                System.out.println(e.getMessage() + System.lineSeparator());
+            }
+        }
+    }
+
+    private ResultGroup makeResultGroup(PlayerGroup playerGroup, DestinationGroup destinationGroup, Ladder ladder) {
+        ResultGroup resultGroup = new ResultGroup();
+        int playerCount = playerGroup.getPlayerCount();
+
+        for(int i = 0; i < playerCount; i++) {
+            int resultIndex = ladder.climbDownLadder(i);
+            resultGroup.add(new Result(playerGroup.getName(i), destinationGroup.getName(resultIndex)));
+        }
+
+        return resultGroup;
     }
 
 }
