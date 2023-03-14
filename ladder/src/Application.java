4526b0b3e86b4184d523400d380299de65dc702d
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-03-14 22:59:57 +0900

    기타 파일

--- ladder/src/Application.java
+++
@@ -1,9 +0,0 @@
-public class Application {
-    public static void main(String[] args){
-        ladderController ladderController = new ladderController();
-        ladderController.getInput();
-        ladderController.createLadder();
-        ladderController.testPrintLadder();
-
-    }
-}
--- ladder/src/UserInput.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class UserInput {
-
-    public int integerInput(){
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
-    }
-}
