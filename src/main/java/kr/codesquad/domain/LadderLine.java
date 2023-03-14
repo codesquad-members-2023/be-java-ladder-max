48fd8c33ac360f4a272c756a1af385f91bcb86fb
pinetree <eodnjs568@gmail.com>
2023-03-14 23:12:41 +0900

    Feat "LadderLine 클래스 메서드 수정"
    Ladder 클래스에 있던 메서드들을 클래스에 맞게 LadderLine 내에 둠

--- src/main/java/kr/codesquad/domain/LadderLine.java
+++ src/main/java/kr/codesquad/domain/LadderLine.java
@@ -3,9 +3,10 @@ package kr.codesquad.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class LadderLine {
-    private List<String> ladderline;
+    private final List<String> ladderline;
 
 
     LadderLine() {
@@ -15,6 +16,13 @@ public class LadderLine {
     public List<String> makeLadderLine(int numberOfPeople) {
         for (int column = 0; column < 2 * numberOfPeople - 1; column++) {
             makeBasicLadderRow(column);
+            /*
+            재귀적으로 호출?
+            아니면, try-catch문?
+
+             */
+            if(verifyLadderLine(column));
+
         }
         return ladderline;
     }
@@ -25,5 +33,32 @@ public class LadderLine {
             return;
         }
         ladderline.add("     ");
+    }
+
+    /*
+    ladderLine에서 "-----"가 있는지 확인하고, 없으면 makeRandomLadderLine을 호출한다.
+     */
+    private boolean verifyLadderLine(int column) {
+        if (!ladderline.contains("-----")) {
+            makeRandomLadderLine(column);
+            return true;
+        }
+        return false;
+    }
+
+    private void makeRandomLadderLine(int column) {
+        Random random = new Random();
+        if (column % 2 != 0 && random.nextBoolean()) {
+            ladderline.set(column, "-----");
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (String s : ladderline) {
+            sb.append(s);
+        }
+        return sb.toString();
     }
 }
