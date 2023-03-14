62138e4841ff43db4296b344db8f862f64af309f
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-14 16:34:51 +0900

    Refactor(): Input 클래스 리팩토링
    필요없다고 생각했던 InputHandler과 Validator를 삭제하고
    삭제한 클래스의 기능들을 Input클래스에 넣어줬습니다.

--- src/main/java/kr/codesquad/Input.java
+++ src/main/java/kr/codesquad/Input.java
@@ -5,13 +5,37 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Input {
+    private static final int PLAYER_MAX_LENGTH = 5;
     private final Scanner sc = new Scanner(System.in);
 
-    public List<String> inputPlayers() {
+    public List<String> inputValidatedPlayers() {
+        String[] players = null;
+
+        try {
+            players = inputPlayers();
+            validatePlayersLength(players);
+        }
+        catch (RuntimeException e){
+            System.out.println(e.getMessage());
+            inputValidatedPlayers();
+        }
+        return Arrays.asList(players);
+    }
+
+    private String[] inputPlayers() {
         System.out.println("참가자 명부를 주시오");
         String[] splitPlayers = this.sc.nextLine().split(",");
 
-        return Arrays.asList(splitPlayers);
+        return splitPlayers;
+    }
+
+    private void validatePlayersLength(String[] players) {
+        Arrays.stream(players)
+                .forEach(s -> {
+                    if (s.length() > PLAYER_MAX_LENGTH) {
+                        throw new RuntimeException("참가자 이름이 너무 길잖소. 최대 글자수는 " + PLAYER_MAX_LENGTH + "이오.");
+                    }
+                });
     }
 
     public int inputHeight() {
