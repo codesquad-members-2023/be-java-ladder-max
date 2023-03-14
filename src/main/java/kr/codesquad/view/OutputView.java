814798ba5367fccdf9a342becf66fb539ff6b3fa
pinetree <eodnjs568@gmail.com>
2023-03-14 23:49:21 +0900

    feat "OutputView 내 printLadder 메서드 수정"
    Ladder Class의 인스턴스를 매개변수로 받아 바로 출력하는 형식으로 변경
    이는 Ladder Class에서 toString을 오버라이드 했기떄문에 가능

--- src/main/java/kr/codesquad/view/OutputView.java
+++ src/main/java/kr/codesquad/view/OutputView.java
@@ -1,27 +1,36 @@
 package kr.codesquad.view;
 
-import java.util.Arrays;
+import kr.codesquad.domain.Ladder;
+import kr.codesquad.domain.LadderLine;
+
 import java.util.List;
 
-public class Outputview {
+public class OutputView {
 
-    public void printLadder(List<List<String>> ladder){
-        for(List<String> ladderRow : ladder){
-            for (String s : ladderRow) {
-                System.out.print(s);
-            }
-            System.out.println();
+    public void printLadder(Ladder ladder) {
+        System.out.println(ladder);
+//        for (LadderLine ladderRow : ladder) {
+////            for (String s : ladderRow) {
+////                System.out.print(s);
+////            }
+//            System.out.println();
         }
-    }
 
-    public void printPeopleList(List<String> nameList){
+
+    /*
+    정렬하기 위한 메서드 추ㅣㅁ
+     */
+
+//    public static String centerString(int width, String s) {
+//        return String.format("%-" + width + "s", String.format("%" + (s.length() + (width - s.length()) / 2) + "s", s));
+//    }
+
+    public void printPeopleList(List<String> nameList) {
         for (int i = 0; i < nameList.size(); i++) {
-            System.out.print(nameList.get(i)+" ");
+            System.out.print(nameList.get(i) + " ");
         }
         System.out.println();
     }
-
-
 
 
 }
