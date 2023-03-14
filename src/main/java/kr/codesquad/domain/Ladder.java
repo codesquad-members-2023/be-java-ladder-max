061528987358d1dd6e665dacfee971feb8afc819
pinetree <eodnjs568@gmail.com>
2023-03-14 23:11:47 +0900

    fix "Ladder 클래스와 LadderLine 클래스 분리"
    Ladder 클래스에서 LadderLine을 일급컬렉션으로 멤버변수로 갖고서 makeLadder
    메서드 하나로 호출.

--- src/main/java/kr/codesquad/domain/Ladder.java
+++ src/main/java/kr/codesquad/domain/Ladder.java
@@ -1,64 +1,45 @@
 package kr.codesquad.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class Ladder {
 
 
-    private List<LadderRow> ladder;
-    private static final int LADDER_LNE_LENGTH = 5;
+    private final List<LadderLine> ladderLines;
 
 
     public Ladder() {
-        // 생성자 통해 ladder 배열 초기
-        this.ladder = new ArrayList<>();
+        this.ladderLines = new ArrayList<>();
+
     }
 
-    public List<LadderRow> makeLadder(int peopleNumber, int ladderHeight) {
-        makeLadderRow(peopleNumber, ladderHeight);
-        return this.ladder;
-    }
-
-    private void makeLadderRow(int peopleNumber, int ladderHeight) {
+    public List<LadderLine> makeLadder(int numberOfPeople, int ladderHeight) {
         for (int row = 0; row < ladderHeight; row++) {
-            List<String> ladderRow = new ArrayList<>();
-            makeLadderColumn(peopleNumber, ladderRow);
-            ladder.add(ladderRow);
+            LadderLine ladderLine = new LadderLine();
+            ladderLine.makeLadderLine(numberOfPeople);
+            ladderLines.add(ladderLine);
         }
+        return this.ladderLines;
     }
 
-    private void makeLadderColumn(int peoleNumber, List<String> ladderRow) {
-        for (int column = 0; column < 2 * peoleNumber - 1; column++) {
-            makeBasicPeopleLine(ladderRow, column);
-            verifyLadderLine(ladderRow, column);
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (LadderLine ladderLine : ladderLines) {
+            sb.append(ladderLine.toString());
+            sb.append("\n");
         }
+        return sb.toString();
     }
 
-    private void verifyLadderLine(List<String> ladderRow, int column) {
-        if (!ladderRow.contains("-----")) {
-            makeRandomLadderLine(ladderRow, column);
-        }
-    }
+    public static void main(String[] args) {
+        Ladder ladder = new Ladder();
+        ladder.makeLadder(4,5);
+        System.out.println(ladder);
 
-    private void makeBasicPeopleLine(List<String> ladderRow, int column) {
-        if (column % 2 == 0) {
-            ladderRow.add("|");
-            return;
-        }
-        ladderRow.add("     ");
-    }
-
-    private void makeRandomLadderLine(List<String> ladderRow, int column) {
-        Random random = new Random();
-        if (column % 2 != 0 && random.nextBoolean()) {
-            ladderRow.set(column, "-----");
-        }
-    }
-
-    public List<LadderRow> getLadder() {
-        return ladder;
     }
 
 
