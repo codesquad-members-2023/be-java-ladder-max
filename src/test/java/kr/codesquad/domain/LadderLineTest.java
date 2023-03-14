11c6821a7728ca516f1f00cf5a7aeaa9da81d6ba
pinetree <eodnjs568@gmail.com>
2023-03-14 22:22:12 +0900

    feat "LadderLine Test 클래스 생성"
    LadddrLine의makeLadderLine 클래스를 number 매개변수로 받아 생성 시,
    ladderLine의 Element개수가 기대와 동일하게 생성되는지 확인하는 테스트 코드 작성

--- src/test/java/kr/codesquad/domain/LadderLineTest.java
+++ src/test/java/kr/codesquad/domain/LadderLineTest.java
@@ -1,4 +1,27 @@
+package kr.codesquad.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class LadderLineTest {
-  
+    @DisplayName("LadderLine의 열 개수 체크하는 Test 코드 작성")
+    @Test
+    void ladderLineTest(){
+        //given
+        int number = 4;
+        //4가 입력되었으므로 4*2 -1 = 7
+        LadderLine ladderLine = new LadderLine();
+
+        //when
+        List<String> ladderLineTest = ladderLine.makeLadderLine(number);
+
+        //then
+        org.assertj.core.api.Assertions.assertThat(ladderLineTest.size()).isEqualTo(7);
+    }
+
 }
