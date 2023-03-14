c8638c971b16af27bf18745efdf565b98feb394c
he2joo <hottestej6@gamil.com>
2023-03-14 18:03:20 +0900

    feat : player 이름 출력메서드와 사다리 출력 메서드 추가
    LadderGame에 pkayers의 이름을 간격에 맞춰 출력하는 메서드와 사다리를 출력하는 메서드를 추가했다.

--- src/main/java/kr/codesquad/domain/LadderGame.java
+++ src/main/java/kr/codesquad/domain/LadderGame.java
@@ -5,8 +5,34 @@ public class LadderGame {
     public void start(String players, int row){
         this.players = players.split(",");
         Ladder ladder = new Ladder(row,this.players.length);
-        ladder.make();
+        printPlayers();
+        printLadder(ladder.make());
     }
-    private void printLadder(){}
-    private void printPlayers(){}
+    private void printLadder(String ladder){
+        System.out.println(ladder);
+    }
+    private void printPlayers(){
+        // 첫번째 플레이어 이름 출력
+        System.out.print(players[0]);
+        System.out.print(" ".repeat(6-players[0].length()));
+
+        // 가운데 플레이어 이름 출력
+        int endIdx = players.length-1;
+        for (int i=1;i<endIdx;i++){
+            String str = players[i];
+            int space = (5-str.length());
+            System.out.print(" ".repeat(space/2));
+            System.out.print(str);
+            System.out.print(" ".repeat((space-space/2)));
+            System.out.print(" ".repeat(3));
+        }
+        // 마지막 플레이어 이름 출력
+        if (players[endIdx].length() < 3){
+            System.out.print(" ".repeat(3-players[endIdx].length()));
+            System.out.println(players[endIdx]);
+            return;
+        }
+        System.out.println(players[endIdx]);
+
+    }
 }
