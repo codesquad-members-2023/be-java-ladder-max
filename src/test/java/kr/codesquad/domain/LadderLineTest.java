4a41ab3a6f67cd9f8f9fcd0610df9824bd6e2ea5
pinetree <eodnjs568@gmail.com>
2023-03-15 00:06:51 +0900

    feat "LadderLineTest Class 생성"
    ladderLineTest를 통해 테스트 코드 작성

--- src/test/java/kr/codesquad/domain/LadderLineTest.java
+++ src/test/java/kr/codesquad/domain/LadderLineTest.java
@@ -1,12 +1,9 @@
 package kr.codesquad.domain;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class LadderLineTest {
     @DisplayName("LadderLine의 열 개수 체크하는 Test 코드 작성")
@@ -15,10 +12,10 @@ class LadderLineTest {
         //given
         int number = 4;
         //4가 입력되었으므로 4*2 -1 = 7
-        LadderLine ladderLine = new LadderLine();
+        LadderRow ladderLine = new LadderRow();
 
         //when
-        List<String> ladderLineTest = ladderLine.makeLadderLine(number);
+        List<String> ladderLineTest = ladderLine.makeLadderRow(number);
 
         //then
         org.assertj.core.api.Assertions.assertThat(ladderLineTest.size()).isEqualTo(7);
