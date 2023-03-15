9ee712644c23e7728be6dec235c419526ee3c340
he2joo <hottestej6@gamil.com>
2023-03-16 00:25:56 +0900

    feat : Line클래스의 테스트클래스 작성
    Line클래스에서 사다리의 가로 줄이 연속으로 나오는지, 열이 맞게 출력되는지 확인하는 테스트 메서드를 작성했다.

--- src/test/java/LineTest.java
+++ src/test/java/LineTest.java
@@ -1,2 +1,26 @@
+import kr.codesquad.domain.Line;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class LineTest {
+    @Test
+    @DisplayName("사다리에 가로줄이 연속으로 나오지 않는지 검사")
+    void testMakeLine() {
+        Line line = new Line(11);
+        String str = line.toString();
+
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(str).doesNotContain("-----|-----");
+        softly.assertAll();
+    }
+    @Test
+    @DisplayName("Line의 열이 맞게 출력되는지 확인")
+    void testColumn() {
+        Line line = new Line(11);
+        String str = line.toString();
+        String[] columns = str.split("\\|");
+        Assertions.assertEquals(11, columns.length*2-1);
+    }
 }
