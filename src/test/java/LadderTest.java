bedd392d5fa0e6b9d08356317284bd69f28daa9d
he2joo <hottestej6@gamil.com>
2023-03-16 00:33:24 +0900

    feat : Ladder를 테스트하는 LadderTest 작성
    Ladder클래스를 테스트 하기 위해 LadderTest에 사다리의 행과 열이 맞게 출력 되는지, 가로줄이 연속으로 나오는지 테스트하는 메서드를 작성했다.

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,2 +1,45 @@
-public class LadderTest {
+import kr.codesquad.domain.Ladder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.assertj.core.api.SoftAssertions;
+
+class LadderTest {
+
+    @Test
+    @DisplayName("사다리의 행이 맞게 출력되는지 확인")
+    void testRow() {
+        int row = 5;
+        int column = 3;
+        Ladder ladder = new Ladder(row, column);
+        String result = ladder.make();
+
+        String[] lines = result.split("\n");
+        Assertions.assertEquals(row, lines.length);
+    }
+    @Test
+    @DisplayName("사다리의 열이 맞게 출력되는지 확인")
+    void testColumn() {
+        int row = 5;
+        int headCount = 3;
+        Ladder ladder = new Ladder(row, headCount);
+        String result = ladder.make();
+
+        String[] lines = result.split("\n");
+        for (String line : lines) {
+            String[] columns = line.split("\\|");
+            Assertions.assertEquals(headCount, columns.length);
+        }
+    }
+    @Test
+    @DisplayName("사다리 가로줄이 연속으로 나오는지 확인")
+    void testMakeLadder() {
+        Ladder ladder = new Ladder(5,4);
+        String result = ladder.make();
+
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(result).doesNotContain("-----|-----");
+        softly.assertAll();
+    }
 }
+
