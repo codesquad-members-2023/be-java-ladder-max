c7d3ab8b0a61b33495e6a9f5755a929cb8529ad7
crtEvent <crtevn@gmail.com>
2023-03-16 00:58:14 +0900

    test: PlayerTest 예외 테스트시 더 정확한 Exception 지정
    - Exception -> IllegalArgumentException 사용
    - layer.getName() 메서드 이름이 변경되어 player.getCenterAlignedName()으로 수정

--- src/test/java/kr/codesquad/domain/PlayerTest.java
+++ src/test/java/kr/codesquad/domain/PlayerTest.java
@@ -3,6 +3,7 @@ package kr.codesquad.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import kr.codesquad.domain.player.Player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,7 +17,7 @@ public class PlayerTest {
     @DisplayName("플레이어에 입력된 이름이 5글자보다 크면 예외 발생.")
     void testGetNameThrowException() {
         assertThatThrownBy(() -> new Player(SYLLABLE.repeat(MAX_NAME_LENGTH + 1)))
-                .isInstanceOf(Exception.class);
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -24,7 +25,7 @@ public class PlayerTest {
     void testGetNameLength() {
         for(int i = 1; i <= MAX_NAME_LENGTH; i++) {
             player = new Player(SYLLABLE.repeat(i));
-            assertThat(player.getName().length()).isEqualTo(MAX_NAME_LENGTH);
+            assertThat(player.getCenterAlignedName().length()).isEqualTo(MAX_NAME_LENGTH);
         }
     }
 
