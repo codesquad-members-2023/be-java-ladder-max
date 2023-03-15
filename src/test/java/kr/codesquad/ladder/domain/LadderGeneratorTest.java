8aa980aa4a8a282fc5aa23dcf5cb4dd57914a886
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-15 15:04:16 +0900

    test: LadderGenerator 객체 생성시 예외가 발생하는지 테스트 추가

--- src/test/java/kr/codesquad/ladder/domain/LadderGeneratorTest.java
+++ src/test/java/kr/codesquad/ladder/domain/LadderGeneratorTest.java
@@ -1,27 +1,17 @@
-package kr.codesquad.ladder;
-
-import java.util.ArrayList;
-import java.util.List;
-import kr.codesquad.ladder.domain.Ladder;
-import kr.codesquad.ladder.domain.LadderGenerator;
-import kr.codesquad.ladder.domain.Line;
-import kr.codesquad.ladder.domain.Names;
-import kr.codesquad.ladder.domain.Point;
-import kr.codesquad.ladder.domain.Points;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.Random;
+package kr.codesquad.ladder.domain;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class LadderGeneratorTest {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import kr.codesquad.ladder.exception.InvalidNumberOfMinimumLadderHeightException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-    private static final String BRIDGE = "-----";
-    private static final String EMPTY = "     ";
-    private static final String BAR = "|";
+class LadderGeneratorTest {
 
     private int m;
     private LadderGenerator ladderGenerator;
@@ -30,13 +20,13 @@ class LadderGeneratorTest {
     @DisplayName("사람 2명과 사다리 높이2가 주어질 때 랜덤한 사다리 생성하는지 테스트")
     public void generateLadderPart_testcase1() {
         //given
-        String input = "pobi,hounx";
-        Names names = new Names(input, 2);
+        String[] nameArray = {"pobi", "honux"};
+        Names names = new Names(createNameList(nameArray));
         m = 2;
         //mocking
         Random mockRandom = mock(Random.class);
         when(mockRandom.nextBoolean()).thenReturn(true, false);
-        ladderGenerator = new LadderGenerator(m, 1);
+        ladderGenerator = new LadderGenerator(m);
         //when
         Ladder actual = ladderGenerator.createLines(names.size(), mockRandom);
         //then
@@ -49,27 +39,47 @@ class LadderGeneratorTest {
         Assertions.assertThat(actual).isEqualTo(expected);
     }
 
+    private List<Name> createNameList(String[] nameArray) {
+        List<Name> nameList = new ArrayList<>();
+        for (String name : nameArray) {
+            nameList.add(new Name(name));
+        }
+        return nameList;
+    }
+
     @Test
     @DisplayName("사다리의 한 라인에 브리지가 2개 이상 연속적으로 나오지 않는지 테스트")
     public void generateLadderPart_testcase3() {
         //given
-        String input = "pobi,hounx,crong";
-        Names names = new Names(input, 2);
+        String[] nameArray = {"pobi", "honux", "crong"};
+        Names names = new Names(createNameList(nameArray));
         m = 1;
         //mocking
         Random mockRandom = mock(Random.class);
         when(mockRandom.nextBoolean()).thenReturn(true, true);
-        ladderGenerator = new LadderGenerator(m, 1);
+        ladderGenerator = new LadderGenerator(m);
         //when
         Ladder actual = ladderGenerator.createLines(names.size(), mockRandom);
         //then
-        Line line1 =
-            new Line(new Points(new ArrayList<>(
-                List.of(Point.ofBar(), Point.ofBridge(), Point.ofBar(), Point.ofEmpty(),
-                    Point.ofBar()))));
+        Line line1 = new Line(new Points(new ArrayList<>(
+            List.of(Point.ofBar(), Point.ofBridge(), Point.ofBar(), Point.ofEmpty(),
+                Point.ofBar()))));
         Ladder expected = new Ladder(new ArrayList<>(List.of(line1)));
 
         Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("객체 생성시 최소 사다리 높이보다 적은 사다리 높이 입력시 예외가 발생하는지 테스트")
+    public void newLadderGenerator() {
+        //given
+        int maximumLadderHeight = -1;
+        //when
+        Throwable actual =
+            Assertions.catchThrowable(() -> new LadderGenerator(maximumLadderHeight));
+        //then
+        Assertions.assertThat(actual)
+            .isInstanceOf(InvalidNumberOfMinimumLadderHeightException.class);
     }
 
 }
