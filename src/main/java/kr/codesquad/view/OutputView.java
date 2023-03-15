c66035291c19a7a45be5ca4824c017794a438368
pinetree <eodnjs568@gmail.com>
2023-03-15 16:09:42 +0900

    feat "Outputview 내 centeAligned 메서드 추가"
    참여자의 이름이 가운데 정렬하게끔 메서드 추가.
    해당 메서드는 printpeopleList 메서드에서 호출해서 사용.

--- src/main/java/kr/codesquad/view/OutputView.java
+++ src/main/java/kr/codesquad/view/OutputView.java
@@ -12,19 +12,26 @@ public class OutputView {
         }
 
 
-    /*
-    정렬하기 위한 메서드 추ㅣㅁ
-     */
-
-//    public static String centerString(int width, String s) {
-//        return String.format("%-" + width + "s", String.format("%" + (s.length() + (width - s.length()) / 2) + "s", s));
-//    }
-
     public void printPeopleList(List<String> nameList) {
-        for (int i = 0; i < nameList.size(); i++) {
-            System.out.print(nameList.get(i) + " ");
+        StringBuilder sb = new StringBuilder();
+        nameList.forEach(name -> sb.append(centerAligned(name,5)));
+        System.out.println(sb);
+    }
+
+
+
+    private String centerAligned(final String name, final int length) {
+        if (name.length() > length) {
+            return String.format("%s", name);
         }
-        System.out.println();
+        if (name.length() == 4) {
+            return String.format(" %s ", name);
+        }
+        if (name.length() == 5) {
+            return String.format("%s ", name);
+        }
+        final int padding = (length - name.length()) / 2;
+        return String.format("%" + padding + "s%s%" + padding + "s ", "", name, "");
     }
 
 
