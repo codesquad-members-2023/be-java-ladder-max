b246c0cfa157203e460fcc422a0468e143f11fe0
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-15 14:58:18 +0900

    modify: LadderResult 타입 리스트를 관리하는 일급 컬렉션으로 변경

--- src/main/java/kr/codesquad/ladder/domain/LadderResults.java
+++ src/main/java/kr/codesquad/ladder/domain/LadderResults.java
@@ -2,53 +2,21 @@ package kr.codesquad.ladder.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class LadderResults {
 
     private final List<LadderResult> ladderResults;
 
-    public LadderResults(String[] ladderResults, int countOfPeople) {
-        validateCountOfLadderResult(ladderResults, countOfPeople);
-        this.ladderResults = toList(ladderResults);
-    }
-
-    private void validateCountOfLadderResult(String[] ladderResults, int countOfPeople) {
-        if (ladderResults.length != countOfPeople) {
-            throw new InvalidCountOfLadderResultException(countOfPeople);
-        }
-    }
-
-    private List<LadderResult> toList(String[] ladderResults) {
-        List<LadderResult> ladderResultList = new ArrayList<>();
-        for (String ladderResult : ladderResults) {
-            ladderResultList.add(new LadderResult(ladderResult));
-        }
-        return ladderResultList;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof LadderResults)) {
-            return false;
-        }
-        LadderResults that = (LadderResults) o;
-        return Objects.equals(ladderResults, that.ladderResults);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(ladderResults);
+    public LadderResults(List<LadderResult> ladderResults) {
+        this.ladderResults = new ArrayList<>(ladderResults);
     }
 
     @Override
     public String toString() {
-        return ladderResults.stream()
-            .map(ladderResult -> String.format("%-5s", ladderResult.toString()))
-            .collect(Collectors.joining());
+        StringBuilder sb = new StringBuilder();
+        for (LadderResult ladderResult : ladderResults) {
+            sb.append(ladderResult).append("\n");
+        }
+        return sb.toString();
     }
 }
