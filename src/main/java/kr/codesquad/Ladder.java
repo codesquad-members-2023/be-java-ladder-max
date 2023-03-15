f4e49c6cbc3a8fc441d941eeba79a6e04858baf3
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-15 13:50:31 +0900

    Refactor($Ladder): lader 클래스 리팩토링
    본래 ladder 클래스에 랜덤한 클래스를 생성하는 기능이 있었는데, 많은
    기능이 한 클래스에 있는 것 같아 무작위의 사다리를 만드는 기능을
    분리했습니다.
    또한 infromladder()에서 boolean의 리스트를 넘겨 줬었는데, drawLadder()로
    메서드를 바꿔 ladder의 모양을 그려서 String으로 반환하게 했습니다.

--- src/main/java/kr/codesquad/Ladder.java
+++ src/main/java/kr/codesquad/Ladder.java
@@ -1,44 +1,21 @@
 package kr.codesquad;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Ladder {
-    private final static int PLAYER_MAX_LENGTH = 5;
-    private final List<String> players;
-    private final int height;
-    private final List<Line> ladder = new ArrayList<>();
+    private final List<Line> ladder;
 
-    Ladder(List<String> players, int height) {
-        this.players = players;
-        this.height = height;
-        buildLadder();
-    }
-    
-    Ladder(List<Line> ladder) {
-        this.players = null;
-        this.height = ladder.size();
-        this.ladder.addAll(ladder);
+    public Ladder(List<Line> ladder) {
+        this.ladder = ladder;
     }
 
-    private void buildLadder() {
-        for (int cnt = 0; cnt < height; cnt++) {
-            Line l = new Line(players.size() - 1);
-            ladder.add(l);
+    public String drawLadder() {
+        StringBuilder sb = new StringBuilder();
+
+        for (Line row : ladder) {
+            sb.append(row.drawLine());
+            sb.append("\n");
         }
-    }
-
-    private List<String> informPlayers() {
-        return this.players;
-    }
-
-    public List<List<Boolean>> informLadder() {
-        List<List<Boolean>> ladder = new ArrayList<>();
-
-        for (Line l : this.ladder) {
-            ladder.add(l.getLine());
-        }
-        return ladder;
+        return sb.toString();
     }
 }
