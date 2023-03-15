d8e935136e231565eae435c0d0dbbf4f0f2a943c
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-15 14:48:36 +0900

    style: 코드 정리

--- src/main/java/kr/codesquad/ladder/domain/LadderGenerator.java
+++ src/main/java/kr/codesquad/ladder/domain/LadderGenerator.java
@@ -3,6 +3,7 @@ package kr.codesquad.ladder.domain;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import kr.codesquad.ladder.exception.InvalidNumberOfMinimumLadderHeightException;
 
 public class LadderGenerator {
 
