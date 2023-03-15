353e6a7a33c004def1148ba096dee713d2a8a67a
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-15 16:29:04 +0900

    Refactor($LadderMaker): LadderMaker클래스 리팩토링
    먼저 사다리의 다리가 무작위로 만들어지므로 메서드의 이름을
    makeLadder에서 makeRandomLadder로 변경했습니다.
    또한 makeRandomLadder안에서 Line의 생성자를 호출하는 경우가 있습니다.
    이를 Ladder의 메서드의 호출로 변경하여 LadderMaker와 Line 사이의
    의존성을 없앴습니다.

--- src/main/java/kr/codesquad/LadderMaker.java
+++ src/main/java/kr/codesquad/LadderMaker.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class LadderMaker {
     RandomBooleanGenerator rbg = new RandomBooleanGenerator();
 
-    public Ladder make(int width, int height) {
-        List<Line> rows = new ArrayList<>();
+    public Ladder makeRandomLadder(int width, int height) {
+        Ladder ladder = new Ladder();
 
         for (int cnt = 0; cnt < height; cnt++) {
             List<Boolean> randomBoolean = rbg.generate(width);
-            rows.add(new Line(randomBoolean));
+            ladder.addLine(randomBoolean);
         }
-        return new Ladder(rows);
+        return ladder;
     }
 }
