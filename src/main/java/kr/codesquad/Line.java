0ef8e4ccc93eaf71b247a778ddd8bf3ef1dd8872
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-15 13:47:56 +0900

    Refactor($Line): 인스턴스 필드와 import문 리팩토링
    쓰지않는 import문과 인스턴스 변수를 제거했습니다.

--- src/main/java/kr/codesquad/Line.java
+++ src/main/java/kr/codesquad/Line.java
@@ -1,19 +1,15 @@
 package kr.codesquad;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Line {
     private static final String RUNG = "=".repeat(5);
     private static final String BLANK = " ".repeat(5);
     private static final String POLE = "|";
     private final List<Boolean> line;
-    private final int width;
 
     public Line (List<Boolean> line) {
         this.line = line;
-        this.width = line.size();
     }
 
     public String drawLine() {
