a625c71ee9c1b4c61511fb2ba80f9414be4e7195
jwp345 <jwp135@naver.com>
2023-03-15 21:21:11 +0900

    OutputView가 필요 없다고 생각 되었기 때문에 삭제 및 메소드 이름들 변경 및 Optional을 사용해 예외처리 리팩토링

--- src/main/java/kr/codesquad/controller/GameController.java
+++ src/main/java/kr/codesquad/controller/GameController.java
@@ -1,8 +1,8 @@
 package kr.codesquad.controller;
 
 import kr.codesquad.domain.Ladder;
+import kr.codesquad.domain.Line;
 import kr.codesquad.view.InputView;
-import kr.codesquad.view.OutputView;
 
 import java.util.List;
 
@@ -10,38 +10,41 @@ public class GameController {
 
     public void run() {
         InputView inputView = new InputView();
-        OutputView outputView = new OutputView(); // 멤버변수로 빼는 게 좋나..?
-        try {
-            List<String> people = inputView.insertNames();
-            int peopleLen = people.size();
-            Ladder ladder = new Ladder(people, inputView.insertResults(peopleLen), inputView.insertHeight());
-            printLadderResult(inputView, outputView, ladder);
-        } catch (NumberFormatException e) {
-            System.out.println("숫자 형식은 int 형식만 지원합니다.");
-        } catch (Exception e) {}
+        List<String> people = inputView.insertNames();
+        int peopleLen = people.size();
+        Ladder ladder = new Ladder(people, inputView.insertResults(peopleLen), inputView.insertHeight());
+        printLadderResult(inputView, ladder);
     }
 
-    private void printLadderResult(InputView inputView, OutputView outputView, Ladder ladder) {
-        outputView.printLadderStatus(ladder);
+    private void printLadderResult(InputView inputView, Ladder ladder) {
+        printLadderStatus(ladder);
         String input = inputView.insertResultName();
         while (!input.equals("춘식이")) {
-            printAllOrPersonResult(input, outputView, ladder);
+            printAllOrPersonResult(input, ladder);
             input = inputView.insertResultName();
         }
-        outputView.endGame();
+        System.out.print("\r\n게임을 종료합니다.");
     }
 
-    private void printAllOrPersonResult(String input, OutputView outputView, Ladder ladder) {
+    private void printAllOrPersonResult(String input, Ladder ladder) {
         System.out.println("\r\n실행 결과");
         if(input.equals("all")) {
-            outputView.printAllResults(ladder.getFinalPositions());
+            ladder.getFinalPositions().stream().forEach(System.out::println);
             return;
         }
         if(!containsPerson(input, ladder)) {
-            outputView.printNoNameError();
+            System.out.println("\r\n잘못된 사람을 입력하셨습니다. 다시 입력해주세요.");
             return;
         }
-        outputView.printPersonResult(ladder.getFinalPositions(), input);
+        printPersonResult(ladder.getFinalPositions(), input);
+    }
+
+    private void printPersonResult(List<String> finalPositions, String input) {
+        System.out.println(finalPositions
+                .stream().map(o -> o.split( " : "))
+                .filter(o -> o[0].equals(input))
+                .findFirst()
+                .orElse(new String[] {"", "없는 이름이 로직을 탐."})[1]);
     }
 
     private boolean containsPerson(String input, Ladder ladder) {
@@ -49,5 +52,33 @@ public class GameController {
             return true;
         }
         return false;
+    }
+
+    private void printLadderStatus(Ladder ladder) {
+        List<String> people = ladder.getPeople();
+        List<Line> lines = ladder.getLines();
+        List<String> results = ladder.getResults();
+        printPeople(people);
+        for(Line line : lines) {
+            System.out.print("    |");
+            System.out.println(line);
+        }
+        printList(results);
+    }
+
+    private void printPeople(List<String> people) {
+        System.out.println();
+        System.out.println("사다리 결과");
+        System.out.println();
+        printList(people);
+    }
+
+    private void printList(List<String> list) {
+        System.out.println(" " + list.stream()
+                .map(o -> " ".repeat((6 - o.length()) / 2) + o
+                        + " ".repeat((8 - o.length()) / 2))
+                .reduce(new StringBuilder(), (sb, o) ->
+                                sb.append(o)
+                        , StringBuilder::append));
     }
 }
--- src/main/java/kr/codesquad/view/InputView.java
+++ src/main/java/kr/codesquad/view/InputView.java
@@ -8,62 +8,91 @@ import java.util.stream.Collectors;
 public class InputView {
 
     private final Scanner sc;
-    private final int maxLength = 5;
-    private final int minNum = 0;
+    private static final int maxLength = 5;
+    private static final int minLimit = 0;
 
     public InputView() {
         this.sc = new Scanner(System.in);
     }
 
     public List<String> insertResults(int len) {
-        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
-        Optional<List<String>> results = parseInput();
-        if(!results.isPresent() || !isValidResults(results.get(), len)) {
-            System.out.println("각 결과의 길이는 5 이하여야 하거나 결과의 길이가 사람들의 길이가 다릅니다.");
-            throw new IllegalArgumentException();
+        Optional<List<String>> results = Optional.empty();
+        while(!checkValidResults(results, len)) {
+            System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+            results = parseInput();
         }
         return results.get();
     }
 
-    private boolean isValidResults(List<String> results, int len) {
+    private boolean checkValidResults(Optional<List<String>> opResults, int len) {
+        if(opResults.isEmpty()) {
+            return false;
+        }
+        List<String> results = opResults.get();
         if(results.size() != len) {
+            System.out.println("결과의 길이가 참여한 사람들의 길이와 다릅니다.");
             return false;
         }
         return true;
     }
 
     public int insertHeight() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int height = Integer.parseInt(this.sc.nextLine());
-        if(height < minNum) {
-            System.out.println("음수를 입력하셨습니다.");
-            throw new IllegalArgumentException();
+        Optional<String> input = Optional.empty();
+        while(!checkValidNumber(input)) {
+            System.out.println("최대 사다리 높이는 몇 개인가요?");
+            input = Optional.of(this.sc.nextLine());
         }
-        return height;
+        return Integer.parseInt(input.get());
+    }
+
+    private boolean checkValidNumber(Optional<String> opInput) {
+        if(opInput.isEmpty()) {
+            return false;
+        }
+        try {
+            int num = Integer.parseInt(opInput.get());
+            return checkLimit(num);
+        } catch (NumberFormatException e) {
+            System.out.println("올바른 int 숫자 형식이 아닙니다.");
+            return false;
+        }
+    }
+
+    private boolean checkLimit(int num) {
+        if(num < minLimit) {
+            System.out.println("입력되는 숫자는 0보다 작을 수 없습니다.");
+            return false;
+        }
+        return true;
     }
 
     public List<String> insertNames() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        Optional<List<String>> people = parseInput();
-        if(!people.isPresent() || containsDuplicateOrEnd(people.get())) {
-            System.out.println("각 이름의 길이는 5 이하여야 합니다. 중복된 이름이 들어가거나 \"춘식이\"란 이름은 사용하실 수 없습니다.");
-            throw new IllegalArgumentException();
+        Optional<List<String>> people = Optional.empty();
+        while(!checkValidName(people)) {
+            System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+            people = parseInput();
         }
         return people.get();
     }
 
-    private boolean containsDuplicateOrEnd(List<String> people) {// 중복된 이름 있거나 춘식이란 이름 있는지 검증
+    private boolean checkValidName(Optional<List<String>> opPeople) {// 중복된 이름 있거나 춘식이란 이름 있는지 검증
+        if(opPeople.isEmpty()) {
+            return false;
+        }
+        List<String> people = opPeople.get();
         Set<String> peopleSet = new HashSet<>(people);
         if(peopleSet.size() != people.size() || peopleSet.contains("춘식이")) {
-            return true;
+            System.out.println("중복된 이름이 들어가거나 \"춘식이\"란 이름은 사용하실 수 없습니다.");
+            return false;
         }
-        return false;
+        return true;
     }
 
     private Optional<List<String>> parseInput() {
         List<String> list = Arrays.stream(this.sc.nextLine()
                 .split(",")).collect(Collectors.toList());
         if(isAnyOverFive(list)) {
+            System.out.println("각 입력의 길이는 5 이하여야 합니다.");
             return Optional.empty();
         }
         return Optional.of(list);
@@ -71,8 +100,7 @@ public class InputView {
 
     public boolean isAnyOverFive(List<String> people) {
         return people.stream()
-                .filter(o -> o.length() > maxLength)
-                .findFirst().isPresent();
+                .anyMatch(o -> o.length() > maxLength);
     }
 
     public String insertResultName() {
