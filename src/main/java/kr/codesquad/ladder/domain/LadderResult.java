de972254a56d62826ce4270db8ab6d1d59908716
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-15 14:55:54 +0900

    modify: 기존 LadderResult 객체의 구조를 Destination 객체로 변경하고 개념 설계를 변경
    * 기존 LadderResult 객체의 의미는 사다리 게임의 도착지 값을 저장하는 래핑 클래스의 역할이었다면 변경된 개념은 사다리 게임의 이름과 도착지 정보를 저장하는 역할로 변경되었습니다.

--- src/main/java/kr/codesquad/ladder/domain/LadderResult.java
+++ src/main/java/kr/codesquad/ladder/domain/LadderResult.java
@@ -1,43 +1,17 @@
 package kr.codesquad.ladder.domain;
 
-import java.util.Objects;
-
 public class LadderResult {
 
-    private static final int LENGTH_LIMIT = 5;
+    private final Name name;
+    private final Destination destination;
 
-    private final String value;
-
-    public LadderResult(String value) {
-        validateLengthOfLadderResult(value);
-        this.value = value;
-    }
-
-    public void validateLengthOfLadderResult(String value) {
-        if (value.length() > LENGTH_LIMIT) {
-            throw new InvalidLengthOfLadderResultException();
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof LadderResult)) {
-            return false;
-        }
-        LadderResult that = (LadderResult) o;
-        return Objects.equals(value, that.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
+    public LadderResult(Name name, Destination destination) {
+        this.name = name;
+        this.destination = destination;
     }
 
     @Override
     public String toString() {
-        return value;
+        return String.format("%s : %s", name, destination);
     }
 }
