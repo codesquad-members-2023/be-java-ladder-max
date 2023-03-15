41bcf6f71b988f7a91e994d8aa7b6fa703ed0f15
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-15 16:20:35 +0900

    Refactor: Output 리팩토링
    사다리의 모양정보를 Line 클래스에 넣어줬기 때문에 output 클래스에 있는
    모양정보는 삭제했습니다.
    또한 player 리스트가 들어왔을 때 이를 출력할 모양으로 변경 하는 메서드를
    생성했습니다.
    사다리 모양과 player 리스트가 들어왔을 때 이를 출력하게 했습니다.

--- src/main/java/kr/codesquad/Output.java
+++ src/main/java/kr/codesquad/Output.java
@@ -4,40 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Output {
-    private static final int PLAYER_MAX_LENGTH = 5;
-    private static final String POLE = "|";
-    private static final String RUNG = "-".repeat(5);
-    private static final String BLANK = " ".repeat(5);
+    public void printWholeFigure(List<String> players, String ladderFigure) {
+        String playersFigure = drawPlayers(players);
+        System.out.println(playersFigure);
+        System.out.println(ladderFigure);
+    }
 
-    public void printPlayers(List<String> players) {
+    private String drawPlayers(List<String> players) {
         StringBuilder sb = new StringBuilder();
 
         for (String player : players) {
             sb.append(String.format("%-6s", player));
-        }
-        System.out.println(sb);
-    }
-
-    public List<String> lineToString(List<List<Boolean>> lineInformation) {
-        List<String> shape = new ArrayList<>();
-
-        for (List l : lineInformation) {
-            shape.add(encapsulateLine(l));
-        }
-        return shape;
-    }
-    private String encapsulateLine(List<Boolean> line) {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(POLE);
-        for (boolean bool : line) {
-            if (bool) {
-                sb.append(RUNG);
-                sb.append(POLE);
-                continue;
-            }
-            sb.append(BLANK);
-            sb.append(POLE);
         }
         return sb.toString();
     }
