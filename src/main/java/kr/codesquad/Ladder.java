221340f263f9e64c57b19e95368ab3b7a09133eb
LaStella <jingyu.ham94@gmail.com>
2023-03-16 14:15:29 +0900

    refactor: Ladder 클래스 리펙토링
    사다리의 행을 나타내는 객체 Line을 만들었습니다.
    - Line은 다리 유무를 나타내는 점들의 리스트 points를 가집니다.
    - 한 점(i)에서 다리 유무를 반환하는 메소드 hasBridge(int i)와 점들의 개수를 반환하는 size()메소드를 가집니다.

--- src/main/java/kr/codesquad/Ladder.java
+++ src/main/java/kr/codesquad/Ladder.java
@@ -3,28 +3,27 @@ package kr.codesquad;
 import java.util.ArrayList;
 
 public class Ladder {
-  private final ArrayList<ArrayList<Boolean>> ladder;
+    private ArrayList<Line> ladder;
 
-  public Ladder(ArrayList<ArrayList<Boolean>> ladder) {
-    this.ladder = ladder;
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    for (ArrayList<Boolean> row : ladder) {
-      sb.append("  "); // 사람 이름 5글자가 기준이므로 사다리 앞에 2칸의 간격 추가
-      for (boolean hasLine : row) {
-        String line = hasLine ? "|-----" : "|     ";
-        sb.append(line);
-      }
-      sb.append("\n");
+    public Ladder(ArrayList<Line> ladder) {
+        this.ladder = ladder;
     }
 
-    return String.valueOf(sb);
-  }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (var line : ladder) {
+            sb.append("  "); // 사람 이름 5글자가 기준이므로 사다리 앞에 2칸의 간격 추가
+            for (int i = 0; i < line.size(); i++) {
+                sb.append(line.hasBridge(i) ? "|-----" : "|     ");
+            }
+            sb.append("\n");
+        }
 
-  public ArrayList<ArrayList<Boolean>> getLadder() {
-    return ladder;
-  }
+        return String.valueOf(sb);
+    }
+
+    public ArrayList<Line> getLadder() {
+        return ladder;
+    }
 }
