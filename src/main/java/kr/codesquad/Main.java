f2357a20871bfc33668d2065053b4ce98a736e7c
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-16 15:28:17 +0900

    Refactor($Main): 메인 메서드 리팩토링
    패키지가 나뉘었고 클래스의 이름이 변경되어, 이에 맞게 메인 메서드를
    수정했습니다.

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,16 +1,19 @@
 package kr.codesquad;
 
+import kr.codesquad.domain.Ladder;
+import kr.codesquad.domain.LadderMaker;
+import kr.codesquad.view.View;
+
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Input input = new Input();
-        List<String> players = input.inputValidatedPlayers();
-        int height = input.inputHeight();
+        View view = new View();
+        List<String> players = view.inputValidatedPlayers();
+        int height = view.inputHeight();
         LadderMaker maker = new LadderMaker();
         Ladder ladder = maker.makeRandomLadder(players.size() - 1, height);
         String ladderFigure = ladder.drawLadder();
-        Output output = new Output();
-        output.printWholeFigure(players, ladderFigure);
+        view.printWholeFigure(players, ladderFigure);
     }
 }
