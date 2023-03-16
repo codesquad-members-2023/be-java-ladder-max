bd37f13a0508fdd34631160da4786dc3e51e2742
LaStella <jingyu.ham94@gmail.com>
2023-03-16 10:14:38 +0900

    chore: 테스트클래스 주석 추가

--- src/test/java/kr/codesquad/RandomLadderGeneratorTest.java
+++ src/test/java/kr/codesquad/RandomLadderGeneratorTest.java
@@ -7,12 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RandomLadderGeneratorTest {
   @Test
-  @DisplayName("사다리의 라인은 겹치지 않습니다.")
-  public void testGenerate() {
+  @DisplayName("랜덤하게 생성시 사다리의 라인은 겹치지 않습니다.")
+  public void testRandomGenerate() {
     RandomLadderGenerator generator = new RandomLadderGenerator();
     var ladder = generator.generate(5, 5);
     for (var row : ladder.getLadder()) {
       for (int i = 1 ; i < row.size()-1 ; i++) {
+        // 현재위치와 앞 또는 뒤 위치에 라인이 모두 있는 경우는 없습니다.
         assertFalse(row.get(i) && row.get(i-1));
         assertFalse(row.get(i) && row.get(i+1));
       }
