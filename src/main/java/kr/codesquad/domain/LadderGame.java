30c05e4ecad2a4ca5e55a6a4418258d7070a2d70
he2joo <hottestej6@gamil.com>
2023-03-16 16:23:12 +0900

    feat : Players클래스에 결과 저장하는 메서듣 추가
    원래 Players에 있던 사다리 결과를 계산하는 메서드를 삭제하고 결과를 받아 Map에 저장하는 sortResult()를 추가했다. 이 메서드는 생성자로 int[]를 받아 Map에 플레이어의 이름과 결과를 저장한다. Players의 생성자 매개변수가 변경되어 LadderGame도 변경하였다.

--- src/main/java/kr/codesquad/domain/LadderGame.java
+++ src/main/java/kr/codesquad/domain/LadderGame.java
@@ -5,20 +5,22 @@ import java.util.List;
 
 public class LadderGame {
     private Players players;
-    public void start(String list,String result, int row){
+    public void start(String list,String resultList, int row){
         List<String> playersList = Arrays.asList(list.split(","));
 
         Ladder ladder = new Ladder(row,playersList.size());
-        this.players = new Players(playersList,result,ladder.make());
-
-        System.out.println(players.getNames()); // 플레이어 이름
-        System.out.println(ladder.make()); // 사다리 모양
-        System.out.println(players.getResult()); // 결과
+        this.players = new Players(playersList,resultList,ladder.calculateResult());
+        printAll(players,ladder);
     }
     public void searchResult(String id) {
         if ("춘식이".equals(id)){
             throw new IllegalArgumentException();
         }
         System.out.println(players.searchResult(id));
+    }
+    private void printAll(Players players, Ladder ladder){ // 플레이어 이름, 사다리 모양, 결과
+        System.out.println(players.getNames());
+        System.out.println(ladder.make());
+        System.out.println(players.getResultList());
     }
 }
