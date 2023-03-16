5935fad82f8ee95316e72c132d4fd4e1c309b5bb
DOEKYONG <apochmn@naver.com>
2023-03-16 20:28:21 +0900

    refactor : controller에 있던 예외처리 Input으로 이동 입력 받을 때 검증 할 수 있도록 함

--- src/main/java/kr/codesquad/ladder/view/Input.java
+++ src/main/java/kr/codesquad/ladder/view/Input.java
@@ -1,25 +1,55 @@
 package kr.codesquad.ladder.view;
 
+import kr.codesquad.ladder.domain.User;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Input {
 
-    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private  BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static int ladderLength() throws IOException {
+    public  int ladderLength() throws IOException {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         return  Integer.parseInt(bufferedReader.readLine());
     }
 
-    public static   String insertUser() throws IOException {
+    public String insertUser() throws IOException {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         return bufferedReader.readLine();
     }
 
-    public static   String insertResult() throws IOException {
+    public    String insertResult() throws IOException {
         System.out.println("결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
         return bufferedReader.readLine();
+    }
+    public String insertFindUser() throws IOException {
+        System.out.println("결과를 보고 싶은 사람은?");
+        return bufferedReader.readLine();
+    }
+
+
+    // 유효성 검사 통과한 참가자 입력 값
+    public  String vaildName() throws IOException {
+        String name = insertUser();
+        Validator validator = new Validator();
+        while (!validator.nameValidate(name.split(","))){
+            name = insertUser();
+        }
+        return name;
+    }
+
+
+    // 유효성 검사 통과한 결과값 입력 값
+    public String vaildResult(int userCount) throws IOException {
+        String name = insertResult();
+        Validator validator = new Validator();
+        int inputLength = name.split(",").length;
+        while (!validator.resultValidate(userCount,inputLength)||!validator.nameValidate(name.split(","))){
+            name = insertResult();
+            inputLength = name.split(",").length;
+        }
+        return name;
     }
 }
