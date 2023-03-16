98ab8454734baee8435c0119ddad09295b5a83d9
LaStella <jingyu.ham94@gmail.com>
2023-03-16 14:30:23 +0900

    refactor: RandomLadderGenerator 클래스와 테스트클래스 리펙토링
    사다리의 행을 나타내는 객체 Line을 생성하여 사다리를 생성하도록 수정하였습니다.
    테스트 클래스에서 SoftAssertion을 사용하여 테스트를 진행합니다.
    - 테스트에서 실패하는 케이스가 나와도 이후의 테스트를 모두 진행한 후 결과를 출력합니다.

--- src/main/java/kr/codesquad/RandomLadderGenerator.java
+++ src/main/java/kr/codesquad/RandomLadderGenerator.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomLadderGenerator {
-  private final Random random = new Random();
+    private final Random random = new Random();
 
-  public Ladder generate(int sizeOfPeople, int sizeOfLadder) {
-    ArrayList<ArrayList<Boolean>> ladder = new ArrayList<>(sizeOfLadder);
-    for (int row = 0; row < sizeOfLadder; row++) {
-      ladder.add(new ArrayList<>());
-      ladder.get(row).add(random.nextBoolean());
-      for (int col = 1; col < sizeOfPeople-1; col++) {
-        boolean hasLine = ladder.get(row).get(col-1) ? false : random.nextBoolean(); // 이전 열의 라인 유무확인
-        ladder.get(row).add(hasLine);
-      }
-      ladder.get(row).add(false); // 마지막 열은 라인이 없으므로 false를 저장한다.
+    public Ladder generate(int sizeOfPeople, int sizeOfLadder) {
+        ArrayList<Line> ladder = new ArrayList<>();
+        while (sizeOfLadder-- > 0) {
+            Line line = new Line();
+            line.add(random.nextBoolean());
+            for (int col = 1; col < sizeOfPeople - 1; col++) {
+                // 이전 열의 라인 유무확인
+                line.add(line.hasBridge(col - 1) ? false : random.nextBoolean());
+            }
+            line.add(false); // 마지막 열은 라인이 없으므로 false를 저장한다.
+            ladder.add(line);
+        }
+        return new Ladder(ladder);
     }
-
-    return new Ladder(ladder);
-  }
 }
