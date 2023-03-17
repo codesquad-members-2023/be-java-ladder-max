c2cd0d4fbb64acc1a0317581cdd1575d2896b8e1
LaStella <jingyu.ham94@gmail.com>
2023-03-17 10:17:33 +0900

    refacotr: LadderGame 클래스 리펙토링
    사람 이름 입력과 사다리 높이 입력에서 잘못된 입력시 재귀호출로 재입력 받지 않도록 수정하였습니다.
    사람 이름 입력과 사다리 높이 입력을 while문을 사용하여 올바른 입력을 받을 때까지 반복합니다.
    잘못된 입력은 Optional을 사용하여 비어있는 객체를 반환합니다.

--- src/main/java/kr/codesquad/domain/LadderGame.java
+++ src/main/java/kr/codesquad/domain/LadderGame.java
@@ -7,87 +7,95 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public class LadderGame implements Runnable {
-  private final int MIN_SIZE_OF_PEOPLE = 2;
-  private final int MIN_SIZE_OF_LADDER = 1;
-  private final int MAX_NAME_SIZE = 5;
-  private final Console console;
-  private final RandomLadderGenerator generator;
+    private final int MIN_SIZE_OF_PEOPLE = 2;
+    private final int MIN_SIZE_OF_LADDER = 1;
+    private final int MAX_NAME_SIZE = 5;
+    private final Console console;
+    private final RandomLadderGenerator generator;
 
-  public LadderGame(Console console, RandomLadderGenerator generator) {
-    this.console = console;
-    this.generator = generator;
-  }
-
-  @Override
-  public void run() {
-    ArrayList<Player> playerArrayList = inputPlayersToPlayerList("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-    int sizeOfLadder = inputToLadderSize("최대 사다리 높이는 몇 개인가요?");
-
-    Ladder ladder = generator.generate(playerArrayList.size(),sizeOfLadder);
-    printPlayerList(playerArrayList); // 사람 이름 출력
-    System.out.println(ladder); // 사다리 출력
-  }
-
-  private void printPlayerList(ArrayList<Player> playerArrayList) {
-    StringBuilder sb = new StringBuilder();
-    for (var player : playerArrayList) {
-      sb.append(alignCenter(player.getName())).append(" ");
+    public LadderGame(Console console, RandomLadderGenerator generator) {
+        this.console = console;
+        this.generator = generator;
     }
-    System.out.println(sb);
-  }
 
-  // 5글자를 기준으로 이름을 가운데로 정렬하는 메소드
-  private String alignCenter(String name) {
-    StringBuilder sb = new StringBuilder();
-    while(sb.length() < (MAX_NAME_SIZE-name.length())/2) sb.append(" ");
-    sb.append(name);
-    while(sb.length() < MAX_NAME_SIZE) sb.append(" ");
-    return sb.toString();
-  }
+    @Override
+    public void run() {
+        ArrayList<Player> players;
+        int LadderSize;
 
-  private ArrayList<Player> inputPlayersToPlayerList(String prompt) {
-    String inputString = console.input(prompt);
-    Optional<ArrayList<Player>> parsedValue = parsePlayerList(inputString);
-    if (parsedValue.isEmpty()) {
-      console.inputError();
-      return inputPlayersToPlayerList(prompt);
+        while (true) {
+            Optional<ArrayList<Player>> inputPlayers = inputNamesToPlayers();
+            if (inputPlayers.isPresent()) {
+                players = inputPlayers.get();
+                break;
+            }
+        }
+
+        while (true) {
+            Optional<Integer> inputLadderSize = inputNumberToLadderSize();
+            if (inputLadderSize.isPresent()) {
+                LadderSize = inputLadderSize.get();
+                break;
+            }
+        }
+
+        Ladder ladder = generator.generate(players.size(), LadderSize);
+        printPlayerList(players); // 사람 이름 출력
+        System.out.println(ladder); // 사다리 출력
     }
-    return parsedValue.get();
-  }
 
-  private Optional<ArrayList<Player>> parsePlayerList(String inputString) {
-    String[] playerNames = inputString.split(",");
-    if (!isValidPlayer(playerNames)) return Optional.empty();
-
-    ArrayList<Player> result = new ArrayList<>();
-    for (int i = 0; i < playerNames.length; i++) {
-      result.add(new Player(playerNames[i], i));
+    private void printPlayerList(ArrayList<Player> playerArrayList) {
+        StringBuilder sb = new StringBuilder();
+        for (var player : playerArrayList) {
+            sb.append(alignCenter(player.getName())).append(" ");
+        }
+        System.out.println(sb);
     }
-    return Optional.of(result);
-  }
 
-  private boolean isValidPlayer(String[] playerNames) {
-    if (playerNames.length < MIN_SIZE_OF_PEOPLE) return false; // 최소 플레이어수 확인
-    return Arrays.stream(playerNames).allMatch(name -> name.length() < MAX_NAME_SIZE);
-  }
-
-  private Optional<Integer> parseLadderSize(String inputString) {
-    try {
-      int result = Integer.parseInt(inputString);
-      if (result < MIN_SIZE_OF_LADDER) return Optional.empty();
-      return Optional.of(result);
-    } catch (NumberFormatException e) {
-      return Optional.empty();
+    // 5글자를 기준으로 이름을 가운데로 정렬하는 메소드
+    private String alignCenter(String name) {
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < (MAX_NAME_SIZE - name.length()) / 2) sb.append(" ");
+        sb.append(name);
+        while (sb.length() < MAX_NAME_SIZE) sb.append(" ");
+        return sb.toString();
     }
-  }
 
-  private int inputToLadderSize(String prompt) {
-    String inputString = console.input(prompt);
-    Optional<Integer> parsedValue = parseLadderSize(inputString);
-    if (parsedValue.isEmpty()) {
-      console.inputError();
-      return inputToLadderSize(prompt);
+    private Optional<ArrayList<Player>> inputNamesToPlayers() {
+        var input = console.input("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        if (input.isEmpty()) return Optional.empty();
+        return parsePlayerList(input.get());
     }
-    return parsedValue.get();
-  }
+
+    private Optional<ArrayList<Player>> parsePlayerList(String inputString) {
+        String[] playerNames = inputString.split(",");
+        if (!isValidPlayer(playerNames)) return Optional.empty();
+
+        ArrayList<Player> result = new ArrayList<>();
+        for (int i = 0; i < playerNames.length; i++) {
+            result.add(new Player(playerNames[i], i));
+        }
+        return Optional.of(result);
+    }
+
+    private boolean isValidPlayer(String[] playerNames) {
+        if (playerNames.length < MIN_SIZE_OF_PEOPLE) return false; // 최소 플레이어수 확인
+        return Arrays.stream(playerNames).allMatch(name -> name.length() < MAX_NAME_SIZE);
+    }
+
+    private Optional<Integer> parseLadderSize(String inputString) {
+        try {
+            int result = Integer.parseInt(inputString);
+            if (result < MIN_SIZE_OF_LADDER) return Optional.empty();
+            return Optional.of(result);
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
+    }
+
+    private Optional<Integer> inputNumberToLadderSize() {
+        var input = console.input("최대 사다리 높이는 몇 개인가요?");
+        if (input.isEmpty()) return Optional.empty();
+        return parseLadderSize(input.get());
+    }
 }
