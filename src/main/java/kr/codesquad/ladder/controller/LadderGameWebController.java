afda6005b7f2c72d55cae9808c930ff6745ec9a6
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-17 15:47:51 +0900

    feat: LadderGameManager 객체 생성하여 적용
    * 기존 컨트롤러가 직접 객체를 생성하여 사다리 객체를 생성하는 대신 LadderGameManager 객체에게 시키도록 변경하였습니다.

--- src/main/java/kr/codesquad/ladder/controller/LadderGameWebController.java
+++ src/main/java/kr/codesquad/ladder/controller/LadderGameWebController.java
@@ -1,17 +1,21 @@
 package kr.codesquad.ladder.controller;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStreamReader;
 import java.util.Random;
 import kr.codesquad.ladder.domain.Destinations;
-import kr.codesquad.ladder.domain.DestinationsFactory;
 import kr.codesquad.ladder.domain.Ladder;
 import kr.codesquad.ladder.domain.LadderGenerator;
 import kr.codesquad.ladder.domain.LadderResults;
 import kr.codesquad.ladder.domain.LadderResultsFactory;
 import kr.codesquad.ladder.domain.Names;
-import kr.codesquad.ladder.domain.NamesFactory;
 import kr.codesquad.ladder.exception.InvalidCountOfPeopleException;
+import kr.codesquad.ladder.view.LadderConsoleReader;
+import kr.codesquad.ladder.view.LadderConsoleWriter;
+import kr.codesquad.ladder.view.LadderReader;
+import kr.codesquad.ladder.view.LadderWriter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,16 +26,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Slf4j
 public class LadderGameWebController {
 
-    private final NamesFactory namesFactory;
-    private final DestinationsFactory destinationsFactory;
     private final Random random = new Random();
-
-    @Autowired
-    public LadderGameWebController(NamesFactory namesFactory,
-        DestinationsFactory destinationsFactory) {
-        this.namesFactory = namesFactory;
-        this.destinationsFactory = destinationsFactory;
-    }
 
     @GetMapping("/ladder")
     public ModelAndView ladderCountOfPeopleForm() {
@@ -43,7 +38,6 @@ public class LadderGameWebController {
     public ModelAndView ladderFormByCountOfPeople(
         @RequestParam("countOfPeople") int countOfPeople) {
         if (countOfPeople < 2) {
-            log.info("countOfPeople : " + countOfPeople);
             throw new InvalidCountOfPeopleException();
         }
 
@@ -57,21 +51,36 @@ public class LadderGameWebController {
         @RequestParam("nameOfPeople") String[] nameOfPeople,
         @RequestParam("destination") String[] destinationArray,
         @RequestParam("maximumLadderHeight") int maximumLadderHeight) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(
+            toInputStream(nameOfPeople, destinationArray, maximumLadderHeight))));
+        LadderWriter ladderWriter = new LadderConsoleWriter();
+        LadderReader ladderReader = new LadderConsoleReader(br, ladderWriter);
+        LadderGameManager consoleController = new LadderGameManager(
+            ladderReader, ladderWriter);
+
         ModelAndView mav = new ModelAndView("ladder");
-        Names names = namesFactory.createNames(nameOfPeople);
-        Destinations destinations = destinationsFactory.createDestinations(destinationArray,
-            nameOfPeople.length);
-        LadderGenerator ladderGenerator = new LadderGenerator(maximumLadderHeight);
+        Names names = consoleController.getNamesOfPeoples();
+        Destinations destinations = consoleController.getDestinations(names.size());
+        LadderGenerator ladderGenerator = consoleController.getLadderGenerator();
+        Ladder ladder = ladderGenerator.createLines(names.size(), random);
         LadderResultsFactory ladderResultsFactory = new LadderResultsFactory();
-        Ladder ladder = ladderGenerator.createLines(nameOfPeople.length, random);
         LadderResults ladderResults =
-            ladderResultsFactory.getAllLadderResult(names, ladder, destinations);
+            ladderResultsFactory.getLadderResults(names, ladder, destinations);
 
         mav.getModelMap().addAttribute("names", names);
         mav.getModelMap().addAttribute("ladder", ladder);
         mav.getModelMap().addAttribute("destinations", destinations);
         mav.getModelMap().addAttribute("ladderResults", ladderResults);
         return mav;
+    }
+
+    private byte[] toInputStream(String[] nameOfPeople, String[] destinationArray,
+        int maximumLadderHeight) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.join(",", nameOfPeople)).append("\n");
+        sb.append(String.join(",", destinationArray)).append("\n");
+        sb.append(maximumLadderHeight);
+        return sb.toString().getBytes();
     }
 
 }
