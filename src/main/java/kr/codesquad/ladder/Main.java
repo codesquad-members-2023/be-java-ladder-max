a7e5d0c6129c8531453acc33a97c14a6f87c4a35
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-17 15:03:20 +0900

    feat: 스프링 부트 프레임워크 메인 추가

--- src/main/java/kr/codesquad/ladder/Main.java
+++ src/main/java/kr/codesquad/ladder/Main.java
@@ -1,23 +1,11 @@
 package kr.codesquad.ladder;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import kr.codesquad.ladder.controller.LadderConsoleReaderController;
-import kr.codesquad.ladder.controller.LadderGame;
-import kr.codesquad.ladder.view.LadderConsoleReader;
-import kr.codesquad.ladder.view.LadderConsoleWriter;
-import kr.codesquad.ladder.view.LadderReader;
-import kr.codesquad.ladder.view.LadderWriter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
-
     public static void main(String[] args) {
-        LadderWriter ladderWriter = new LadderConsoleWriter();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        LadderReader ladderReader = new LadderConsoleReader(reader, ladderWriter);
-        LadderConsoleReaderController readerController = new LadderConsoleReaderController(
-            ladderReader, ladderWriter);
-        LadderGame game = new LadderGame(readerController, ladderWriter);
-        game.start();
+        SpringApplication.run(Main.class, args);
     }
 }
