33507c47a06bc3229421273b51879a3dae8a882c
DOEKYONG <apochmn@naver.com>
2023-03-20 01:47:25 +0900

    feat : 검색해서 찾는 서비스 추가

--- src/main/java/kr/codesquad/ladder/Service/LadderService.java
+++ src/main/java/kr/codesquad/ladder/Service/LadderService.java
@@ -3,18 +3,28 @@ package kr.codesquad.ladder.Service;
 import kr.codesquad.ladder.domain.*;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public class LadderService {
+    User user;
+    Result result;
+    Ladder ladder;
+    ResultLog resultLog;
+
 
     public String makeLadder(int length, String name, String resultName) {
         int countOfPesron = name.split(",").length;
-        User user = new User(name);
-        Result result = new Result(resultName);
-        Ladder ladder = new Ladder(length,countOfPesron);
-        ResultLog resultLog = new ResultLog(ladder,user,result,countOfPesron);
+         user = new User(name);
+         result = new Result(resultName);
+         ladder = new Ladder(length,countOfPesron);
+         resultLog = new ResultLog(ladder,user,result,countOfPesron);
         return ladder.toString();
+    }
+
+    public String resultLadder(String search)  {
+        return resultLog.existValidate(search);
     }
 
 }
