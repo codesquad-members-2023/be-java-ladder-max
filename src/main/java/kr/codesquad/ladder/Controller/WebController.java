573fa8a371e82dc49d308fdb5c690ec9bfcb7c20
DOEKYONG <apochmn@naver.com>
2023-03-20 01:49:59 +0900

    feat : 결과 페이지 매핑 및 기존 get매핑 제거

--- src/main/java/kr/codesquad/ladder/Controller/WebController.java
+++ src/main/java/kr/codesquad/ladder/Controller/WebController.java
@@ -8,34 +8,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
 public class WebController {
-
-
+    LadderService ladderService = new LadderService();
     @PostMapping("/ladder")
     public String postLadder (HttpServletRequest request,Model model){
         String name = request.getParameter("name");
         String result = request.getParameter("result");
         int length = Integer.parseInt(request.getParameter("length"));
-        String ladder = new LadderService().makeLadder(length,name,result).toString();
-        System.out.println(ladder);
+        String ladder = ladderService.makeLadder(length,name,result).toString();
         model.addAttribute("value",ladder);
         model.addAttribute("userName",name.split(","));
         model.addAttribute("resultName",result.split(","));
         return"ladder";
     }
-    @GetMapping("/ladder")
-    public String getLadderPage(){
-
-        return "ladder";
+    @PostMapping("/result")
+    public String postResultPage(HttpServletRequest request,Model model)  {
+        String str =request.getParameter("search");
+        model.addAttribute("resultLadder",ladderService.resultLadder(str));
+        return "result";
     }
-
 
 }
