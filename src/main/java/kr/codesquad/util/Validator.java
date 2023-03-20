bb84cd17e53364cae6b5b99ba3a75806a743402e
CDchan <kdjin1215@Naver.com>
2023-03-20 22:27:02 +0900

    refactor: validator 클래스 생성
    util패키지에 validator클래스를 생성한다.

---
+++ src/main/java/kr/codesquad/util/Validator.java
@@ -0,0 +1,60 @@
+package kr.codesquad.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Validator {
+
+    private static final String NAME_LENGTH_ERROR_MESSAGE = "이름은 5글자 이하여야 합니다. 다시 입력해주세요.";
+    private static final String PLAYER_NUMBER_ERROR_MESSAGE = "player는 2명 이상이 필요합니다. 다시 입력해주세요.";
+    private final String ERROR_MESSAGE_FOR_LADDER_HEIGHT = "사다리 높이는 2이상으로 입력";
+
+    public List<String> getValidPlayerFromUser(String players) {
+        try {
+            List<String> tempList = parseInputStringToValidNames(players);
+            validatePlayerNumAndThrowException(tempList);
+            return tempList;
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    private List<String> parseInputStringToValidNames(String inputString) {
+        return Arrays.stream(inputString.split(","))
+                .map(this::validateNameAndThrowException)
+                .collect(Collectors.toList());
+    }
+
+    private String validateNameAndThrowException(String name) {
+        if (name.length() > 5) {
+            throw new RuntimeException(NAME_LENGTH_ERROR_MESSAGE);
+        }
+        return name;
+    }
+
+    private void validatePlayerNumAndThrowException(List playersList) {
+        if (playersList.size() < 2) {
+            throw new RuntimeException(PLAYER_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
+    public boolean getValidHeightFromUser(int ladderHeight) {
+        try {
+            validateHeightAndThrowException(ladderHeight) ;
+            return true;
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    private int validateHeightAndThrowException(int height) {
+        if (height < 2) {
+            throw new RuntimeException(ERROR_MESSAGE_FOR_LADDER_HEIGHT);
+        }
+        return height;
+    }
+
+}
