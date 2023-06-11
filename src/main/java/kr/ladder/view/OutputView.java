13e263001a8aa38c4dd7ab11ad3e13b9f036db1c
sudago <sudago22@gmail.com>
2023-06-11 18:46:28 +0900

    fix: 플레이어 이름 주변에 공백을 적절히 배치

--- src/main/java/kr/ladder/view/OutputView.java
+++ src/main/java/kr/ladder/view/OutputView.java
@@ -19,14 +19,24 @@ public class OutputView {
     // 이 부분은 OutputView 의 일이 아니지 않나 고민중...
     private void printPlayersName(String[] players){
         for (String player : players) {
-            sb.append(player).append(insertBlank(player.length()));
+            sb.append(insertBlankAroundPlayerName(player));
         }
         sb.append("\n");
     }
 
-    public String insertBlank(int playerNameLength){
-        return " ".repeat(DEFAULT_WIDTH - playerNameLength);
+    // TODO: 테스트를 위해 private -> public로 변경했는데 더 나은 방법이 없을까?
+    public String insertBlankAroundPlayerName(String playerName){
+        return insertFront(playerName.length()) + playerName + insertBack(playerName.length());
     }
+
+    private String insertFront(int playerNameLength){ // 글자수가 홀수면 (이름 뒤에 들어가는 공백 수 - 1) 개가 들어가야 한다.
+        return " ".repeat((int)Math.floor((double)(DEFAULT_WIDTH - playerNameLength) / 2));
+    }
+
+    private String insertBack(int playerNameLength){ // 글자수가 홀수면 (이름 앞에 들어가는 공백 수 + 1) 개가 들어가야 한다.
+        return " ".repeat((int)Math.ceil((double)(DEFAULT_WIDTH - playerNameLength) / 2));
+    }
+
 
     private void printLadder(String ladder){
         sb.append(ladder);
