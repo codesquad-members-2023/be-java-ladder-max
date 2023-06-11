56100edc200272532805f93b7811e98db6da4a51
sudago <sudago22@gmail.com>
2023-06-11 18:43:21 +0900

    chore: 주석 제거

--- src/main/java/kr/ladder/domain/Ladder.java
+++ src/main/java/kr/ladder/domain/Ladder.java
@@ -54,30 +54,6 @@ public class Ladder {
         }
         return true;
     }
-
-//    욕심 시작...
-//    private void rebuildColumn(int ladderHeight){
-//        int[] columnFalses = new int[ladder.size()];
-//        for (LadderLine ladderLine : ladder) {
-//            List<Boolean> booleans = ladderLine.ladderLine;
-//            for (int i = 0; i < booleans.size(); i++) {
-//                if (!booleans.get(i)){
-//                    columnFalses[i]++;
-//                }
-//            }
-//        }
-//
-//        for (int i = 0; i < columnFalses.length; i++) {
-//            if (columnFalses[i] == ladderHeight) {
-//                changeLadderColumn(i);
-//            }
-//        }
-//    }
-
-    // 하드 코딩해서 마지막 열에서 추가하는 것으로 생각중..
-//    private void changeLadderColumn(int falseIndex){
-//
-//    }
     
     public String generate(){
         StringBuilder sb = new StringBuilder();
